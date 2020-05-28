6d55d68f261efd1433495466dbba117a16112364
liuyunbin <yunbinliu@outlook.com>
2020-05-28 22:37:26 +0800

    添加 函数 rotate_copy 的 单元测试

---
+++ cpp-standard-library/algorithm/rotate_copy/rotate_copy_unittest.cc
@@ -0,0 +1,20 @@
+
+#include "rotate_copy.h"
+
+#include <algorithm>
+#include <string>
+
+#include <gtest/gtest.h>
+
+TEST(test_algorithm, test_rotate_copy) {
+  std::string str_1 = "12341234abcd";
+  std::string str_2 = "12341234abcd";
+  std::string str_3 = "12341234abcd";
+  std::string str_4 = "12341234abcd";
+  std::rotate_copy(str_1.begin(), str_1.begin() + 3, str_1.end(),
+                   str_3.begin());
+  liuyunbin::rotate_copy(str_2.begin(), str_2.begin() + 3, str_2.end(),
+                         str_4.begin());
+  EXPECT_EQ(str_1, str_2);
+  EXPECT_EQ(str_3, str_4);
+}
