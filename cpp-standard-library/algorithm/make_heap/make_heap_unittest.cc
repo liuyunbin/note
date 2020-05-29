48f909b9d8af3fa88a683e1da9a17ef1272e42b4
liuyunbin <yunbinliu@outlook.com>
2020-05-29 14:22:03 +0800

    添加 函数 make_heap 的 单元测试

---
+++ cpp-standard-library/algorithm/make_heap/make_heap_unittest.cc
@@ -0,0 +1,24 @@
+
+#include "make_heap.h"
+
+#include <algorithm>
+#include <string>
+
+#include <gtest/gtest.h>
+
+TEST(test_algorithm, test_make_heap) {
+  {
+    std::string str_1 = "98653";
+    std::string str_2 = "98653";
+    std::make_heap(str_1.begin(), str_1.end());
+    liuyunbin::make_heap(str_2.begin(), str_2.end());
+    EXPECT_EQ(str_1, str_2);
+  }
+  {
+    std::string str_1 = "98657";
+    std::string str_2 = "98657";
+    std::make_heap(str_1.begin(), str_1.end());
+    liuyunbin::make_heap(str_2.begin(), str_2.end());
+    EXPECT_EQ(str_1, str_2);
+  }
+}
