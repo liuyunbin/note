11746547fa172ef1946254c04660a241b02c4648
liuyunbin <yunbinliu@outlook.com>
2020-05-29 13:58:13 +0800

    添加 函数 pop_heap 的 单元测试

---
+++ cpp-standard-library/algorithm/pop_heap/pop_heap_unittest.cc
@@ -0,0 +1,24 @@
+
+#include "pop_heap.h"
+
+#include <algorithm>
+#include <string>
+
+#include <gtest/gtest.h>
+
+TEST(test_algorithm, test_pop_heap) {
+  {
+    std::string str_1 = "98653";
+    std::string str_2 = "98653";
+    std::pop_heap(str_1.begin(), str_1.end());
+    liuyunbin::pop_heap(str_2.begin(), str_2.end());
+    EXPECT_EQ(str_1, str_2);
+  }
+  {
+    std::string str_1 = "98765";
+    std::string str_2 = "98765";
+    std::pop_heap(str_1.begin(), str_1.end());
+    liuyunbin::pop_heap(str_2.begin(), str_2.end());
+    EXPECT_EQ(str_1, str_2);
+  }
+}
