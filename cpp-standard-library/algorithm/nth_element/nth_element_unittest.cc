32b0597d64ef8c346ef0a32f38490142ad61ba2a
liuyunbin <yunbinliu@outlook.com>
2020-05-29 20:08:09 +0800

    添加 函数 nth_element 的 单元测试

---
+++ cpp-standard-library/algorithm/nth_element/nth_element_unittest.cc
@@ -0,0 +1,15 @@
+
+#include "nth_element.h"
+
+#include <algorithm>
+#include <string>
+
+#include <gtest/gtest.h>
+
+TEST(test_algorithm, test_nth_element) {
+  std::string str_1 = "246810";
+  std::string str_2 = "864210";
+  std::nth_element(str_1.begin(), str_1.begin() + 3, str_1.end());
+  liuyunbin::nth_element(str_2.begin(), str_2.begin() + 3, str_2.end());
+  EXPECT_EQ(str_1[2], str_2[2]);
+}
