a42582b04c055c8242879c8c3a08570c07593fa0
liuyunbin <yunbinliu@outlook.com>
2020-05-29 19:20:08 +0800

    添加 函数 partial_sort 的 单元测试

---
+++ cpp-standard-library/algorithm/partial_sort/partial_sort_unittest.cc
@@ -0,0 +1,15 @@
+
+#include "partial_sort.h"
+
+#include <algorithm>
+#include <string>
+
+#include <gtest/gtest.h>
+
+TEST(test_algorithm, test_partial_sort) {
+  std::string str_1 = "98653";
+  std::string str_2 = "98653";
+  std::partial_sort(str_1.begin(), str_1.begin() + 3, str_1.end());
+  liuyunbin::partial_sort(str_2.begin(), str_2.begin() + 3, str_2.end());
+  EXPECT_EQ(std::equal(str_1.begin(), str_1.begin() + 3, str_2.begin()), true);
+}
