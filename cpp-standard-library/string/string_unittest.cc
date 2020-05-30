cfe8d01f724041dcc8af82f90473c14d1f910584
liuyunbin <yunbinliu@outlook.com>
2020-05-30 12:17:46 +0800

    添加 类 string 的 单元测试

---
+++ cpp-standard-library/string/string_unittest.cc
@@ -0,0 +1,14 @@
+
+#include "string.h"
+
+#include <gtest/gtest.h>
+
+TEST(test_string, test_string) {
+  liuyunbin::string str_1 = "12345";
+  liuyunbin::string str_2 = "123";
+
+  str_1.swap(str_2);
+
+  EXPECT_NE(str_1, str_2);
+}
+
