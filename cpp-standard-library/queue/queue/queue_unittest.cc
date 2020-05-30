9acf9cba79e429d58091368f636af5e948e96e78
liuyunbin <yunbinliu@outlook.com>
2020-05-30 11:59:28 +0800

    添加 类 queue 的 单元测试

---
+++ cpp-standard-library/queue/queue/queue_unittest.cc
@@ -0,0 +1,26 @@
+
+#include "queue.h"
+
+#include <gtest/gtest.h>
+
+// 显示实例化
+template class liuyunbin::queue<int>;
+
+TEST(test_queue, test_queue) {
+  liuyunbin::queue<int> li_1;
+  li_1.push(0);
+  li_1.push(1);
+  li_1.push(2);
+  li_1.push(3);
+  li_1.push(4);
+  liuyunbin::queue<int> li_2;
+  li_2.push(5);
+  li_2.push(6);
+  li_2.push(7);
+  li_2.push(8);
+  li_2.push(9);
+
+  li_1.swap(li_2);
+
+  EXPECT_NE(li_1, li_2);
+}
