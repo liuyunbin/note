7c0427afc64fcb4ff493e74c898b85b2c30c0140
liuyunbin <yunbinliu@outlook.com>
2020-05-31 11:09:36 +0800

    添加 类 back_insert_iterator 的 显示实例化测试

---
+++ cpp-standard-library/iterator/back_insert_iterator/back_insert_iterator_unittest.cc
@@ -0,0 +1,9 @@
+
+#include "back_insert_iterator.h"
+
+#include <gtest/gtest.h>
+
+// 显示实例化
+template class liuyunbin::back_insert_iterator<std::string>;
+
+TEST(test_iterator, test_back_insert_iterator) {}
