7e1dd5e9f65f45d912e8033414c4633ab6ae540c
liuyunbin <yunbinliu@outlook.com>
2020-06-13 03:50:08 +0800

    使用 前向、双向和随机迭代器测试 函数 fill_n

--- cpp-standard-library/algorithm/fill_n/fill_n_unittest.cc
+++ cpp-standard-library/algorithm/fill_n/fill_n_unittest.cc
@@ -2,16 +2,46 @@
 #include "fill_n.h"
 
 #include <algorithm>
-#include <string>
+#include <forward_list>
+#include <list>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_fill_n) {
-  std::string str_1 = "123456789";
-  std::string str_2 = "123456789";
-  const char ch = '7';
-  const int n = 5;
-  std::fill_n(str_1.begin(), n, ch);
-  liuyunbin::fill_n(str_2.begin(), n, ch);
-  EXPECT_EQ(str_1, str_2);
+  // 前向迭代器
+  {
+    std::forward_list<int> forward_list_1 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_2 = {5, 6, 7, 8, 9};
+
+    int v = 3;
+    int n = 5;
+    std::fill_n(forward_list_1.begin(), n, v);
+    liuyunbin::fill_n(forward_list_2.begin(), n, v);
+    EXPECT_EQ(forward_list_1, forward_list_2);
+  }
+
+  // 双向迭代器
+  {
+    std::list<int> list_1 = {0, 1, 2, 3, 4};
+    std::list<int> list_2 = {5, 6, 7, 8, 9};
+
+    int v = 3;
+    int n = 5;
+    std::fill_n(list_1.begin(), n, v);
+    liuyunbin::fill_n(list_2.begin(), n, v);
+    EXPECT_EQ(list_1, list_2);
+  }
+
+  // 随机迭代器
+  {
+    std::vector<int> vector_1 = {0, 1, 2, 3, 4};
+    std::vector<int> vector_2 = {5, 6, 7, 8, 9};
+
+    int v = 3;
+    int n = 5;
+    std::fill_n(vector_1.begin(), n, v);
+    liuyunbin::fill_n(vector_2.begin(), n, v);
+    EXPECT_EQ(vector_1, vector_2);
+  }
 }
