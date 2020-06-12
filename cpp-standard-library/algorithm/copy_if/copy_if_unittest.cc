c90f25cf7d99c6fa03bdbeb6d94ff71b15620b7a
liuyunbin <yunbinliu@outlook.com>
2020-06-13 01:45:20 +0800

    使用 输入、前向、双向和随机迭代器测试 函数 copy_if

--- cpp-standard-library/algorithm/copy_if/copy_if_unittest.cc
+++ cpp-standard-library/algorithm/copy_if/copy_if_unittest.cc
@@ -2,21 +2,63 @@
 #include "copy_if.h"
 
 #include <algorithm>
-#include <cctype>
-#include <string>
+#include <forward_list>
+#include <iterator>
+#include <list>
+#include <sstream>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_copy_if) {
-  std::string str_from_1 = "1234abcd";
-  std::string str_from_2 = "1234abcd";
-  std::string str_to_1 = "abcd";
-  std::string str_to_2 = "abcd";
+  // 输入迭代器
+  {
+    std::istringstream stream_1("1 2 3 4 5");
+    std::istringstream stream_2("1 2 3 4 5");
+    std::forward_list<int> forward_list_3 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_4 = {5, 6, 2, 3, 4};
+    auto f = [](int v) { return v < 3 ? true : false; };
+    std::copy_if(std::istream_iterator<int>(stream_1),
+                 std::istream_iterator<int>(), forward_list_3.begin(), f);
+    liuyunbin::copy_if(std::istream_iterator<int>(stream_2),
+                       std::istream_iterator<int>(), forward_list_4.begin(), f);
+    EXPECT_EQ(forward_list_3, forward_list_4);
+  }
 
-  auto f = [](char ch) { return std::isdigit(ch) ? true : false; };
+  // 前向迭代器
+  {
+    std::forward_list<int> forward_list_1 = {1, 2, 3, 4, 5};
+    std::forward_list<int> forward_list_2 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_3 = {5, 6, 2, 3, 4};
+    auto f = [](int v) { return v < 3 ? true : false; };
+    std::copy_if(forward_list_1.cbegin(), forward_list_1.cend(),
+                 forward_list_2.begin(), f);
+    liuyunbin::copy_if(forward_list_1.cbegin(), forward_list_1.cend(),
+                       forward_list_3.begin(), f);
+    EXPECT_EQ(forward_list_2, forward_list_3);
+  }
 
-  std::copy_if(str_from_1.begin(), str_from_1.end(), str_to_1.begin(), f);
-  liuyunbin::copy_if(str_from_2.begin(), str_from_2.end(), str_to_2.begin(), f);
-  EXPECT_EQ(str_from_1, str_from_2);
-  EXPECT_EQ(str_to_1, str_to_2);
+  // 双向迭代器
+  {
+    std::list<int> list_1 = {1, 2, 3, 4, 5};
+    std::forward_list<int> forward_list_2 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_3 = {5, 6, 2, 3, 4};
+    auto f = [](int v) { return v < 3 ? true : false; };
+    std::copy_if(list_1.cbegin(), list_1.cend(), forward_list_2.begin(), f);
+    liuyunbin::copy_if(list_1.cbegin(), list_1.cend(), forward_list_3.begin(),
+                       f);
+    EXPECT_EQ(forward_list_2, forward_list_3);
+  }
+
+  // 随机迭代器
+  {
+    std::vector<int> vector_1 = {1, 2, 3, 4, 5};
+    std::forward_list<int> forward_list_2 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_3 = {5, 6, 2, 3, 4};
+    auto f = [](int v) { return v < 3 ? true : false; };
+    std::copy_if(vector_1.cbegin(), vector_1.cend(), forward_list_2.begin(), f);
+    liuyunbin::copy_if(vector_1.cbegin(), vector_1.cend(),
+                       forward_list_3.begin(), f);
+    EXPECT_EQ(forward_list_2, forward_list_3);
+  }
 }
