288e94ef2b6a816d96717bcb157fb85553754e7b
liuyunbin <yunbinliu@outlook.com>
2020-06-13 01:50:43 +0800

    使用 输入、前向、双向和随机迭代器测试 函数 copy_n

--- cpp-standard-library/algorithm/copy_n/copy_n_unittest.cc
+++ cpp-standard-library/algorithm/copy_n/copy_n_unittest.cc
@@ -2,18 +2,60 @@
 #include "copy_n.h"
 
 #include <algorithm>
+#include <forward_list>
+#include <iterator>
+#include <list>
+#include <sstream>
 #include <string>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_copy_n) {
-  std::string str_from_1 = "1234";
-  std::string str_from_2 = "1234";
-  std::string str_to_1 = "abcd";
-  std::string str_to_2 = "abcd";
-  const int n = 3;
-  std::copy_n(str_from_1.begin(), n, str_to_1.begin());
-  liuyunbin::copy_n(str_from_2.begin(), n, str_to_2.begin());
-  EXPECT_EQ(str_from_1, str_from_2);
-  EXPECT_EQ(str_to_1, str_to_2);
+  // 输入迭代器
+  {
+    std::istringstream stream_1("1 2 3 4 5");
+    std::istringstream stream_2("1 2 3 4 5");
+    int n = 5;
+    std::forward_list<int> forward_list_3 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_4 = {5, 6, 7, 8, 9};
+    std::copy_n(std::istream_iterator<int>(stream_1), n,
+                forward_list_3.begin());
+    liuyunbin::copy_n(std::istream_iterator<int>(stream_2), n,
+                      forward_list_4.begin());
+    EXPECT_EQ(forward_list_3, forward_list_4);
+  }
+
+  // 前向迭代器
+  {
+    std::forward_list<int> forward_list_1 = {1, 2, 3, 4, 5};
+    std::forward_list<int> forward_list_2 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_3 = {5, 6, 7, 8, 9};
+    int n = 5;
+    std::copy_n(forward_list_1.cbegin(), n, forward_list_2.begin());
+    liuyunbin::copy_n(forward_list_1.cbegin(), n, forward_list_3.begin());
+    EXPECT_EQ(forward_list_2, forward_list_3);
+  }
+
+  // 双向迭代器
+  {
+    std::list<int> list_1 = {1, 2, 3, 4, 5};
+    std::forward_list<int> forward_list_2 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_3 = {5, 6, 7, 8, 9};
+    int n = 5;
+    std::copy_n(list_1.cbegin(), n, forward_list_2.begin());
+    liuyunbin::copy_n(list_1.cbegin(), n, forward_list_3.begin());
+    EXPECT_EQ(forward_list_2, forward_list_3);
+  }
+
+  // 随机迭代器
+  {
+    std::vector<int> vector_1 = {1, 2, 3, 4, 5};
+    std::forward_list<int> forward_list_2 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_3 = {5, 6, 7, 8, 9};
+    int n = 5;
+    std::copy_n(vector_1.cbegin(), n, forward_list_2.begin());
+    liuyunbin::copy_n(vector_1.cbegin(), n, forward_list_3.begin());
+    EXPECT_EQ(forward_list_2, forward_list_3);
+  }
 }
