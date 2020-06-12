f079c1a221912e4a4d8ad7e63a678772fd889fe2
liuyunbin <yunbinliu@outlook.com>
2020-06-13 03:59:59 +0800

    使用 前向、双向和随机迭代器测试 函数 find

--- cpp-standard-library/algorithm/find/find_unittest.cc
+++ cpp-standard-library/algorithm/find/find_unittest.cc
@@ -2,27 +2,61 @@
 #include "find.h"
 
 #include <algorithm>
+#include <forward_list>
+#include <iterator>
+#include <list>
+#include <sstream>
 #include <string>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_find) {
+  // 前向迭代器
   {
-    std::string str = "abcdefghi";
-    const char ch = 'h';
-    EXPECT_EQ(std::find(str.begin(), str.end(), ch),
-              liuyunbin::find(str.begin(), str.end(), ch));
+    std::forward_list<int> forward_list_1 = {1, 2, 3, 4, 5};
+    int v = 3;
+    auto x = std::find(forward_list_1.cbegin(), forward_list_1.cend(), v);
+    auto y = liuyunbin::find(forward_list_1.cbegin(), forward_list_1.cend(), v);
+    EXPECT_EQ(x, y);
   }
   {
-    std::string str = "abcdefghi";
-    const char ch = '\0';
-    EXPECT_EQ(std::find(str.begin(), str.end(), ch),
-              liuyunbin::find(str.begin(), str.end(), ch));
+    std::forward_list<int> forward_list_1 = {1, 2, 3, 4, 5};
+    int v = 10;
+    auto x = std::find(forward_list_1.cbegin(), forward_list_1.cend(), v);
+    auto y = liuyunbin::find(forward_list_1.cbegin(), forward_list_1.cend(), v);
+    EXPECT_EQ(x, y);
+  }
+
+  // 双向迭代器
+  {
+    std::list<int> list_1 = {1, 2, 3, 4, 5};
+    int v = 3;
+    auto x = std::find(list_1.cbegin(), list_1.cend(), v);
+    auto y = liuyunbin::find(list_1.cbegin(), list_1.cend(), v);
+    EXPECT_EQ(x, y);
   }
   {
-    std::string str = "abcdefghi";
-    const char ch = '1';
-    EXPECT_EQ(std::find(str.begin(), str.end(), ch),
-              liuyunbin::find(str.begin(), str.end(), ch));
+    std::list<int> list_1 = {1, 2, 3, 4, 5};
+    int v = 10;
+    auto x = std::find(list_1.cbegin(), list_1.cend(), v);
+    auto y = liuyunbin::find(list_1.cbegin(), list_1.cend(), v);
+    EXPECT_EQ(x, y);
+  }
+
+  // 随机迭代器
+  {
+    std::vector<int> vector_1 = {1, 2, 3, 4, 5};
+    int v = 3;
+    auto x = std::find(vector_1.cbegin(), vector_1.cend(), v);
+    auto y = liuyunbin::find(vector_1.cbegin(), vector_1.cend(), v);
+    EXPECT_EQ(x, y);
+  }
+  {
+    std::vector<int> vector_1 = {1, 2, 3, 4, 5};
+    int v = 10;
+    auto x = std::find(vector_1.cbegin(), vector_1.cend(), v);
+    auto y = liuyunbin::find(vector_1.cbegin(), vector_1.cend(), v);
+    EXPECT_EQ(x, y);
   }
 }
