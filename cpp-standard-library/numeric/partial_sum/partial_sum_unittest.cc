bb6da2d00c6d9494c29b95b033235495fdff97d0
liuyunbin <yunbinliu@outlook.com>
2020-06-12 14:47:48 +0800

    使用 输入、前向、双向和随机迭代器测试 函数 partial_sum

--- cpp-standard-library/numeric/partial_sum/partial_sum_unittest.cc
+++ cpp-standard-library/numeric/partial_sum/partial_sum_unittest.cc
@@ -1,15 +1,63 @@
 
 #include "partial_sum.h"
 
+#include <forward_list>
+#include <iterator>
 #include <list>
 #include <numeric>
+#include <sstream>
+#include <string>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_numeric, test_partial_sum) {
-  std::list<int> li_1 = {1, 3, 5, 7, 9};
-  std::list<int> li_2 = {1, 3, 5, 7, 9};
-  std::partial_sum(li_1.cbegin(), li_1.cend(), li_1.begin());
-  liuyunbin::partial_sum(li_2.cbegin(), li_2.cend(), li_2.begin());
-  EXPECT_EQ(li_1, li_2);
+  // 输入迭代器
+  {
+    std::istringstream stream_1("1 2 3 4 5");
+    std::istringstream stream_2("1 2 3 4 5");
+    std::forward_list<int> forward_list_3 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_4 = {5, 6, 7, 8, 9};
+    std::partial_sum(std::istream_iterator<int>(stream_1),
+                     std::istream_iterator<int>(), forward_list_3.begin());
+    liuyunbin::partial_sum(std::istream_iterator<int>(stream_2),
+                           std::istream_iterator<int>(),
+                           forward_list_4.begin());
+    EXPECT_EQ(forward_list_3, forward_list_4);
+  }
+
+  // 前向迭代器
+  {
+    std::forward_list<int> forward_list_1 = {1, 2, 3, 4, 5};
+    std::forward_list<int> forward_list_2 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_3 = {5, 6, 7, 8, 9};
+    std::partial_sum(forward_list_1.cbegin(), forward_list_1.cend(),
+                     forward_list_2.begin());
+    liuyunbin::partial_sum(forward_list_1.cbegin(), forward_list_1.cend(),
+                           forward_list_3.begin());
+    EXPECT_EQ(forward_list_2, forward_list_3);
+  }
+
+  // 双向迭代器
+  {
+    std::list<int> list_1 = {1, 2, 3, 4, 5};
+    std::forward_list<int> forward_list_2 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_3 = {5, 6, 7, 8, 9};
+    std::partial_sum(list_1.cbegin(), list_1.cend(), forward_list_2.begin());
+    liuyunbin::partial_sum(list_1.cbegin(), list_1.cend(),
+                           forward_list_3.begin());
+    EXPECT_EQ(forward_list_2, forward_list_3);
+  }
+
+  // 随机迭代器
+  {
+    std::vector<int> vector_1 = {1, 2, 3, 4, 5};
+    std::forward_list<int> forward_list_2 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_3 = {5, 6, 7, 8, 9};
+    std::partial_sum(vector_1.cbegin(), vector_1.cend(),
+                     forward_list_2.begin());
+    liuyunbin::partial_sum(vector_1.cbegin(), vector_1.cend(),
+                           forward_list_3.begin());
+    EXPECT_EQ(forward_list_2, forward_list_3);
+  }
 }
