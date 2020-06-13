cf6a1c86545b9715aa6e7d24acdf525bf9d9e4a3
liuyunbin <yunbinliu@outlook.com>
2020-06-13 18:41:49 +0800

    使用 随机迭代器 测试 函数 nth_element

--- cpp-standard-library/algorithm/nth_element/nth_element_unittest.cc
+++ cpp-standard-library/algorithm/nth_element/nth_element_unittest.cc
@@ -7,6 +7,7 @@
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_nth_element) {
+  // 随机迭代器
   std::string str_1 = "246810";
   std::string str_2 = "864210";
   std::nth_element(str_1.begin(), str_1.begin() + 3, str_1.end());
