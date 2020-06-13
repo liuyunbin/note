b1693f6f4f3349817296dafb18c765483c20a82d
liuyunbin <yunbinliu@outlook.com>
2020-06-14 05:46:25 +0800

    使用 前向、双向和随机迭代器测试 函数 partition_point

--- cpp-standard-library/algorithm/partition_point/partition_point_unittest.cc
+++ cpp-standard-library/algorithm/partition_point/partition_point_unittest.cc
@@ -2,14 +2,62 @@
 #include "partition_point.h"
 
 #include <algorithm>
-#include <cctype>
-#include <string>
+#include <forward_list>
+#include <list>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_partition_point) {
-  std::string str = "1234abcd";
-  auto f = [](char ch) { return std::isdigit(ch) ? true : false; };
-  EXPECT_EQ(std::partition_point(str.begin(), str.end(), f),
-            liuyunbin::partition_point(str.begin(), str.end(), f));
+  // 前向迭代器
+  {
+    std::forward_list<int> forward_list_1 = {1, 2, 3, 4, 5};
+    auto f = [](int v) { return v < 3; };
+    auto x =
+        std::partition_point(forward_list_1.cbegin(), forward_list_1.cend(), f);
+    auto y = liuyunbin::partition_point(forward_list_1.cbegin(),
+                                        forward_list_1.cend(), f);
+    EXPECT_EQ(x, y);
+  }
+  {
+    std::forward_list<int> forward_list_1 = {1, 2, 3, 4, 5};
+    auto f = [](int v) { return v == 10; };
+    auto x =
+        std::partition_point(forward_list_1.cbegin(), forward_list_1.cend(), f);
+    auto y = liuyunbin::partition_point(forward_list_1.cbegin(),
+                                        forward_list_1.cend(), f);
+    EXPECT_EQ(x, y);
+  }
+
+  // 双向迭代器
+  {
+    std::list<int> list_1 = {1, 2, 3, 4, 5};
+    auto f = [](int v) { return v < 3; };
+    auto x = std::partition_point(list_1.cbegin(), list_1.cend(), f);
+    auto y = liuyunbin::partition_point(list_1.cbegin(), list_1.cend(), f);
+    EXPECT_EQ(x, y);
+  }
+  {
+    std::list<int> list_1 = {1, 2, 3, 4, 5};
+    auto f = [](int v) { return v == 10; };
+    auto x = std::partition_point(list_1.cbegin(), list_1.cend(), f);
+    auto y = liuyunbin::partition_point(list_1.cbegin(), list_1.cend(), f);
+    EXPECT_EQ(x, y);
+  }
+
+  // 随机迭代器
+  {
+    std::vector<int> vector_1 = {1, 2, 3, 4, 5};
+    auto f = [](int v) { return v < 3; };
+    auto x = std::partition_point(vector_1.cbegin(), vector_1.cend(), f);
+    auto y = liuyunbin::partition_point(vector_1.cbegin(), vector_1.cend(), f);
+    EXPECT_EQ(x, y);
+  }
+  {
+    std::vector<int> vector_1 = {1, 2, 3, 4, 5};
+    auto f = [](int v) { return v == 10; };
+    auto x = std::partition_point(vector_1.cbegin(), vector_1.cend(), f);
+    auto y = liuyunbin::partition_point(vector_1.cbegin(), vector_1.cend(), f);
+    EXPECT_EQ(x, y);
+  }
 }
