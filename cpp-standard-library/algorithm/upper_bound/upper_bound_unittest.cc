533a84a4f512e6b9e8ca2282c5b355613d0f0c46
liuyunbin <yunbinliu@outlook.com>
2020-06-13 17:15:47 +0800

    使用 前向、双向和随机迭代器测试 函数 upper_bound

--- cpp-standard-library/algorithm/upper_bound/upper_bound_unittest.cc
+++ cpp-standard-library/algorithm/upper_bound/upper_bound_unittest.cc
@@ -2,21 +2,68 @@
 #include "upper_bound.h"
 
 #include <algorithm>
-#include <string>
+#include <forward_list>
+#include <list>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_upper_bound) {
+  // 前向迭代器
   {
-    std::string str = "123456789";
-    const char ch = '7';
-    EXPECT_EQ(std::upper_bound(str.cbegin(), str.cend(), ch),
-              liuyunbin::upper_bound(str.cbegin(), str.cend(), ch));
+    std::forward_list<int> forward_list_1 = {0, 1, 2, 3, 4};
+
+    int v = 3;
+    auto x =
+        std::upper_bound(forward_list_1.cbegin(), forward_list_1.cend(), v);
+    auto y = liuyunbin::upper_bound(forward_list_1.cbegin(),
+                                    forward_list_1.cend(), v);
+    EXPECT_EQ(x, y);
   }
   {
-    std::string str = "123456789";
-    const char ch = 'a';
-    EXPECT_EQ(std::upper_bound(str.cbegin(), str.cend(), ch),
-              liuyunbin::upper_bound(str.cbegin(), str.cend(), ch));
+    std::forward_list<int> forward_list_1 = {0, 1, 2, 3, 4};
+
+    int v = 10;
+    auto x =
+        std::upper_bound(forward_list_1.cbegin(), forward_list_1.cend(), v);
+    auto y = liuyunbin::upper_bound(forward_list_1.cbegin(),
+                                    forward_list_1.cend(), v);
+    EXPECT_EQ(x, y);
+  }
+
+  // 双向迭代器
+  {
+    std::list<int> list_1 = {0, 1, 2, 3, 4};
+
+    int v = 3;
+    auto x = std::upper_bound(list_1.cbegin(), list_1.cend(), v);
+    auto y = liuyunbin::upper_bound(list_1.cbegin(), list_1.cend(), v);
+    EXPECT_EQ(x, y);
+  }
+  {
+    std::list<int> list_1 = {0, 1, 2, 3, 4};
+
+    int v = 10;
+    auto x = std::upper_bound(list_1.cbegin(), list_1.cend(), v);
+    auto y = liuyunbin::upper_bound(list_1.cbegin(), list_1.cend(), v);
+    EXPECT_EQ(x, y);
+  }
+
+  // 随机迭代器
+  {
+    std::vector<int> vector_1 = {0, 1, 2, 3, 4};
+
+    int v = 3;
+    auto x = std::upper_bound(vector_1.cbegin(), vector_1.cend(), v);
+    auto y = liuyunbin::upper_bound(vector_1.cbegin(), vector_1.cend(), v);
+    EXPECT_EQ(x, y);
+  }
+  {
+    std::vector<int> vector_1 = {0, 1, 2, 3, 4};
+
+    int v = 10;
+    auto x = std::upper_bound(vector_1.cbegin(), vector_1.cend(), v);
+    auto y = liuyunbin::upper_bound(vector_1.cbegin(), vector_1.cend(), v);
+    EXPECT_EQ(x, y);
   }
 }
