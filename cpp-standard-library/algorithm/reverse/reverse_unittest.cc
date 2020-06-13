404a6176f37a41085ef793d1cd19d484618f49b0
liuyunbin <yunbinliu@outlook.com>
2020-06-14 07:22:03 +0800

    使用 双向和随机迭代器测试 函数 reverse

--- cpp-standard-library/algorithm/reverse/reverse_unittest.cc
+++ cpp-standard-library/algorithm/reverse/reverse_unittest.cc
@@ -2,14 +2,29 @@
 #include "reverse.h"
 
 #include <algorithm>
-#include <string>
+#include <list>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_reverse) {
-  std::string str_1 = "12341234abcd";
-  std::string str_2 = "12341234abcd";
-  std::reverse(str_1.begin(), str_1.end());
-  liuyunbin::reverse(str_2.begin(), str_2.end());
-  EXPECT_EQ(str_1, str_2);
+  // 双向迭代器
+  {
+    std::list<int> list_1 = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+    std::list<int> list_2 = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+
+    std::reverse(list_1.begin(), list_1.end());
+    liuyunbin::reverse(list_2.begin(), list_2.end());
+    EXPECT_EQ(list_1, list_2);
+  }
+
+  // 随机迭代器
+  {
+    std::vector<int> vector_1 = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+    std::vector<int> vector_2 = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+
+    std::reverse(vector_1.begin(), vector_1.end());
+    liuyunbin::reverse(vector_2.begin(), vector_2.end());
+    EXPECT_EQ(vector_1, vector_2);
+  }
 }
