e1808af214f6270b4a4bc5d244633b657d7b4732
liuyunbin <yunbinliu@outlook.com>
2020-06-13 13:29:42 +0800

    使用 输入、前向、双向和随机迭代器测试 函数 includes

--- cpp-standard-library/algorithm/includes/includes_unittest.cc
+++ cpp-standard-library/algorithm/includes/includes_unittest.cc
@@ -1,34 +1,112 @@
 
 #include "includes.h"
 
-#include <algorithm>
+#include <forward_list>
+#include <iterator>
+#include <list>
+#include <numeric>
+#include <sstream>
 #include <string>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_includes) {
+  // 输入迭代器
   {
-    std::string str_1 = "abcdefghi";
-    std::string str_2 = "abcdefghi";
-    EXPECT_EQ(
-        std::includes(str_1.begin(), str_1.end(), str_2.begin(), str_2.end()),
-        liuyunbin::includes(str_1.begin(), str_1.end(), str_2.begin(),
-                            str_2.end()));
+    std::istringstream stream_1("0 1 2 3 4");
+    std::istringstream stream_2("5 6 7 8 9");
+    std::istringstream stream_3("0 1 2 3 4");
+    std::istringstream stream_4("5 6 7 8 9");
+
+    auto x = std::includes(
+        std::istream_iterator<int>(stream_1), std::istream_iterator<int>(),
+        std::istream_iterator<int>(stream_2), std::istream_iterator<int>());
+    auto y = liuyunbin::includes(
+        std::istream_iterator<int>(stream_3), std::istream_iterator<int>(),
+        std::istream_iterator<int>(stream_4), std::istream_iterator<int>());
+    EXPECT_EQ(x, y);
   }
   {
-    std::string str_1 = "abcdefghi";
-    std::string str_2 = "defghi";
-    EXPECT_EQ(
-        std::includes(str_1.begin(), str_1.end(), str_2.begin(), str_2.end()),
-        liuyunbin::includes(str_1.begin(), str_1.end(), str_2.begin(),
-                            str_2.end()));
+    std::istringstream stream_1("0 1 2 3 4");
+    std::istringstream stream_2("2 3 4");
+    std::istringstream stream_3("0 1 2 3 4");
+    std::istringstream stream_4("2 3 4");
+
+    auto x = std::includes(
+        std::istream_iterator<int>(stream_1), std::istream_iterator<int>(),
+        std::istream_iterator<int>(stream_2), std::istream_iterator<int>());
+    auto y = liuyunbin::includes(
+        std::istream_iterator<int>(stream_3), std::istream_iterator<int>(),
+        std::istream_iterator<int>(stream_4), std::istream_iterator<int>());
+    EXPECT_EQ(x, y);
+  }
+
+  // 前向迭代器
+  {
+    std::forward_list<int> forward_list_1 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_2 = {5, 6, 7, 8, 9};
+
+    auto x = std::includes(forward_list_1.cbegin(), forward_list_1.cend(),
+                           forward_list_2.cbegin(), forward_list_2.cend());
+    auto y =
+        liuyunbin::includes(forward_list_1.cbegin(), forward_list_1.cend(),
+                            forward_list_2.cbegin(), forward_list_2.cend());
+    EXPECT_EQ(x, y);
   }
   {
-    std::string str_1 = "abcdefghi";
-    std::string str_2 = "123456";
-    EXPECT_EQ(
-        std::includes(str_1.begin(), str_1.end(), str_2.begin(), str_2.end()),
-        liuyunbin::includes(str_1.begin(), str_1.end(), str_2.begin(),
-                            str_2.end()));
+    std::forward_list<int> forward_list_1 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_2 = {2, 3, 4};
+
+    auto x = std::includes(forward_list_1.cbegin(), forward_list_1.cend(),
+                           forward_list_2.cbegin(), forward_list_2.cend());
+    auto y =
+        liuyunbin::includes(forward_list_1.cbegin(), forward_list_1.cend(),
+                            forward_list_2.cbegin(), forward_list_2.cend());
+    EXPECT_EQ(x, y);
+  }
+
+  // 双向迭代器
+  {
+    std::list<int> list_1 = {0, 1, 2, 3, 4};
+    std::list<int> list_2 = {5, 6, 7, 8, 9};
+
+    auto x = std::includes(list_1.cbegin(), list_1.cend(), list_2.cbegin(),
+                           list_2.cend());
+    auto y = liuyunbin::includes(list_1.cbegin(), list_1.cend(),
+                                 list_2.cbegin(), list_2.cend());
+    EXPECT_EQ(x, y);
+  }
+  {
+    std::list<int> list_1 = {0, 1, 2, 3, 4};
+    std::list<int> list_2 = {2, 3, 4};
+
+    auto x = std::includes(list_1.cbegin(), list_1.cend(), list_2.cbegin(),
+                           list_2.cend());
+    auto y = liuyunbin::includes(list_1.cbegin(), list_1.cend(),
+                                 list_2.cbegin(), list_2.cend());
+    EXPECT_EQ(x, y);
+  }
+
+  // 随机迭代器
+  {
+    std::vector<int> vector_1 = {0, 1, 2, 3, 4};
+    std::vector<int> vector_2 = {5, 6, 7, 8, 9};
+
+    auto x = std::includes(vector_1.cbegin(), vector_1.cend(),
+                           vector_2.cbegin(), vector_2.cend());
+    auto y = liuyunbin::includes(vector_1.cbegin(), vector_1.cend(),
+                                 vector_2.cbegin(), vector_2.cend());
+    EXPECT_EQ(x, y);
+  }
+  {
+    std::vector<int> vector_1 = {0, 1, 2, 3, 4};
+    std::vector<int> vector_2 = {2, 3, 4};
+
+    auto x = std::includes(vector_1.cbegin(), vector_1.cend(),
+                           vector_2.cbegin(), vector_2.cend());
+    auto y = liuyunbin::includes(vector_1.cbegin(), vector_1.cend(),
+                                 vector_2.cbegin(), vector_2.cend());
+    EXPECT_EQ(x, y);
   }
 }
