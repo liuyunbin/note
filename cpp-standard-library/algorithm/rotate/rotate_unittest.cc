bc6910b64e2804802320abe3b804ef4a1dee9002
liuyunbin <yunbinliu@outlook.com>
2020-06-14 07:46:19 +0800

    使用 前向、双向和随机迭代器测试 函数 rotate

--- cpp-standard-library/algorithm/rotate/rotate_unittest.cc
+++ cpp-standard-library/algorithm/rotate/rotate_unittest.cc
@@ -2,14 +2,48 @@
 #include "rotate.h"
 
 #include <algorithm>
-#include <string>
+#include <forward_list>
+#include <iterator>
+#include <list>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_rotate) {
-  std::string str_1 = "12341234abcd";
-  std::string str_2 = "12341234abcd";
-  std::rotate(str_1.begin(), str_1.begin() + 3, str_1.end());
-  liuyunbin::rotate(str_2.begin(), str_2.begin() + 3, str_2.end());
-  EXPECT_EQ(str_1, str_2);
+  // 前向迭代器
+  {
+    std::forward_list<int> forward_list_1 = {7, 8, 0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_2 = {7, 8, 0, 1, 2, 3, 4};
+
+    liuyunbin::rotate(forward_list_1.begin(),
+                      std::next(forward_list_1.begin(), 2),
+                      forward_list_1.end());
+    liuyunbin::rotate(forward_list_2.begin(),
+                      std::next(forward_list_2.begin(), 2),
+                      forward_list_2.end());
+    EXPECT_EQ(forward_list_1, forward_list_2);
+  }
+
+  // 双向迭代器
+  {
+    std::list<int> list_1 = {7, 8, 0, 1, 2, 3, 4};
+    std::list<int> list_2 = {7, 8, 0, 1, 2, 3, 4};
+
+    std::rotate(list_1.begin(), std::next(list_1.begin(), 2), list_1.end());
+    liuyunbin::rotate(list_2.begin(), std::next(list_2.begin(), 2),
+                      list_2.end());
+    EXPECT_EQ(list_1, list_2);
+  }
+
+  // 随机迭代器
+  {
+    std::vector<int> vector_1 = {7, 8, 0, 1, 2, 3, 4};
+    std::vector<int> vector_2 = {7, 8, 0, 1, 2, 3, 4};
+
+    std::rotate(vector_1.begin(), std::next(vector_1.begin(), 2),
+                vector_1.end());
+    liuyunbin::rotate(vector_2.begin(), std::next(vector_2.begin(), 2),
+                      vector_2.end());
+    EXPECT_EQ(vector_1, vector_2);
+  }
 }
