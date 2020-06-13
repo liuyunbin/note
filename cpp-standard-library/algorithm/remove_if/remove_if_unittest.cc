e832115fe01abb87bdd1022481cc6704c327653b
liuyunbin <yunbinliu@outlook.com>
2020-06-14 06:27:56 +0800

    使用 前向、双向和随机迭代器测试 函数 remove_if

--- cpp-standard-library/algorithm/remove_if/remove_if_unittest.cc
+++ cpp-standard-library/algorithm/remove_if/remove_if_unittest.cc
@@ -2,16 +2,70 @@
 #include "remove_if.h"
 
 #include <algorithm>
-#include <cctype>
-#include <string>
+#include <forward_list>
+#include <list>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_remove_if) {
-  std::string str_1 = "12341234abcd";
-  std::string str_2 = "12341234abcd";
-  auto f = [](char ch) { return std::isdigit(ch) ? true : false; };
-  auto p1 = std::remove_if(str_1.begin(), str_1.end(), f);
-  liuyunbin::remove_if(str_2.begin(), str_2.end(), f);
-  EXPECT_TRUE(std::equal(str_1.begin(), p1, str_2.begin()));
+  // 前向迭代器
+  {
+    std::forward_list<int> forward_list_1 = {1, 2, 3, 4, 5};
+    std::forward_list<int> forward_list_2 = {1, 2, 3, 4, 5};
+
+    auto f = [](const int &v) { return v < 3 ? true : false; };
+    std::remove_if(forward_list_1.begin(), forward_list_1.end(), f);
+    liuyunbin::remove_if(forward_list_2.begin(), forward_list_2.end(), f);
+    EXPECT_EQ(forward_list_1, forward_list_2);
+  }
+  {
+    std::forward_list<int> forward_list_1 = {1, 2, 3, 4, 5};
+    std::forward_list<int> forward_list_2 = {1, 2, 3, 4, 5};
+
+    auto f = [](const int &v) { return v > 10 ? true : false; };
+    std::remove_if(forward_list_1.begin(), forward_list_1.end(), f);
+    liuyunbin::remove_if(forward_list_2.begin(), forward_list_2.end(), f);
+    EXPECT_EQ(forward_list_1, forward_list_2);
+  }
+
+  // 双向迭代器
+  {
+    std::list<int> list_1 = {1, 2, 3, 4, 5};
+    std::list<int> list_2 = {1, 2, 3, 4, 5};
+
+    auto f = [](const int &v) { return v < 3 ? true : false; };
+    std::remove_if(list_1.begin(), list_1.end(), f);
+    liuyunbin::remove_if(list_2.begin(), list_2.end(), f);
+    EXPECT_EQ(list_1, list_2);
+  }
+  {
+    std::list<int> list_1 = {1, 2, 3, 4, 5};
+    std::list<int> list_2 = {1, 2, 3, 4, 5};
+
+    auto f = [](const int &v) { return v > 10 ? true : false; };
+    std::remove_if(list_1.begin(), list_1.end(), f);
+    liuyunbin::remove_if(list_2.begin(), list_2.end(), f);
+    EXPECT_EQ(list_1, list_2);
+  }
+
+  // 随机迭代器
+  {
+    std::vector<int> vector_1 = {1, 2, 3, 4, 5};
+    std::vector<int> vector_2 = {1, 2, 3, 4, 5};
+
+    auto f = [](const int &v) { return v < 3 ? true : false; };
+    std::remove_if(vector_1.begin(), vector_1.end(), f);
+    liuyunbin::remove_if(vector_2.begin(), vector_2.end(), f);
+    EXPECT_EQ(vector_1, vector_2);
+  }
+  {
+    std::vector<int> vector_1 = {1, 2, 3, 4, 5};
+    std::vector<int> vector_2 = {1, 2, 3, 4, 5};
+
+    auto f = [](const int &v) { return v > 10 ? true : false; };
+    std::remove_if(vector_1.begin(), vector_1.end(), f);
+    liuyunbin::remove_if(vector_2.begin(), vector_2.end(), f);
+    EXPECT_EQ(vector_1, vector_2);
+  }
 }
