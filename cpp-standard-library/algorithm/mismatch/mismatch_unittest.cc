f0c81db085458739dc2ca8c488f6d6de8c89c2d5
liuyunbin <yunbinliu@outlook.com>
2020-06-13 18:01:01 +0800

    使用 前向、双向和随机迭代器测试 函数 mismatch

--- cpp-standard-library/algorithm/mismatch/mismatch_unittest.cc
+++ cpp-standard-library/algorithm/mismatch/mismatch_unittest.cc
@@ -2,27 +2,76 @@
 #include "mismatch.h"
 
 #include <algorithm>
+#include <forward_list>
+#include <iterator>
+#include <list>
+#include <sstream>
 #include <string>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_mismatch) {
+  // 输入迭代器 不好测试
+
+  // 前向迭代器
   {
-    std::string str_1 = "acdfbdeghi";
-    std::string str_2 = "acdfbdeghi";
-    EXPECT_EQ(std::mismatch(str_1.begin(), str_1.end(), str_2.begin()),
-              liuyunbin::mismatch(str_1.begin(), str_1.end(), str_2.begin()));
+    std::forward_list<int> forward_list_1 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_2 = {5, 6, 7, 8, 9};
+
+    auto x = std::mismatch(forward_list_1.cbegin(), forward_list_1.cend(),
+                           forward_list_2.cbegin());
+    auto y = liuyunbin::mismatch(forward_list_1.cbegin(), forward_list_1.cend(),
+                                 forward_list_2.cbegin());
+    EXPECT_EQ(x, y);
   }
   {
-    std::string str_1 = "acdfbdeghi";
-    std::string str_2 = "acdfbdegh1";
-    EXPECT_EQ(std::mismatch(str_1.begin(), str_1.end(), str_2.begin()),
-              liuyunbin::mismatch(str_1.begin(), str_1.end(), str_2.begin()));
+    std::forward_list<int> forward_list_1 = {0, 1, 2, 3, 4};
+
+    auto x = std::mismatch(forward_list_1.cbegin(), forward_list_1.cend(),
+                           forward_list_1.cbegin());
+    auto y = liuyunbin::mismatch(forward_list_1.cbegin(), forward_list_1.cend(),
+                                 forward_list_1.cbegin());
+    EXPECT_EQ(x, y);
+  }
+
+  // 双向迭代器
+  {
+    std::list<int> list_1 = {0, 1, 2, 3, 4};
+    std::list<int> list_2 = {5, 6, 7, 8, 9};
+
+    auto x = std::mismatch(list_1.cbegin(), list_1.cend(), list_2.cbegin());
+    auto y =
+        liuyunbin::mismatch(list_1.cbegin(), list_1.cend(), list_2.cbegin());
+    EXPECT_EQ(x, y);
   }
   {
-    std::string str_1 = "acdfbdeghi";
-    std::string str_2 = "1cdfbdeghi";
-    EXPECT_EQ(std::mismatch(str_1.begin(), str_1.end(), str_2.begin()),
-              liuyunbin::mismatch(str_1.begin(), str_1.end(), str_2.begin()));
+    std::list<int> list_1 = {0, 1, 2, 3, 4};
+
+    auto x = std::mismatch(list_1.cbegin(), list_1.cend(), list_1.cbegin());
+    auto y =
+        liuyunbin::mismatch(list_1.cbegin(), list_1.cend(), list_1.cbegin());
+    EXPECT_EQ(x, y);
+  }
+
+  // 随机迭代器
+  {
+    std::vector<int> vector_1 = {0, 1, 2, 3, 4};
+    std::vector<int> vector_2 = {5, 6, 7, 8, 9};
+
+    auto x =
+        std::mismatch(vector_1.cbegin(), vector_1.cend(), vector_2.cbegin());
+    auto y = liuyunbin::mismatch(vector_1.cbegin(), vector_1.cend(),
+                                 vector_2.cbegin());
+    EXPECT_EQ(x, y);
+  }
+  {
+    std::vector<int> vector_1 = {0, 1, 2, 3, 4};
+
+    auto x =
+        std::mismatch(vector_1.cbegin(), vector_1.cend(), vector_1.cbegin());
+    auto y = liuyunbin::mismatch(vector_1.cbegin(), vector_1.cend(),
+                                 vector_1.cbegin());
+    EXPECT_EQ(x, y);
   }
 }
