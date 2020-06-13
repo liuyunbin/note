61f99677976ad105c94858a6395f670fb8d5d1bf
liuyunbin <yunbinliu@outlook.com>
2020-06-13 18:06:04 +0800

    使用 输入、前向、双向和随机迭代器测试 函数 move

--- cpp-standard-library/algorithm/move/move_unittest.cc
+++ cpp-standard-library/algorithm/move/move_unittest.cc
@@ -2,17 +2,58 @@
 #include "move.h"
 
 #include <algorithm>
+#include <forward_list>
+#include <iterator>
+#include <list>
+#include <sstream>
 #include <string>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_move) {
-  std::string str_from_1 = "1234";
-  std::string str_from_2 = "1234";
-  std::string str_to_1 = "abcd";
-  std::string str_to_2 = "abcd";
-  std::move(str_from_1.begin(), str_from_1.end(), str_to_1.begin());
-  liuyunbin::move(str_from_2.begin(), str_from_2.end(), str_to_2.begin());
-  EXPECT_EQ(str_from_1, str_from_2);
-  EXPECT_EQ(str_to_1, str_to_2);
+  // 输入迭代器
+  {
+    std::istringstream stream_1("1 2 3 4 5");
+    std::istringstream stream_2("1 2 3 4 5");
+    std::forward_list<int> forward_list_3 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_4 = {5, 6, 7, 8, 9};
+    std::move(std::istream_iterator<int>(stream_1),
+              std::istream_iterator<int>(), forward_list_3.begin());
+    liuyunbin::move(std::istream_iterator<int>(stream_2),
+                    std::istream_iterator<int>(), forward_list_4.begin());
+    EXPECT_EQ(forward_list_3, forward_list_4);
+  }
+
+  // 前向迭代器
+  {
+    std::forward_list<int> forward_list_1 = {1, 2, 3, 4, 5};
+    std::forward_list<int> forward_list_2 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_3 = {5, 6, 7, 8, 9};
+    std::move(forward_list_1.cbegin(), forward_list_1.cend(),
+              forward_list_2.begin());
+    liuyunbin::move(forward_list_1.cbegin(), forward_list_1.cend(),
+                    forward_list_3.begin());
+    EXPECT_EQ(forward_list_2, forward_list_3);
+  }
+
+  // 双向迭代器
+  {
+    std::list<int> list_1 = {1, 2, 3, 4, 5};
+    std::forward_list<int> forward_list_2 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_3 = {5, 6, 7, 8, 9};
+    std::move(list_1.cbegin(), list_1.cend(), forward_list_2.begin());
+    liuyunbin::move(list_1.cbegin(), list_1.cend(), forward_list_3.begin());
+    EXPECT_EQ(forward_list_2, forward_list_3);
+  }
+
+  // 随机迭代器
+  {
+    std::vector<int> vector_1 = {1, 2, 3, 4, 5};
+    std::forward_list<int> forward_list_2 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_3 = {5, 6, 7, 8, 9};
+    std::move(vector_1.cbegin(), vector_1.cend(), forward_list_2.begin());
+    liuyunbin::move(vector_1.cbegin(), vector_1.cend(), forward_list_3.begin());
+    EXPECT_EQ(forward_list_2, forward_list_3);
+  }
 }
