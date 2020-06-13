538526cf9be9210e6973639bc5ceb4c65c131ff3
liuyunbin <yunbinliu@outlook.com>
2020-06-13 15:03:53 +0800

    使用 前向、双向和随机迭代器测试 函数 is_sorted

--- cpp-standard-library/algorithm/is_sorted/is_sorted_unittest.cc
+++ cpp-standard-library/algorithm/is_sorted/is_sorted_unittest.cc
@@ -2,12 +2,57 @@
 #include "is_sorted.h"
 
 #include <algorithm>
+#include <cctype>
+#include <forward_list>
+#include <list>
+#include <numeric>
 #include <string>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_is_sorted) {
-  std::string str = "1234abcd";
-  EXPECT_EQ(std::is_sorted(str.begin(), str.end()),
-            liuyunbin::is_sorted(str.begin(), str.end()));
+  // 前向迭代器
+  {
+    std::forward_list<int> forward_list_1 = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+    auto x = std::is_sorted(forward_list_1.cbegin(), forward_list_1.cend());
+    auto y =
+        liuyunbin::is_sorted(forward_list_1.cbegin(), forward_list_1.cend());
+    EXPECT_EQ(x, y);
+  }
+  {
+    std::forward_list<int> forward_list_1 = {1, 2, 3, 4, 5};
+    auto x = std::is_sorted(forward_list_1.cbegin(), forward_list_1.cend());
+    auto y =
+        liuyunbin::is_sorted(forward_list_1.cbegin(), forward_list_1.cend());
+    EXPECT_EQ(x, y);
+  }
+
+  // 双向迭代器
+  {
+    std::list<int> list_1 = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+    auto x = std::is_sorted(list_1.cbegin(), list_1.cend());
+    auto y = liuyunbin::is_sorted(list_1.cbegin(), list_1.cend());
+    EXPECT_EQ(x, y);
+  }
+  {
+    std::list<int> list_1 = {1, 2, 3, 4, 5};
+    auto x = std::is_sorted(list_1.cbegin(), list_1.cend());
+    auto y = liuyunbin::is_sorted(list_1.cbegin(), list_1.cend());
+    EXPECT_EQ(x, y);
+  }
+
+  // 随机迭代器
+  {
+    std::vector<int> vector_1 = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+    auto x = std::is_sorted(vector_1.cbegin(), vector_1.cend());
+    auto y = liuyunbin::is_sorted(vector_1.cbegin(), vector_1.cend());
+    EXPECT_EQ(x, y);
+  }
+  {
+    std::vector<int> vector_1 = {1, 2, 3, 4, 5};
+    auto x = std::is_sorted(vector_1.cbegin(), vector_1.cend());
+    auto y = liuyunbin::is_sorted(vector_1.cbegin(), vector_1.cend());
+    EXPECT_EQ(x, y);
+  }
 }
