d2f24ca816faf2cadaac0a8820060f34dbc8ca79
liuyunbin <yunbinliu@outlook.com>
2020-06-13 13:13:51 +0800

    使用 前向、双向和随机迭代器测试 函数 generate_n

--- cpp-standard-library/algorithm/generate_n/generate_n_unittest.cc
+++ cpp-standard-library/algorithm/generate_n/generate_n_unittest.cc
@@ -2,19 +2,51 @@
 #include "generate_n.h"
 
 #include <algorithm>
-#include <string>
+#include <forward_list>
+#include <list>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_generate_n) {
-  std::string str_1 = "abcdefghi";
-  std::string str_2 = "abcdefghi";
+  // 输出迭代器 不好测试
 
-  auto f = []() { return '1'; };
+  // 前向迭代器
+  {
+    std::forward_list<int> forward_list_1 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_2 = {5, 6, 7, 8, 9};
 
-  const int n = 5;
+    auto f = []() { return 1; };
+    int n = 5;
 
-  std::generate_n(str_1.begin(), n, f);
-  liuyunbin::generate_n(str_2.begin(), n, f);
-  EXPECT_EQ(str_1, str_2);
+    std::generate_n(forward_list_1.begin(), n, f);
+    liuyunbin::generate_n(forward_list_2.begin(), n, f);
+    EXPECT_EQ(forward_list_1, forward_list_2);
+  }
+
+  // 双向迭代器
+  {
+    std::list<int> list_1 = {0, 1, 2, 3, 4};
+    std::list<int> list_2 = {5, 6, 7, 8, 9};
+
+    auto f = []() { return 1; };
+    int n = 5;
+
+    std::generate_n(list_1.begin(), n, f);
+    liuyunbin::generate_n(list_2.begin(), n, f);
+    EXPECT_EQ(list_1, list_2);
+  }
+
+  // 随机迭代器
+  {
+    std::vector<int> vector_1 = {0, 1, 2, 3, 4};
+    std::vector<int> vector_2 = {5, 6, 7, 8, 9};
+
+    auto f = []() { return 1; };
+    int n = 5;
+
+    std::generate_n(vector_1.begin(), n, f);
+    liuyunbin::generate_n(vector_2.begin(), n, f);
+    EXPECT_EQ(vector_1, vector_2);
+  }
 }
