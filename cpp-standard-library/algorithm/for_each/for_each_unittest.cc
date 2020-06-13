e5b1ebb8b0010df14c73907517a81b3f63b347d7
liuyunbin <yunbinliu@outlook.com>
2020-06-13 13:02:00 +0800

    使用 输入、前向、双向和随机迭代器测试 函数 for_each

--- cpp-standard-library/algorithm/for_each/for_each_unittest.cc
+++ cpp-standard-library/algorithm/for_each/for_each_unittest.cc
@@ -2,18 +2,75 @@
 #include "for_each.h"
 
 #include <algorithm>
-#include <cctype>
-#include <string>
+#include <forward_list>
+#include <iterator>
+#include <list>
+#include <sstream>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_for_each) {
-  std::string str_1 = "abcdefghi";
-  std::string str_2 = "abcdefghi";
+  // 输入迭代器
+  {
+    std::istringstream stream_1("1 2 3 4 5");
+    std::istringstream stream_2("1 2 3 4 5");
 
-  auto f = [](char &ch) { return ch = std::toupper(ch); };
+    std::vector<int> ve_1;
+    std::vector<int> ve_2;
 
-  std::for_each(str_1.begin(), str_1.end(), f);
-  liuyunbin::for_each(str_2.begin(), str_2.end(), f);
-  EXPECT_EQ(str_1, str_2);
+    auto f_1 = [&ve_1](const int &v) { return ve_1.push_back(v); };
+    auto f_2 = [&ve_2](const int &v) { return ve_2.push_back(v); };
+
+    std::for_each(std::istream_iterator<int>(stream_1),
+                  std::istream_iterator<int>(), f_1);
+    liuyunbin::for_each(std::istream_iterator<int>(stream_2),
+                        std::istream_iterator<int>(), f_2);
+    EXPECT_EQ(ve_1, ve_2);
+  }
+
+  // 前向迭代器
+  {
+    std::forward_list<int> forward_list_1 = {1, 2, 3, 4, 5};
+
+    std::vector<int> ve_1;
+    std::vector<int> ve_2;
+
+    auto f_1 = [&ve_1](const int &v) { return ve_1.push_back(v); };
+    auto f_2 = [&ve_2](const int &v) { return ve_2.push_back(v); };
+
+    std::for_each(forward_list_1.cbegin(), forward_list_1.cend(), f_1);
+    liuyunbin::for_each(forward_list_1.cbegin(), forward_list_1.cend(), f_2);
+    EXPECT_EQ(ve_1, ve_2);
+  }
+
+  // 双向迭代器
+  {
+    std::list<int> list_1 = {1, 2, 3, 4, 5};
+
+    std::vector<int> ve_1;
+    std::vector<int> ve_2;
+
+    auto f_1 = [&ve_1](const int &v) { return ve_1.push_back(v); };
+    auto f_2 = [&ve_2](const int &v) { return ve_2.push_back(v); };
+
+    std::for_each(list_1.cbegin(), list_1.cend(), f_1);
+    liuyunbin::for_each(list_1.cbegin(), list_1.cend(), f_2);
+    EXPECT_EQ(ve_1, ve_2);
+  }
+
+  // 随机迭代器
+  {
+    std::vector<int> vector_1 = {1, 2, 3, 4, 5};
+
+    std::vector<int> ve_1;
+    std::vector<int> ve_2;
+
+    auto f_1 = [&ve_1](const int &v) { return ve_1.push_back(v); };
+    auto f_2 = [&ve_2](const int &v) { return ve_2.push_back(v); };
+
+    std::for_each(vector_1.cbegin(), vector_1.cend(), f_1);
+    liuyunbin::for_each(vector_1.cbegin(), vector_1.cend(), f_2);
+    EXPECT_EQ(ve_1, ve_2);
+  }
 }
