17d9379071d45458c5e126096d888c17ec2fc08d
liuyunbin <yunbinliu@outlook.com>
2020-06-13 17:52:59 +0800

    使用 前向、双向和随机迭代器测试 函数 minmax_element

--- cpp-standard-library/algorithm/minmax_element/minmax_element_unittest.cc
+++ cpp-standard-library/algorithm/minmax_element/minmax_element_unittest.cc
@@ -2,12 +2,36 @@
 #include "minmax_element.h"
 
 #include <algorithm>
-#include <initializer_list>
+#include <forward_list>
+#include <list>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_minmax_element) {
-  std::initializer_list<int> li = {1, 2, 3, 4, 5};
-  EXPECT_EQ(std::minmax_element(li.begin(), li.end()),
-            liuyunbin::minmax_element(li.begin(), li.end()));
+  // 前向迭代器
+  {
+    std::forward_list<int> forward_list_1 = {1, 2, 8, 4, 5};
+    auto x =
+        std::minmax_element(forward_list_1.cbegin(), forward_list_1.cend());
+    auto y = liuyunbin::minmax_element(forward_list_1.cbegin(),
+                                       forward_list_1.cend());
+    EXPECT_EQ(x, y);
+  }
+
+  // 双向迭代器
+  {
+    std::list<int> list_1 = {1, 2, 8, 4, 5};
+    auto x = std::minmax_element(list_1.cbegin(), list_1.cend());
+    auto y = liuyunbin::minmax_element(list_1.cbegin(), list_1.cend());
+    EXPECT_EQ(x, y);
+  }
+
+  // 随机迭代器
+  {
+    std::vector<int> vector_1 = {1, 2, 8, 4, 5};
+    auto x = std::minmax_element(vector_1.cbegin(), vector_1.cend());
+    auto y = liuyunbin::minmax_element(vector_1.cbegin(), vector_1.cend());
+    EXPECT_EQ(x, y);
+  }
 }
