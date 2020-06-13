4e22c9ac839943e411e17aefeec68e39514bce31
liuyunbin <yunbinliu@outlook.com>
2020-06-13 17:56:48 +0800

    添加 C++14 说明

--- cpp-standard-library/algorithm/mismatch/mismatch.h
+++ cpp-standard-library/algorithm/mismatch/mismatch.h
@@ -28,6 +28,7 @@ std::pair<InputIt1, InputIt2> mismatch(InputIt1 first1, InputIt1 last1,
   return {first1, first2};
 }
 
+// C++14
 template <typename InputIt1, typename InputIt2>
 std::pair<InputIt1, InputIt2> mismatch(InputIt1 first1, InputIt1 last1,
                                        InputIt2 first2, InputIt2 last2) {
