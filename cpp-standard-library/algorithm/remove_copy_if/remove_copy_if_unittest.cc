0076e4aec44bd0df931dbb1ee1f9647397acf0ac
liuyunbin <yunbinliu@outlook.com>
2020-06-14 06:20:16 +0800

    使用 输入、前向、双向和随机迭代器测试 函数 remove_copy_if

--- cpp-standard-library/algorithm/remove_copy_if/remove_copy_if_unittest.cc
+++ cpp-standard-library/algorithm/remove_copy_if/remove_copy_if_unittest.cc
@@ -3,15 +3,141 @@
 
 #include <algorithm>
 #include <cctype>
+#include <forward_list>
+#include <iterator>
+#include <list>
+#include <sstream>
 #include <string>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_remove_copy_if) {
-  std::string str_1 = "12341234abcd";
-  std::string str_2 = "12341234abcd";
-  auto f = [](char ch) { return std::isdigit(ch) ? true : false; };
-  auto p1 = std::remove_copy_if(str_1.begin(), str_1.end(), str_1.begin(), f);
-  liuyunbin::remove_copy_if(str_2.begin(), str_2.end(), str_2.begin(), f);
-  EXPECT_TRUE(std::equal(str_1.begin(), p1, str_2.begin()));
+  // 输入迭代器
+  {
+    std::istringstream stream_1("1 2 3 4 5 1 2 3 4 5");
+    std::istringstream stream_2("1 2 3 4 5 1 2 3 4 5");
+
+    auto f = [](char ch) { return std::isdigit(ch) ? true : false; };
+    std::forward_list<int> forward_list_3 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    std::forward_list<int> forward_list_4 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    std::remove_copy_if(std::istream_iterator<int>(stream_1),
+                        std::istream_iterator<int>(), forward_list_3.begin(),
+                        f);
+    std::remove_copy_if(std::istream_iterator<int>(stream_2),
+                        std::istream_iterator<int>(), forward_list_4.begin(),
+                        f);
+    EXPECT_EQ(forward_list_3, forward_list_4);
+  }
+  {
+    std::istringstream stream_1("1 2 3 4 5 1 2 3 4 5");
+    std::istringstream stream_2("1 2 3 4 5 1 2 3 4 5");
+
+    auto f = [](char ch) { return std::isalpha(ch) ? true : false; };
+    std::forward_list<int> forward_list_3 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    std::forward_list<int> forward_list_4 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    std::remove_copy_if(std::istream_iterator<int>(stream_1),
+                        std::istream_iterator<int>(), forward_list_3.begin(),
+                        f);
+    std::remove_copy_if(std::istream_iterator<int>(stream_2),
+                        std::istream_iterator<int>(), forward_list_4.begin(),
+                        f);
+    EXPECT_EQ(forward_list_3, forward_list_4);
+  }
+
+  // 前向迭代器
+  {
+    std::forward_list<int> forward_list_1 = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+    std::forward_list<int> forward_list_2 = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+
+    auto f = [](const int &v) { return v < 3 ? true : false; };
+    std::forward_list<int> forward_list_3 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    std::forward_list<int> forward_list_4 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    std::remove_copy_if(forward_list_1.cbegin(), forward_list_1.cend(),
+                        forward_list_3.begin(), f);
+    std::remove_copy_if(forward_list_2.begin(), forward_list_2.end(),
+                        forward_list_4.begin(), f);
+
+    EXPECT_EQ(forward_list_3, forward_list_4);
+  }
+  {
+    std::forward_list<int> forward_list_1 = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+    std::forward_list<int> forward_list_2 = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+
+    auto f = [](const int &v) { return v > 10 ? true : false; };
+    std::forward_list<int> forward_list_3 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    std::forward_list<int> forward_list_4 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    std::remove_copy_if(forward_list_1.cbegin(), forward_list_1.cend(),
+                        forward_list_3.begin(), f);
+    std::remove_copy_if(forward_list_2.begin(), forward_list_2.end(),
+                        forward_list_4.begin(), f);
+
+    EXPECT_EQ(forward_list_3, forward_list_4);
+  }
+
+  // 双向迭代器
+  {
+    std::list<int> list_1 = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+    std::list<int> list_2 = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+
+    auto f = [](const int &v) { return v < 3 ? true : false; };
+    std::forward_list<int> forward_list_3 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    std::forward_list<int> forward_list_4 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    std::remove_copy_if(list_1.cbegin(), list_1.cend(), forward_list_3.begin(),
+                        f);
+    std::remove_copy_if(list_2.cbegin(), list_2.cend(), forward_list_4.begin(),
+                        f);
+
+    EXPECT_EQ(forward_list_3, forward_list_4);
+  }
+  {
+    std::list<int> list_1 = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+    std::list<int> list_2 = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+
+    auto f = [](const int &v) { return v > 10 ? true : false; };
+    std::forward_list<int> forward_list_3 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    std::forward_list<int> forward_list_4 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    std::remove_copy_if(list_1.cbegin(), list_1.cend(), forward_list_3.begin(),
+                        f);
+    std::remove_copy_if(list_2.cbegin(), list_2.cend(), forward_list_4.begin(),
+                        f);
+
+    EXPECT_EQ(forward_list_3, forward_list_4);
+  }
+
+  // 随机迭代器
+  {
+    std::vector<int> vector_1 = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+    std::vector<int> vector_2 = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+
+    auto f = [](const int &v) { return v < 3 ? true : false; };
+    std::forward_list<int> forward_list_3 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    std::forward_list<int> forward_list_4 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    std::remove_copy_if(vector_1.cbegin(), vector_1.cend(),
+                        forward_list_3.begin(), f);
+    std::remove_copy_if(vector_2.cbegin(), vector_2.cend(),
+                        forward_list_4.begin(), f);
+
+    EXPECT_EQ(forward_list_3, forward_list_4);
+  }
+  {
+    std::vector<int> vector_1 = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+    std::vector<int> vector_2 = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+
+    auto f = [](const int &v) { return v > 10 ? true : false; };
+    std::forward_list<int> forward_list_3 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    std::forward_list<int> forward_list_4 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    std::remove_copy_if(vector_1.cbegin(), vector_1.cend(),
+                        forward_list_3.begin(), f);
+    std::remove_copy_if(vector_2.cbegin(), vector_2.cend(),
+                        forward_list_4.begin(), f);
+
+    EXPECT_EQ(forward_list_3, forward_list_4);
+  }
 }
