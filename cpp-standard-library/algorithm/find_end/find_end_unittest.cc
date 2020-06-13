a7a9e9fadeba1bf69caf10e17346cf455cb09062
liuyunbin <yunbinliu@outlook.com>
2020-06-13 11:41:56 +0800

    使用 前向、双向和随机迭代器测试 函数 find_end

--- cpp-standard-library/algorithm/find_end/find_end_unittest.cc
+++ cpp-standard-library/algorithm/find_end/find_end_unittest.cc
@@ -1,42 +1,79 @@
 
 #include "find_end.h"
 
-#include <algorithm>
-#include <string>
+#include <forward_list>
+#include <list>
+#include <numeric>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_find_end) {
+  // 前向迭代器
   {
-    std::string str_1 = "abcdefghi";
-    std::string str_2 = "abcdefghi";
-    EXPECT_EQ(
-        std::find_end(str_1.begin(), str_1.end(), str_2.begin(), str_2.end()),
-        liuyunbin::find_end(str_1.begin(), str_1.end(), str_2.begin(),
-                            str_2.end()));
+    std::forward_list<int> forward_list_1 = {0, 1, 2, 3, 4, 0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_2 = {1, 2, 3};
+
+    auto x = std::find_end(forward_list_1.cbegin(), forward_list_1.cend(),
+                           forward_list_2.cbegin(), forward_list_2.cend());
+    auto y =
+        liuyunbin::find_end(forward_list_1.cbegin(), forward_list_1.cend(),
+                            forward_list_2.cbegin(), forward_list_2.cend());
+    EXPECT_EQ(x, y);
   }
   {
-    std::string str_1 = "abcdefghi";
-    std::string str_2 = "defghi";
-    EXPECT_EQ(
-        std::find_end(str_1.begin(), str_1.end(), str_2.begin(), str_2.end()),
-        liuyunbin::find_end(str_1.begin(), str_1.end(), str_2.begin(),
-                            str_2.end()));
+    std::forward_list<int> forward_list_1 = {0, 1, 2, 3, 4, 0, 2, 2, 3, 4};
+    std::forward_list<int> forward_list_2 = {1, 2, 4};
+
+    auto x = std::find_end(forward_list_1.cbegin(), forward_list_1.cend(),
+                           forward_list_2.cbegin(), forward_list_2.cend());
+    auto y =
+        liuyunbin::find_end(forward_list_1.cbegin(), forward_list_1.cend(),
+                            forward_list_2.cbegin(), forward_list_2.cend());
+    EXPECT_EQ(x, y);
+  }
+
+  // 双向迭代器
+  {
+    std::list<int> list_1 = {0, 1, 2, 3, 4, 0, 1, 2, 3, 4};
+    std::list<int> list_2 = {1, 2, 3};
+
+    auto x = std::find_end(list_1.cbegin(), list_1.cend(), list_2.cbegin(),
+                           list_2.cend());
+    auto y = liuyunbin::find_end(list_1.cbegin(), list_1.cend(),
+                                 list_2.cbegin(), list_2.cend());
+    EXPECT_EQ(x, y);
   }
   {
-    std::string str_1 = "abcdefghi";
-    std::string str_2 = "123456";
-    EXPECT_EQ(
-        std::find_end(str_1.begin(), str_1.end(), str_2.begin(), str_2.end()),
-        liuyunbin::find_end(str_1.begin(), str_1.end(), str_2.begin(),
-                            str_2.end()));
+    std::list<int> list_1 = {0, 1, 2, 3, 4, 0, 1, 2, 3, 4};
+    std::list<int> list_2 = {1, 2, 4};
+
+    auto x = std::find_end(list_1.cbegin(), list_1.cend(), list_2.cbegin(),
+                           list_2.cend());
+    auto y = liuyunbin::find_end(list_1.cbegin(), list_1.cend(),
+                                 list_2.cbegin(), list_2.cend());
+    EXPECT_EQ(x, y);
+  }
+
+  // 随机迭代器
+  {
+    std::vector<int> vector_1 = {0, 1, 2, 3, 4, 0, 1, 2, 3, 4};
+    std::vector<int> vector_2 = {1, 2, 3};
+
+    auto x = std::find_end(vector_1.cbegin(), vector_1.cend(),
+                           vector_2.cbegin(), vector_2.cend());
+    auto y = liuyunbin::find_end(vector_1.cbegin(), vector_1.cend(),
+                                 vector_2.cbegin(), vector_2.cend());
+    EXPECT_EQ(x, y);
   }
   {
-    std::string str_1 = "abcdabcd";
-    std::string str_2 = "abcd";
-    EXPECT_EQ(
-        std::find_end(str_1.begin(), str_1.end(), str_2.begin(), str_2.end()),
-        liuyunbin::find_end(str_1.begin(), str_1.end(), str_2.begin(),
-                            str_2.end()));
+    std::vector<int> vector_1 = {0, 1, 2, 3, 4, 0, 1, 2, 3, 4};
+    std::vector<int> vector_2 = {1, 2, 3};
+
+    auto x = std::find_end(vector_1.cbegin(), vector_1.cend(),
+                           vector_2.cbegin(), vector_2.cend());
+    auto y = liuyunbin::find_end(vector_1.cbegin(), vector_1.cend(),
+                                 vector_2.cbegin(), vector_2.cend());
+    EXPECT_EQ(x, y);
   }
 }
