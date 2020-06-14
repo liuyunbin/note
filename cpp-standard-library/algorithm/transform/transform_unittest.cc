43d9a67836ebd9436cf43f664e41b669b2d69b15
liuyunbin <yunbinliu@outlook.com>
2020-06-14 15:30:57 +0800

    使用 输入、前向、双向和随机迭代器测试 函数 transform

--- cpp-standard-library/algorithm/transform/transform_unittest.cc
+++ cpp-standard-library/algorithm/transform/transform_unittest.cc
@@ -3,26 +3,76 @@
 
 #include <algorithm>
 #include <cctype>
-#include <string>
+#include <forward_list>
+#include <iterator>
+#include <list>
+#include <sstream>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_transform) {
+  // 输入迭代器
   {
-    std::string str_1 = "1234abcdyy";
-    std::string str_2 = "1234abcdyy";
-    auto f = [](char ch) { return std::toupper(ch); };
-    std::transform(str_1.begin(), str_1.end(), str_1.begin(), f);
-    liuyunbin::transform(str_2.begin(), str_2.end(), str_2.begin(), f);
-    EXPECT_EQ(str_1, str_2);
+    std::istringstream stream_1("1 2 3 4 5");
+    std::istringstream stream_2("1 2 3 4 5");
+
+    std::forward_list<int> forward_list_3 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_4 = {5, 6, 2, 3, 4};
+
+    auto f = [](int v) { return v * 3; };
+
+    std::transform(std::istream_iterator<int>(stream_1),
+                   std::istream_iterator<int>(), forward_list_3.begin(), f);
+    liuyunbin::transform(std::istream_iterator<int>(stream_2),
+                         std::istream_iterator<int>(), forward_list_4.begin(),
+                         f);
+    EXPECT_EQ(forward_list_3, forward_list_4);
   }
+
+  // 前向迭代器
   {
-    std::string str_1 = "1234abcdyy";
-    std::string str_2 = "1234abcdyy";
-    auto f = [](char ch1, char ch2) { return std::toupper(ch1); };
-    std::transform(str_1.begin(), str_1.end(), str_1.begin(), str_1.begin(), f);
-    liuyunbin::transform(str_2.begin(), str_2.end(), str_2.begin(),
-                         str_2.begin(), f);
-    EXPECT_EQ(str_1, str_2);
+    std::forward_list<int> forward_list_1 = {1, 2, 3, 4, 5};
+    std::forward_list<int> forward_list_2 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_3 = {5, 6, 2, 3, 4};
+
+    auto f = [](int v) { return v * 3; };
+
+    std::transform(forward_list_1.cbegin(), forward_list_1.cend(),
+                   forward_list_2.begin(), f);
+    liuyunbin::transform(forward_list_1.cbegin(), forward_list_1.cend(),
+                         forward_list_3.begin(), f);
+    EXPECT_EQ(forward_list_2, forward_list_3);
+  }
+
+  // 双向迭代器
+  {
+    std::list<int> list_1 = {1, 2, 3, 4, 5};
+
+    std::forward_list<int> forward_list_2 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_3 = {5, 6, 2, 3, 4};
+
+    auto f = [](int v) { return v * 3; };
+
+    std::transform(list_1.cbegin(), list_1.cend(), forward_list_2.begin(), f);
+    liuyunbin::transform(list_1.cbegin(), list_1.cend(), forward_list_3.begin(),
+                         f);
+    EXPECT_EQ(forward_list_2, forward_list_3);
+  }
+
+  // 随机迭代器
+  {
+    std::vector<int> vector_1 = {1, 2, 3, 4, 5};
+
+    std::forward_list<int> forward_list_2 = {0, 1, 2, 3, 4};
+    std::forward_list<int> forward_list_3 = {5, 6, 2, 3, 4};
+
+    auto f = [](int v) { return v * 3; };
+
+    std::transform(vector_1.cbegin(), vector_1.cend(), forward_list_2.begin(),
+                   f);
+    liuyunbin::transform(vector_1.cbegin(), vector_1.cend(),
+                         forward_list_3.begin(), f);
+    EXPECT_EQ(forward_list_2, forward_list_3);
   }
 }
