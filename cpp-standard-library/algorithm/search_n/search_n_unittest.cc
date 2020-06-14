ef2906a8d97987d1c948eae924fae1d30e6efb6f
liuyunbin <yunbinliu@outlook.com>
2020-06-14 08:06:52 +0800

    使用 前向、双向和随机迭代器测试 函数 search_n

--- cpp-standard-library/algorithm/search_n/search_n_unittest.cc
+++ cpp-standard-library/algorithm/search_n/search_n_unittest.cc
@@ -1,38 +1,75 @@
 
 #include "search_n.h"
 
-#include <algorithm>
-#include <string>
+#include <forward_list>
+#include <list>
+#include <numeric>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_search_n) {
+  // 前向迭代器
   {
-    std::string str = "abcabcdaa";
-    const int n = 1;
-    const char ch = 'a';
-    EXPECT_EQ(std::search_n(str.begin(), str.end(), n, ch),
-              liuyunbin::search_n(str.begin(), str.end(), n, ch));
+    std::forward_list<int> forward_list_1 = {0, 1, 2, 3, 4};
+
+    int n = 2;
+    int v = 3;
+    auto x =
+        std::search_n(forward_list_1.cbegin(), forward_list_1.cend(), n, v);
+    auto y = liuyunbin::search_n(forward_list_1.cbegin(), forward_list_1.cend(),
+                                 n, v);
+    EXPECT_EQ(x, y);
   }
   {
-    std::string str = "abcabcdaa";
-    const int n = 2;
-    const char ch = 'a';
-    EXPECT_EQ(std::search_n(str.begin(), str.end(), n, ch),
-              liuyunbin::search_n(str.begin(), str.end(), n, ch));
+    std::forward_list<int> forward_list_1 = {0, 1, 3, 3, 4};
+
+    int n = 2;
+    int v = 3;
+    auto x =
+        std::search_n(forward_list_1.cbegin(), forward_list_1.cend(), n, v);
+    auto y = liuyunbin::search_n(forward_list_1.cbegin(), forward_list_1.cend(),
+                                 n, v);
+    EXPECT_EQ(x, y);
+  }
+
+  // 双向迭代器
+  {
+    std::list<int> list_1 = {0, 1, 2, 3, 4};
+
+    int n = 2;
+    int v = 3;
+    auto x = std::search_n(list_1.cbegin(), list_1.cend(), n, v);
+    auto y = liuyunbin::search_n(list_1.cbegin(), list_1.cend(), n, v);
+    EXPECT_EQ(x, y);
   }
   {
-    std::string str = "abcabcdaa";
-    const int n = 3;
-    const char ch = 'a';
-    EXPECT_EQ(std::search_n(str.begin(), str.end(), n, ch),
-              liuyunbin::search_n(str.begin(), str.end(), n, ch));
+    std::list<int> list_1 = {0, 1, 3, 3, 4};
+
+    int n = 2;
+    int v = 3;
+    auto x = std::search_n(list_1.cbegin(), list_1.cend(), n, v);
+    auto y = liuyunbin::search_n(list_1.cbegin(), list_1.cend(), n, v);
+    EXPECT_EQ(x, y);
+  }
+
+  // 随机迭代器
+  {
+    std::vector<int> vector_1 = {0, 1, 2, 3, 4};
+
+    int n = 2;
+    int v = 3;
+    auto x = std::search_n(vector_1.cbegin(), vector_1.cend(), n, v);
+    auto y = liuyunbin::search_n(vector_1.cbegin(), vector_1.cend(), n, v);
+    EXPECT_EQ(x, y);
   }
   {
-    std::string str = "abcabcdaa";
-    const int n = 1;
-    const char ch = '1';
-    EXPECT_EQ(std::search_n(str.begin(), str.end(), n, ch),
-              liuyunbin::search_n(str.begin(), str.end(), n, ch));
+    std::vector<int> vector_1 = {0, 1, 3, 3, 4};
+
+    int n = 2;
+    int v = 3;
+    auto x = std::search_n(vector_1.cbegin(), vector_1.cend(), n, v);
+    auto y = liuyunbin::search_n(vector_1.cbegin(), vector_1.cend(), n, v);
+    EXPECT_EQ(x, y);
   }
 }
