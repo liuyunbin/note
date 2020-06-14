1a7276ea19e8e979d519e28fccc4d6135af1f8dd
liuyunbin <yunbinliu@outlook.com>
2020-06-14 15:53:46 +0800

    使用 输入、前向、双向和随机迭代器测试 函数 unique_copy

--- cpp-standard-library/algorithm/unique_copy/unique_copy_unittest.cc
+++ cpp-standard-library/algorithm/unique_copy/unique_copy_unittest.cc
@@ -2,14 +2,73 @@
 #include "unique_copy.h"
 
 #include <algorithm>
+#include <forward_list>
+#include <iterator>
+#include <list>
+#include <sstream>
 #include <string>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_unique_copy) {
-  std::string str_1 = "12341234abcd";
-  std::string str_2 = "12341234abcd";
-  auto p1 = std::unique_copy(str_1.begin(), str_1.end(), str_1.begin());
-  liuyunbin::unique_copy(str_2.begin(), str_2.end(), str_2.begin());
-  EXPECT_TRUE(std::equal(str_1.begin(), p1, str_2.begin()));
+  // 输入迭代器
+  {
+    std::istringstream stream_1("1 1 2 2 3 4 5");
+    std::istringstream stream_2("1 1 2 2 3 4 5");
+
+    std::forward_list<int> forward_list_3 = {0, 0, 0, 0, 0, 0};
+    std::forward_list<int> forward_list_4 = {0, 0, 0, 0, 0, 0};
+
+    std::unique_copy(std::istream_iterator<int>(stream_1),
+                     std::istream_iterator<int>(), forward_list_3.begin());
+    liuyunbin::unique_copy(std::istream_iterator<int>(stream_2),
+                           std::istream_iterator<int>(),
+                           forward_list_4.begin());
+
+    EXPECT_EQ(forward_list_3, forward_list_4);
+  }
+
+  // 前向迭代器
+  {
+    std::forward_list<int> forward_list_1 = {1, 1, 2, 2, 3, 4, 5};
+    std::forward_list<int> forward_list_2 = {0, 0, 0, 0, 0, 0};
+    std::forward_list<int> forward_list_3 = {0, 0, 0, 0, 0, 0};
+
+    std::unique_copy(forward_list_1.cbegin(), forward_list_1.cend(),
+                     forward_list_2.begin());
+    liuyunbin::unique_copy(forward_list_1.cbegin(), forward_list_1.cend(),
+                           forward_list_3.begin());
+
+    EXPECT_EQ(forward_list_2, forward_list_3);
+  }
+
+  // 双向迭代器
+  {
+    std::list<int> list_1 = {1, 1, 2, 2, 3, 4, 5};
+
+    std::forward_list<int> forward_list_2 = {0, 0, 0, 0, 0, 0};
+    std::forward_list<int> forward_list_3 = {0, 0, 0, 0, 0, 0};
+
+    std::unique_copy(list_1.cbegin(), list_1.cend(), forward_list_2.begin());
+    liuyunbin::unique_copy(list_1.cbegin(), list_1.cend(),
+                           forward_list_3.begin());
+
+    EXPECT_EQ(forward_list_2, forward_list_3);
+  }
+
+  // 随机迭代器
+  {
+    std::vector<int> vector_1 = {1, 2, 3, 4, 5};
+
+    std::forward_list<int> forward_list_2 = {0, 0, 0, 0, 0, 0};
+    std::forward_list<int> forward_list_3 = {0, 0, 0, 0, 0, 0};
+
+    std::unique_copy(vector_1.cbegin(), vector_1.cend(),
+                     forward_list_2.begin());
+    liuyunbin::unique_copy(vector_1.cbegin(), vector_1.cend(),
+                           forward_list_3.begin());
+
+    EXPECT_EQ(forward_list_2, forward_list_3);
+  }
 }
