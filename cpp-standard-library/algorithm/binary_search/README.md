2f6e2170d58e76427201f16381d95d985cd47f93
liuyunbin <yunbinliu@outlook.com>
2020-06-14 18:46:31 +0800

    修改 binary_search 的迭代器要求为 前向迭代器

--- cpp-standard-library/algorithm/binary_search/README.md
+++ cpp-standard-library/algorithm/binary_search/README.md
@@ -1,18 +1,18 @@
 
 #### 函数原型：
 ```
-template <typename InputIt, typename T, typename Compare>
-bool binary_search(InputIt first, InputIt last, const T &value, Compare comp);
+template <typename ForwardIt, typename T, typename Compare>
+bool binary_search(ForwardIt first, ForwardIt last, const T &value, Compare comp);
 
-template <typename InputIt, typename T>
-bool binary_search(InputIt first, InputIt last, const T &value);
+template <typename ForwardIt, typename T>
+bool binary_search(ForwardIt first, ForwardIt last, const T &value);
 ```
 
 #### 函数说明：
 * 该函数用于判断指有序区间内是否存在元素 value
 
 #### 模板参数要求：
-* C++11 要求至少是前向迭代器，但我觉得，只要满足条件，输入迭代器也可以
+* 至少是前向迭代器
 
 详细内容见 函数实现及其注释
 
--- cpp-standard-library/algorithm/binary_search/binary_search_unittest.cc
+++ cpp-standard-library/algorithm/binary_search/binary_search_unittest.cc
@@ -4,58 +4,13 @@
 #include <algorithm>
 #include <cctype>
 #include <forward_list>
-#include <iterator>
 #include <list>
 #include <numeric>
-#include <sstream>
-#include <string>
 #include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_binary_search) {
-  // 输入迭代器
-  {
-    std::istringstream stream_1("1 2 3 4 5 6 7 8 9");
-    std::istringstream stream_2("1 2 3 4 5 6 7 8 9");
-    int v = 5;
-    auto x = std::binary_search(std::istream_iterator<int>(stream_1),
-                                std::istream_iterator<int>(), v);
-    auto y = liuyunbin::binary_search(std::istream_iterator<int>(stream_2),
-                                      std::istream_iterator<int>(), v);
-    EXPECT_EQ(x, y);
-  }
-  {
-    std::istringstream stream_1("1 2 3 4 6 7 8 9");
-    std::istringstream stream_2("1 2 3 4 6 7 8 9");
-    int v = 5;
-    auto x = std::binary_search(std::istream_iterator<int>(stream_1),
-                                std::istream_iterator<int>(), v);
-    auto y = liuyunbin::binary_search(std::istream_iterator<int>(stream_2),
-                                      std::istream_iterator<int>(), v);
-    EXPECT_EQ(x, y);
-  }
-  {
-    std::istringstream stream_1("1 2 3 4 5 6 7 8 9");
-    std::istringstream stream_2("1 2 3 4 5 6 7 8 9");
-    char ch = '5';
-    auto x = std::binary_search(std::istream_iterator<char>(stream_1),
-                                std::istream_iterator<char>(), ch);
-    auto y = liuyunbin::binary_search(std::istream_iterator<char>(stream_2),
-                                      std::istream_iterator<char>(), ch);
-    EXPECT_EQ(x, y);
-  }
-  {
-    std::istringstream stream_1("1 2 3 4 6 7 8 9");
-    std::istringstream stream_2("1 2 3 4 6 7 8 9");
-    char ch = '5';
-    auto x = std::binary_search(std::istream_iterator<char>(stream_1),
-                                std::istream_iterator<char>(), ch);
-    auto y = liuyunbin::binary_search(std::istream_iterator<char>(stream_2),
-                                      std::istream_iterator<char>(), ch);
-    EXPECT_EQ(x, y);
-  }
-
   // 前向迭代器
   {
     std::forward_list<int> forward_list_1 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
