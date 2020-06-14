1b41624a468c0d82945a5bb80a150e481f0cdb80
liuyunbin <yunbinliu@outlook.com>
2020-06-14 15:03:49 +0800

    使用 前向、双向和随机迭代器测试 函数 stable_partition

--- cpp-standard-library/algorithm/stable_partition/stable_partition_unittest.cc
+++ cpp-standard-library/algorithm/stable_partition/stable_partition_unittest.cc
@@ -4,40 +4,71 @@
 #include <algorithm>
 #include <cctype>
 #include <forward_list>
+#include <iterator>
+#include <list>
+#include <numeric>
+#include <sstream>
 #include <string>
 #include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_stable_partition) {
+  // 前向迭代器
   {
-    std::vector<std::pair<char, std::string>> ve1 = {
+    std::forward_list<std::pair<char, std::string>> forward_list_1 = {
         {'1', "100"}, {'2', "102"}, {'3', "103"}, {'4', "104"}, {'a', "105"},
         {'b', "106"}, {'c', "107"}, {'d', "108"}, {'1', "109"}, {'2', "110"},
         {'e', "111"}, {'f', "112"}, {'g', "113"}, {'1', "114"}, {'2', "115"},
     };
-    std::vector<std::pair<char, std::string>> ve2(ve1);
+    std::forward_list<std::pair<char, std::string>> forward_list_2(
+        forward_list_1);
 
     auto f = [](const std::pair<char, std::string> &item) {
       return std::isdigit(item.first) ? true : false;
     };
-    std::stable_partition(ve1.begin(), ve1.end(), f);
-    liuyunbin::stable_partition(ve2.begin(), ve2.end(), f);
-    EXPECT_EQ(ve1, ve2);
+
+    std::stable_partition(forward_list_1.begin(), forward_list_1.end(), f);
+    liuyunbin::stable_partition(forward_list_2.begin(), forward_list_2.end(),
+                                f);
+    EXPECT_EQ(forward_list_1, forward_list_2);
   }
+
+  // 双向迭代器
   {
-    std::forward_list<std::pair<char, std::string>> ve1 = {
+    std::list<std::pair<char, std::string>> list_1 = {
         {'1', "100"}, {'2', "102"}, {'3', "103"}, {'4', "104"}, {'a', "105"},
         {'b', "106"}, {'c', "107"}, {'d', "108"}, {'1', "109"}, {'2', "110"},
         {'e', "111"}, {'f', "112"}, {'g', "113"}, {'1', "114"}, {'2', "115"},
     };
-    std::forward_list<std::pair<char, std::string>> ve2(ve1);
+    std::list<std::pair<char, std::string>> list_2(list_1);
 
     auto f = [](const std::pair<char, std::string> &item) {
       return std::isdigit(item.first) ? true : false;
     };
-    std::stable_partition(ve1.begin(), ve1.end(), f);
-    liuyunbin::stable_partition(ve2.begin(), ve2.end(), f);
-    EXPECT_EQ(ve1, ve2);
+
+    std::stable_partition(list_1.begin(), list_1.end(), f);
+    liuyunbin::stable_partition(list_2.begin(), list_2.end(), f);
+
+    EXPECT_EQ(list_1, list_2);
+  }
+
+  // 随机迭代器
+  {
+    std::vector<std::pair<char, std::string>> vector_1 = {
+        {'1', "100"}, {'2', "102"}, {'3', "103"}, {'4', "104"}, {'a', "105"},
+        {'b', "106"}, {'c', "107"}, {'d', "108"}, {'1', "109"}, {'2', "110"},
+        {'e', "111"}, {'f', "112"}, {'g', "113"}, {'1', "114"}, {'2', "115"},
+    };
+    std::vector<std::pair<char, std::string>> vector_2(vector_1);
+
+    auto f = [](const std::pair<char, std::string> &item) {
+      return std::isdigit(item.first) ? true : false;
+    };
+
+    std::stable_partition(vector_1.begin(), vector_1.end(), f);
+    liuyunbin::stable_partition(vector_2.begin(), vector_2.end(), f);
+
+    EXPECT_EQ(vector_1, vector_2);
   }
 }
