a19d0d6c73775662803e160cc6e2093e5474fcbc
liuyunbin <yunbinliu@outlook.com>
2020-06-14 15:07:02 +0800

    使用 随机迭代器 测试 函数 stable_sort

--- cpp-standard-library/algorithm/stable_sort/stable_sort_unittest.cc
+++ cpp-standard-library/algorithm/stable_sort/stable_sort_unittest.cc
@@ -3,13 +3,20 @@
 
 #include <algorithm>
 #include <string>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 TEST(test_algorithm, test_stable_sort) {
-  std::string str_1 = "998653";
-  std::string str_2 = "998653";
-  std::stable_sort(str_1.begin(), str_1.end());
-  liuyunbin::stable_sort(str_2.begin(), str_2.end());
-  EXPECT_EQ(str_1, str_2);
+  // 随机迭代器
+  std::vector<std::pair<char, std::string>> vector_1 = {
+      {'1', "100"}, {'2', "102"}, {'3', "103"}, {'4', "104"}, {'a', "105"},
+      {'b', "106"}, {'c', "107"}, {'d', "108"}, {'1', "109"}, {'2', "110"},
+      {'e', "111"}, {'f', "112"}, {'g', "113"}, {'1', "114"}, {'2', "115"},
+  };
+  std::vector<std::pair<char, std::string>> vector_2(vector_1);
+
+  std::stable_sort(vector_1.begin(), vector_1.end());
+  liuyunbin::stable_sort(vector_2.begin(), vector_2.end());
+  EXPECT_EQ(vector_1, vector_2);
 }
