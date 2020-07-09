7ba08f0ca9490bd07116cb0d8aef2f8ec4b34cf4
liuyunbin <yunbinliu@outlook.com>
2020-07-09 08:04:39 +0800

    添加 安装 protobuf

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -12,5 +12,9 @@ sudo apt install libevent-dev -y
 
 sudo apt install libgoogle-glog-dev -y
 
+sudo apt install libprotobuf-dev -y
+
+sudo apt install protobuf-compiler -y
+
 ./scripts/install-gtest-for-ubuntu-20.04.sh
 
