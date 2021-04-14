37444766f5d4249077ae238d2a7a2b15e15a385c
liuyunbin <yunbinliu@outlook.com>
2021-04-14 21:55:53 +0800

    调整目录结构

---
+++ code/test.cc
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+  for (;;) {
+
+  }
+  return 0;
+}
