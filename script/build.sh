530790f381a969943be9260a88a5a6ab628a33b3
刘云宾 <yunbinliu@upchina.com>
2021-07-16 14:08:20 +0800

    调整 libevent 的编译内容

--- script/build.sh
+++ script/build.sh
@@ -6,8 +6,12 @@ mkdir -p build
 
 cd build
 
-cmake .. -DEVENT__DISABLE_OPENSSL=ON \
-         -DEVENT__LIBRARY_TYPE=STATIC
+cmake .. \
+         -DEVENT__DISABLE_OPENSSL=ON \
+         -DEVENT__LIBRARY_TYPE=STATIC \
+         -DEVENT__DISABLE_SAMPLES=ON \
+         -DEVENT__DISABLE_TESTS=ON \
+         -DEVENT__DISABLE_BENCHMARK=ON
 
 make
 
