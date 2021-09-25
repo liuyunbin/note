497303b72bd9ebc5b38ab8a03615df174f0d7360
liuyunbin <yunbinliu@outlook.com>
2021-09-25 13:32:41 +0800

    添加安装依赖

--- script/install-dependency.sh
+++ script/install-dependency.sh
@@ -4,20 +4,26 @@ set -ev
 
 sudo apt install cmake -y
 
-sudo apt install libreadline-dev -y
-
-sudo apt install libncurses5-dev libncursesw5-dev -y
+sudo apt install g++ -y
 
 sudo apt install libevent-dev -y
 
 sudo apt install libgoogle-glog-dev -y
 
+sudo apt install libgtest-dev -y
+
+sudo apt install libncurses5-dev libncursesw5-dev -y
+
 sudo apt install libprotobuf-dev -y
+
+sudo apt install libreadline-dev -y
 
 sudo apt install protobuf-compiler -y
 
+sudo apt install vim -y
+
 #./third-party/googletest/install-googletest.sh
 
-./third-party/openssl/install-openssl.sh
+#./third-party/openssl/install-openssl.sh
 
 #./third-party/libevent/install-libevent.sh
