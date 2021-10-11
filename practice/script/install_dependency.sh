b4e8d8abc664ac3efdc06720e9383049faddc773
liuyunbin <yunbinliu@outlook.com>
2021-10-11 13:17:03 +0800

    practice 单独编译

---
+++ practice/script/install_dependency.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -ev
+
+sudo apt install clang-format -y
+
+sudo apt install cmake -y
+
+sudo apt install g++ -y
+
+sudo apt install icdiff -y
+
+sudo apt install libevent-dev -y
+
+sudo apt install libgoogle-glog-dev -y
+
+sudo apt install libgtest-dev -y
+
+sudo apt install libncurses5-dev libncursesw5-dev -y
+
+sudo apt install libprotobuf-dev -y
+
+sudo apt install libreadline-dev -y
+
+sudo apt install protobuf-compiler -y
+
+sudo apt install vim -y
+
