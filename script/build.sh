f03f77e2429b931325526d94e505e91ba1a27fa6
liuyunbin <yunbinliu@outlook.com>
2021-10-16 18:17:35 +0800

    update cmake

--- script/build.sh
+++ script/build.sh
@@ -8,12 +8,7 @@ mkdir -p build
 
 cd build
 
-cmake .. \
-         -DEVENT__DISABLE_OPENSSL=ON \
-         -DEVENT__LIBRARY_TYPE=STATIC \
-         -DEVENT__DISABLE_SAMPLES=ON \
-         -DEVENT__DISABLE_TESTS=ON \
-         -DEVENT__DISABLE_BENCHMARK=ON
+cmake ..
 
 make
 
