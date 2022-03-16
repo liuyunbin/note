c63ff67a6671255ce13e8f028795663a5e2a34ef
liuyunbin <yunbinliu@outlook.com>
2022-03-16 17:19:36 +0800

    调整目录结构

---
+++ bash/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -u          # 使用未初始化的变量报错
+set -e          # 只要发生错误就退出
+set -o pipefail # 只要管道发生错误就退出
+set -E          # 即使意外退出, 也会执行 trap 命令
+
+cd cpp
+
+mkdir -p build
+
+cd build
+
+cmake ..
+
+make
+
+make test
+
+cd ../..
+
+mv cpp/bin .
+
