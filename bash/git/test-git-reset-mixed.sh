7872eba3d04c3eedf93071973a9affd75029d230
liuyunbin <yunbinliu@outlook.com>
2022-03-25 10:51:28 +0800

    add git reset test script

---
+++ bash/git/test-git-reset-mixed.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -u          # 使用未初始化的变量报错
+set -e          # 只要发生错误就退出
+set -o pipefail # 只要管道发生错误就退出
+set -E          # 即使意外退出, 也会执行 trap 命令
+
+rm -rf test
+mkdir -p test && cd test
+
+git init
+
+echo a > 1.txt
+git add 1.txt
+git commit -m "A"
+
+echo b > 1.txt
+git add 1.txt
+git commit -m "B"
+
+echo c > 1.txt
+git add 1.txt
+git commit -m "C"
+
+echo d > 1.txt
+git add 1.txt
+git commit -m "D"
+
+git reset --mixed HEAD~2
+
+git status
+
+cd ..
+
+rm -rf test
