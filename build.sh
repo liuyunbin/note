5c14ab83a1a6a1ce6989cd712fb9cfebf8cd9e23
liuyunbin <yunbinliu@upchina.com>
2022-09-19 18:06:50 +0800

    use make install

--- build.sh
+++ build.sh
@@ -5,14 +5,9 @@ set -o pipefail # 只要管道发生错误就退出
 set -E          # 即使意外退出, 也会执行 trap 命令
 
 mkdir -p build
-
 cd build
-
-cmake ..
-
-make
-
-make test
-
-cd ..
+cmake -DCMAKE_INSTALL_PREFIX=obj .. # 安装前缀
+make                                # 编译
+make test                           # 测试
+make install                        # 安装
 
