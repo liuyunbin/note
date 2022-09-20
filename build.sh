472d2eca4f5c102eb422b4fc091b0e0dd529f1f3
liuyunbin <yunbinliu@upchina.com>
2022-09-20 12:08:46 +0800

    format build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,7 @@ set -E          # 即使意外退出, 也会执行 trap 命令
 
 mkdir -p build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=obj .. # 安装前缀
-make                                # 编译
-make test                           # 测试
-make install                        # 安装
+cmake ..
+make
+make test
 
