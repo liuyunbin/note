6542df4d93c2fa4cbf1fc07830fb0d93a36ed0d4
liuyunbin <yunbinliu@outlook.com>
2022-10-15 19:06:20 +0800

    修改构建脚本目录

--- note/build.sh
+++ note/build.sh
@@ -3,9 +3,9 @@ set -o nounset  # 使用未初始化的变量报错
 set -o errexit  # 只要发生错误就退出
 set -o pipefail # 只要管道发生错误就退出
 
-mkdir -p build
-cd build
-cmake ..
+mkdir -p ../build
+cd ../build
+cmake ../note
 make
 make test
 
