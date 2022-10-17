d4b2edcdc503a26bf35fb40bfc1e190f5602d069
liuyunbin <yunbinliu@upchina.com>
2022-10-17 18:29:02 +0800

    update alias p

--- note/tool.sh
+++ note/tool.sh
@@ -17,28 +17,57 @@ function log_erro() {
 }
 
 function do_ps_sum() {
-    if [[ $# == 1 ]]; then
-        argv="-e"
+    if [[ $# == 0 ]]; then
+        local key=ruser
+        local argv="-e"
     else
-        argv="-p $2"
+        local key=comm
+        local argv="-p $1"
     fi
-    printf "%16s%10s%10s%10s%10s\n" "KEY" "%CPU" "%MEM" "RSZ(KB)" "NLWP"
-    ps --no-header -o $1,pcpu,pmem,rsz,nlwp $argv | awk '
+
+    printf "%16s%10s%10s%10s%10s\n" "KEY" "NLWP" "%CPU" "%MEM" "RSZ(KB)"
+    ps --no-header -o $key,nlwp,pcpu,pmem,rsz $argv | awk '
         {
-             cpu[$1] += $2
-             mem[$1] += $3
-             rsz[$1] += $4
-            nlwp[$1] += $5
+            nlwp[$1] += $2
+             cpu[$1] += $3
+             mem[$1] += $4
+             rsz[$1] += $5
         }
         END {
             for (key in cpu)
-                printf "%16s%10s%10s%10s%10s\n",key,cpu[key],mem[key],rsz[key],nlwp[key]
-        }' | sort -k 4 -h
+                printf "%16s%10s%10s%10s%10s\n",key,nlwp[key],cpu[key],mem[key],rsz[key]
+        }' | sort -k 5 -h
+}
+
+function do_ps_comm() {
+    if [[ $# == 1 ]]; then
+        local argv="-e"
+    else
+        local argv="-p $2"
+    fi
+
+    ps -o comm,pid,ppid,ruser,pcpu,pmem,rsz,nlwp,state,etime:13,etimes:10 --sort=-$1 $argv | awk '
+        BEGIN {
+            getline
+            printf "%s %15s\n", $0, "启动时间"
+        }
+
+        {
+            run_time=$NF
+            now=systime();
+            start_time = strftime("%Y-%m-%d %H:%M:%S", now-$NF);
+            printf "%s %19s\n", $0, start_time
+        }'
 }
 
 function do_ps() {
     if [[ $# == 0 ]]; then
-        do_ps_sum ruser
+        echo "--------------------------耗 CPU最多的3个进程---------------------------"
+        do_ps_comm pcpu | head -4
+        echo "--------------------------耗内存最多的3个进程---------------------------"
+        do_ps_comm pmem | head -4
+        echo "--------------------------用户数据汇总----------------------------------"
+        do_ps_sum
     else
         argv=
         for key in $@; do
@@ -46,20 +75,10 @@ function do_ps() {
             argv=$argv$(pgrep -d, $key || log_erro "未找到进程 $key")
         done
 
-        ps -o comm,pid,ppid,ruser,pcpu,pmem,rsz,nlwp,etime:10,etimes:10 --sort=-etime -p $argv | awk '
-            BEGIN {
-                getline
-                printf "%s %23s\n", $0, "启动时间"
-            }
-
-            {
-                run_time=$NF
-                now=systime();
-                start_time = strftime("%Y-%m-%d %H:%M:%S", now-$NF);
-                printf "%s %19s\n", $0, start_time
-            }'
-        echo "-------------------------------------------------------------"
-        do_ps_sum comm $argv
+        echo "--------------------------进程数据详情----------------------------------"
+        do_ps_comm etime $argv
+        echo "--------------------------进程数据汇总----------------------------------"
+        do_ps_sum $argv
     fi
 }
 
