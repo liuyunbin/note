03d84fe0207ad03691395fdee546c89d6c5d143d
liuyunbin <yunbinliu@upchina.com>
2022-11-08 18:23:40 +0800

    测试子进程已退出 父进程未正确处理的情况

--- note/计算机基础_僵尸进程/test.cc
+++ note/计算机基础_僵尸进程/test.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <signal.h>
 #include <stdlib.h>
@@ -11,7 +12,8 @@ void log(const std::string& msg) {
 }
 
 void handle_signal(int sig, siginfo_t* sig_info, void * ) {
-    log("捕获信号 SIGCHLD 来自 " + std::to_string(sig_info->si_pid));
+    log("信号处理函数-捕获来自 " + std::to_string(sig_info->si_pid) + " 的信号 SIGCHLD");
+    log("已退出的子进程是: " + std::to_string(waitpid(-1, NULL, 0)));
 }
 
 void set_signal() {
@@ -22,12 +24,29 @@ void set_signal() {
     sigaction(SIGCHLD, &act, NULL);
 }
 
-int main() {
-    //set_signal();
+void handle_signal_2(int sig, siginfo_t* sig_info, void * ) {
+    log("信号处理函数-捕获来自 " + std::to_string(sig_info->si_pid) + " 的信号 SIGCHLD");
+    for (;;) {
+        int fd = waitpid(-1, NULL, WNOHANG);
+        if (fd == -1)
+            break;
+        log("已退出的子进程是: " + std::to_string(fd));
+    }
+}
 
+void set_signal_2() {
+    struct sigaction act;
+    act.sa_sigaction = handle_signal_2;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = SA_RESTART | SA_SIGINFO;
+    sigaction(SIGCHLD, &act, NULL);
+}
+
+int main() {
     log("测试僵尸进程");
+
     log("");
-    log("测试父进程未处理 SIGCHLD 的情况");
+    log("测试子进程已退出, 父进程未处理的情况");
     pid_t fd = fork();
 
     if (fd == 0) {
@@ -41,8 +60,55 @@ int main() {
         log("子进程状态");
         std::string cmd = "ps -o pid,ppid,state -p " + std::to_string(fd);
         system(cmd.data());
+        waitpid(-1, NULL, 0); // 捕捉子进程 和 system
+        waitpid(-1, NULL, 0);
     }
 
+    log("");
+    log("测试子进程已退出, 父进程未正确处理的情况");
+    set_signal();
+    for (int i = 0; i < 5; ++i) {
+        fd = fork();
+        if (fd == 0) {
+            // 子进程
+            log("子进程启动");
+            return 0;
+        }
+    }
+    // 父进程
+    sleep(1);
+    log("子进程状态");
+    std::string cmd = "ps -o pid,ppid,state,comm -C a.out,ps";
+    system(cmd.data());
+/*
+    log("");
+    log("");
+    log("");
+    log("测试子进程已退出, 父进程正确处理的情况");
+    log("恢复 SIGCHLD 的默认处理");
+    signal(SIGCHLD, SIG_DFL);
+    fd = fork();
+
+    if (fd == 0) {
+        // 子进程
+        log("子进程启动");
+        log("子进程退出");
+        return 0;
+    } else {
+        // 父进程
+        sleep(1);
+        log("子进程状态");
+        std::string cmd = "ps -o pid,ppid,state,comm -C a.out,ps";
+        system(cmd.data());
+        sleep(3);
+        log("设置 SIGCHLD 处理函数");
+        set_signal_2();
+        sleep(3);
+        log("子进程状态");
+        system(cmd.data());
+        sleep(2);
+    }
+*/
     return 0;
 }
 
