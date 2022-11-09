d566fd61e148a436534b3f32df39bf9ce37827dc
liuyunbin <yunbinliu@upchina.com>
2022-11-09 11:07:03 +0800

    简化对僵尸进程的处理

--- note/计算机基础_僵尸进程/test.cc
+++ note/计算机基础_僵尸进程/test.cc
@@ -42,6 +42,13 @@ void set_signal_2() {
     sigaction(SIGCHLD, &act, NULL);
 }
 
+void set_signal_3() {
+    struct sigaction act;
+    act.sa_sigaction = handle_signal;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = SA_RESTART | SA_SIGINFO | SA_NOCLDWAIT;
+    sigaction(SIGCHLD, &act, NULL);
+}
 int main() {
     log("测试僵尸进程");
 
@@ -105,6 +112,24 @@ int main() {
     sleep(1);
     log("子进程状态");
     system(cmd.data());
+
+    log("");
+    log("测试子进程已退出, 父进程正确处理的情况, 简化操作");
+    set_signal_3();
+    for (int i = 0; i < 5; ++i) {
+        fd = fork();
+        if (fd == 0) {
+            // 子进程
+            log("子进程启动");
+            return 0;
+        }
+    }
+    // 父进程
+    sleep(1);
+    log("子进程状态");
+    system(cmd.data());
+
+    sleep(1);
 
     return 0;
 }
