9e8125a1777494a0df74569c69a408e648ad3448
liuyunbin <yunbinliu@upchina.com>
2022-11-10 11:25:17 +0800

    暂时添加 计算机基础_孤儿进程/test.cc

--- note/计算机基础_孤儿进程/test.cc
+++ note/计算机基础_孤儿进程/test.cc
@@ -15,7 +15,6 @@ std::map<int, std::string> m;
 void init() {
     m[SIGHUP   ] = " 1-SIGHUP ";
     m[SIGCONT  ] = "18-SIGCONT";
-   // m[SIGSTOP  ] = "19-SIGSTOP";
     m[SIGTSTP  ] = "20-SIGTSTP";
     m[SIGTTIN  ] = "21-SIGTTIN";
     m[SIGTTOU  ] = "22-SIGTTOU";
@@ -30,7 +29,6 @@ void set_signal() {
     act.sa_sigaction = handle_signal;
     sigemptyset(&act.sa_mask);
     act.sa_flags = SA_RESTART | SA_SIGINFO | SA_NOCLDWAIT;
-    sigaction(SIGCHLD, &act, NULL);
     for (auto key : m)
         sigaction(key.first, &act, NULL);
 }
@@ -40,13 +38,13 @@ int main() {
 
     log("测试孤儿进程");
 
-    log();
-    log("测试孤儿进程处于运行状态");
-
     if (fork() == 0) {
         // 子进程
+        log();
+        log("测试孤儿进程处于运行状态");
         if (fork() == 0) {
             // 子进程
+            set_signal();
             log("子进程启动");
             log("子进程状态");
             std::string cmd = "ps -o pid,ppid,state -p " + std::to_string(getpid());
@@ -54,6 +52,7 @@ int main() {
             sleep(2);
             log("子进程状态");
             system(cmd.data());
+            sleep(2);
             log("子进程退出");
             return 0;
         } else {
@@ -64,8 +63,43 @@ int main() {
         }
     }
 
+    sleep(5);
+
+    if (fork() == 0) {
+        // 子进程
+        log();
+        log("测试孤儿进程处于暂停状态");
+        pid_t fd = fork();
+        if (fd == 0) {
+            // 子进程
+            set_signal();
+            log("子进程启动");
+            log("子进程状态");
+            std::string cmd = "ps -o pid,ppid,state -p " + std::to_string(getpid());
+            system(cmd.data());
+            log("子进程使自己暂停");
+            kill(getpid(), SIGSTOP);
+            sleep(2);
+            log("子进程状态");
+            system(cmd.data());
+            log("子进程退出");
+            return 0;
+        } else {
+            // 父进程
+            sleep(3);
+            log("子进程状态");
+            std::string cmd = "ps -o pid,ppid,state -p " + std::to_string(fd);
+            system(cmd.data());
+            log("父进程退出");
+            return 0;
+        }
+    }
+
+    sleep(1);
     sleep(10);
     log("主进程退出");
+    //for (;;)
+    //    ;
 
     return 0;
 }
