09ac48120bca3932677e732f84056334b8e89a7d
liuyunbin <yunbinliu@outlook.com>
2022-11-12 17:21:39 +0800

    添加 计算机基础之信号不可靠

---
+++ note/计算机基础之信号不可靠/test.cc
@@ -0,0 +1,68 @@
+
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <map>
+#include <string>
+
+void log(const std::string& msg = "") {
+    std::cout << "进程(" << getpid() << "): " << msg << std::endl;
+}
+
+void handle_signal(int sig, siginfo_t* sig_info, void*) {
+    log("捕获信号 SIGUSR1");
+}
+
+void set_signal(bool reset = false) {
+    struct sigaction act;
+    act.sa_sigaction = handle_signal;
+    sigemptyset(&act.sa_mask);
+    if (reset)
+        act.sa_flags = SA_SIGINFO | SA_RESETHAND;
+    else
+        act.sa_flags = SA_SIGINFO;
+    sigaction(SIGUSR1, &act, NULL);
+}
+
+int main() {
+    log("测试信号不可靠");
+
+    log();
+    log("测试信号丢失");
+
+    log("注册信号 SIGUSR1 处理函数");
+    set_signal();
+
+    log("阻塞信号 SIGUSR1");
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGUSR1);
+    sigprocmask(SIG_SETMASK, &mask, NULL);
+
+    for (int i = 0; i < 5; ++i) {
+        log("发送信号 SIGUSR1");
+        kill(getpid(), SIGUSR1);
+    }
+
+    log("解除信号 SIGUSR1 阻塞");
+    sigprocmask(SIG_UNBLOCK, &mask, NULL);
+
+    sleep(1);
+
+    log();
+    log("测试信号处理函数被重置");
+    log("注册信号 SIGUSR1 处理函数, 并设置重置信号处理");
+    set_signal(true);
+
+    log("发送信号 SIGUSR1");
+    kill(getpid(), SIGUSR1);
+    sleep(1);
+    log("发送信号 SIGUSR1");
+    kill(getpid(), SIGUSR1);
+    sleep(1);
+    log("主进程退出");
+
+    return 0;
+}
