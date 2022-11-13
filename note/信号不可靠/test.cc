8b93cc71bec8bccc07d78c91f45f1f2e2c8118d3
liuyunbin <yunbinliu@outlook.com>
2022-11-13 11:19:27 +0800

    优化 信号不可靠 的输出

--- note/信号不可靠/test.cc
+++ note/信号不可靠/test.cc
@@ -7,12 +7,14 @@
 #include <map>
 #include <string>
 
+int count = 0;
+
 void log(const std::string& msg = "") {
     std::cout << "进程(" << getpid() << "): " << msg << std::endl;
 }
 
 void handle_signal(int sig, siginfo_t* sig_info, void*) {
-    log("捕获信号 SIGUSR1");
+    log("捕获信号 SIGUSR1 第 " + std::to_string(++count) + " 次");
 }
 
 void set_signal(bool reset = false) {
@@ -41,8 +43,8 @@ int main() {
     sigaddset(&mask, SIGUSR1);
     sigprocmask(SIG_SETMASK, &mask, NULL);
 
-    for (int i = 0; i < 5; ++i) {
-        log("发送信号 SIGUSR1");
+    for (int i = 1; i <= 5; ++i) {
+        log("发送信号 SIGUSR1 第 " + std::to_string(i) + " 次");
         kill(getpid(), SIGUSR1);
     }
 
@@ -52,14 +54,18 @@ int main() {
     sleep(1);
 
     log();
+    log("重置信号处理函数的计数器");
+    count = 0;
+
+    log();
     log("测试信号处理函数被重置");
     log("注册信号 SIGUSR1 处理函数, 并设置重置信号处理");
     set_signal(true);
 
-    log("发送信号 SIGUSR1");
+    log("发送信号 SIGUSR1 第 1 次");
     kill(getpid(), SIGUSR1);
     sleep(1);
-    log("发送信号 SIGUSR1");
+    log("发送信号 SIGUSR1 第 2 次");
     kill(getpid(), SIGUSR1);
     sleep(1);
     log("主进程退出");
