d5298e3ac0c491fb923fdcf6a2f40d8f2ee71dac
liuyunbin <yunbinliu@outlook.com>
2022-12-07 10:20:57 +0800

    添加 测试僵尸进程之父进程未处理

---
+++ note/信号和进程/测试僵尸进程之父进程未处理.cc
@@ -0,0 +1,39 @@
+
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <map>
+#include <string>
+
+void log(const std::string& msg = "") {
+    std::cout << "进程(" << getpid() << "): " << msg << std::endl;
+}
+
+int main() {
+    log("测试僵尸进程之父进程未处理");
+    log();
+
+    pid_t child = fork();
+
+    if (child == 0) {
+        // 子进程
+        for (;;)
+            ;
+    } else {
+        sleep(1);
+        std::string cmd = "ps -o pid,comm,state -p ";
+        cmd += std::to_string(child);
+        log("子进程状态");
+        system(cmd.data());
+        log("杀死子进程");
+        kill(child, SIGKILL);
+        sleep(1);
+        log("子进程状态");
+        system(cmd.data());
+    }
+
+    return 0;
+}
