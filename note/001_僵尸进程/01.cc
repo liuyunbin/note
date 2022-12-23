a0adfdf3879d55aeb26cac9606adc245a407a43f
liuyunbin <yunbinliu@outlook.com>
2022-12-23 19:17:13 +0800

    add 001_僵尸进程

---
+++ note/001_僵尸进程/01.cc
@@ -0,0 +1,39 @@
+
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <map>
+#include <string>
+
+void log(const std::string& msg = "") {
+    std::cout << "进程(" << getpid() << "): " << msg << std::endl;
+}
+
+int main() {
+    log("测试产生僵尸进程之父进程未处理子进程退出的状态信息");
+    log();
+    pid_t child = fork();
+
+    if (child == 0) {
+        // 子进程
+        log("子进程已启动");
+        for (;;)
+            ;
+    }
+    sleep(1);  // 保证子进程已启动
+    std::string cmd = "ps -o pid,comm,state -p " + std::to_string(child);
+    log("子进程状态");
+    system(cmd.data());
+    log("杀死子进程 " + std::to_string(child));
+    kill(child, SIGKILL);
+    sleep(1);
+    log("子进程状态");
+    system(cmd.data());
+
+    log();
+    log("主进程退出");
+    return 0;
+}
---
+++ note/001_僵尸进程/02.cc
@@ -0,0 +1,69 @@
+
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <map>
+#include <string>
+
+void log(const std::string& msg = "") {
+    std::cout << "进程(" << getpid() << "): " << msg << std::endl;
+}
+
+void handle_signal(int sig, siginfo_t* sig_info, void*) {
+    log("捕获来自 " + std::to_string(sig_info->si_pid) + " 的信号 SIGCHLD");
+    int fd = waitpid(-1, NULL, WNOHANG);
+    if (fd > 0) log("已退出的子进程是: " + std::to_string(fd));
+}
+
+void set_signal() {
+    struct sigaction act;
+    act.sa_sigaction = handle_signal;
+    act.sa_flags = SA_SIGINFO;
+    sigemptyset(&act.sa_mask);
+    sigaction(SIGCHLD, &act, NULL);
+}
+
+int main() {
+    log("测试产生僵尸进程之父进程未正确处理子进程退出的状态信息");
+    log();
+
+    log("设置 SIGCHLD 处理为: 调用 waitpid() 一次");
+    set_signal();
+
+    log("阻塞信号 SIGCHLD");
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGCHLD);
+    sigprocmask(SIG_SETMASK, &mask, NULL);
+
+    std::string cmd = "ps -o pid,comm,state -p ";
+
+    for (int i = 1; i <= 5; ++i) {
+        pid_t fd = fork();
+        if (fd == 0) {
+            // 子进程
+            log("第 " + std::to_string(i) + " 个子进程启动后退出");
+            exit(-1);
+        } else {
+            // 父进程
+            cmd += std::to_string(fd) + ",";
+            sleep(1);
+        }
+    }
+
+    cmd.pop_back();
+
+    log("解除信号 SIGCHLD 的阻塞");
+    sigprocmask(SIG_UNBLOCK, &mask, NULL);
+    sleep(1);
+    log("此时, 子进程的状态");
+    system(cmd.data());
+
+    log();
+    log("主进程退出");
+
+    return 0;
+}
