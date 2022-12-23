a324e594343c9ec7fa155f893c152162f1b13577
liuyunbin <yunbinliu@outlook.com>
2022-12-23 19:48:33 +0800

    add 002_孤儿进程

---
+++ note/002_孤儿进程/test.cc
@@ -0,0 +1,51 @@
+
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <map>
+#include <string>
+
+void log(const std::string& msg = "") {
+    std::cout << "进程(" << getpid() << "): " << msg << std::endl;
+}
+
+int main() {
+    log("测试孤儿进程");
+    log();
+
+    if (fork() == 0) {
+        if (fork() == 0) {
+            // 测试的子进程
+            std::string cmd = "ps -o pid,ppid,pgid,sid,state,comm -p ";
+            std::string commond = cmd;
+            commond += std::to_string(getpid()) + ",";
+            commond += std::to_string(getppid());
+            log("进程状态");
+            system(commond.data());
+            log("杀死父进程 " + std::to_string(getppid()));
+            kill(getppid(), SIGKILL);
+            sleep(1);
+            log("进程状态");
+            commond = cmd;
+            commond += std::to_string(getpid()) + ",";
+            commond += std::to_string(getppid());
+            system(commond.data());
+            return 0;
+        } else {
+            // 测试的父进程
+            for (;;)
+                ;
+        }
+    }
+
+    sleep(2);
+
+    log();
+    log("主进程退出");
+
+    return 0;
+}
