c0d87e3cbbb1a5ae95158a2912998e3d946dd4fd
liuyunbin <yunbinliu@outlook.com>
2023-06-11 20:30:39 +0800

    添加 进程

--- src/002/04.cc
+++ src/002/04.cc
@@ -2,37 +2,42 @@
 #include "log.h"
 
 int main() {
+    log();
     log("测试不可被信号打断的休眠(指被捕获的信号) 对 SIGKILL 的处理");
     log();
+
     pid_t fd = fork();
-    std::string cmd = "ps -o pid,state,comm -p " + std::to_string(fd);
     if (fd == 0) {
         if (vfork() == 0) {
-            log("子进程启动");
-            log("子进程休眠10秒");
+            log("测试的子进程启动");
+            log("测试的子进程休眠10秒");
             sleep(10);
-            log("子进程休眠完成");
+            log("测试的子进程休眠完成");
+            log("测试的子进程退出");
             exit(-1);
         }
-        log("父进程休眠1s");
+        log("测试的父进程休眠1s");
         sleep(1);
-        log("父进程退出");
+        log("测试的父进程退出");
         exit(-1);
     } else {
         sleep(1);
+        std::string cmd = "ps -o pid,state,comm -p " + std::to_string(fd);
+
         log("测试的父进程状态");
         system(cmd.data());
-        log("发送信号 SIGKILL");
+
+        log("向测试的父进程发送信号 SIGKILL");
         kill(fd, SIGKILL);
+        sleep(1);
         log("测试的父进程状态");
         system(cmd.data());
         wait(NULL);
-        log("测试的父进程退出");
     }
 
     sleep(10);
-    log();
     log("主进程正常退出");
+    log();
 
     return 0;
 }
--- src/002/05.cc
+++ src/002/05.cc
@@ -1,43 +1,51 @@
 
 #include "log.h"
 
-void handle_signal(int sig) { log("捕捉到信号 SIGUSR1"); }
+void handle_signal(int sig) { log("子进程捕捉到信号 SIGUSR1"); }
 
 int main() {
+    log();
     log("测试 暂停 => 继续");
     log();
-    log("注册信号处理函数");
-    signal(SIGUSR1, handle_signal);
+
     pid_t fd = fork();
-    std::string cmd = "ps -o pid,state,comm -p " + std::to_string(fd);
     if (fd == 0) {
+        log("子进程注册信号处理函数");
+        signal(SIGUSR1, handle_signal);
         for (;;)
             ;
     } else {
         sleep(1);
+        std::string cmd = "ps -o pid,state,comm -p " + std::to_string(fd);
+
         log("使子进程暂停");
         kill(fd, SIGSTOP);
         sleep(1);
         log("子进程状态");
         system(cmd.data());
 
-        log("发送信号 SIGUSR1");
+        log("向子进程发送信号 SIGUSR1");
         kill(fd, SIGUSR1);
         sleep(1);
         log("子进程状态");
         system(cmd.data());
 
-        log("发送信号 SIGCONT");
+        log("向子进程发送信号 SIGCONT");
         kill(fd, SIGCONT);
         sleep(1);
         log("子进程状态");
         system(cmd.data());
+
+        log("向子进程发送信号 SIGKILL");
         kill(fd, SIGKILL);
+        sleep(1);
+        log("子进程状态");
+        system(cmd.data());
         wait(NULL);
     }
 
-    log();
     log("主进程正常退出");
+    log();
 
     return 0;
 }
