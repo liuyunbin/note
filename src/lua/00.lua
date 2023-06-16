e820f26bbc5f9a2ef1a97c25b0293ab9df9ad559
liuyunbin <yunbinliu@outlook.com>
2023-06-16 08:54:45 +0800

    update lua

--- src/lua/00.lua
+++ src/lua/00.lua
@@ -47,8 +47,7 @@ print("测试数字 => 字符串: ", string.char(97, 98, 99, 100))
 print("测试字符串 子串: ", string.sub(v10, 2))
 print("测试字符串匹配(只查询第一个匹配): ", string.match(v10, "123"))
 print("测试字符串匹配(多个): ")
-for v in string.gmatch(v10, "%a+")
-do
+for v in string.gmatch(v10, "%a+") do
     print(v)
 end
 -- 上述匹配支持正则表达式
@@ -66,8 +65,7 @@ end
 
 print("测试字典: ")
 
-for k,v in pairs(v9)
-do
+for k,v in pairs(v9) do
     print(k, " -> ", v)
 end
 
@@ -77,15 +75,13 @@ print("测试 table 插入: ", v9[4])
 table.remove(v9)
 print("测试 table 删除: ", v9[4])
 print("测试 table 排序, 排序前: ") -- 只能对数组排序, 不能对字典排序
-for k,v in pairs(v9)
-do
+for k,v in pairs(v9) do
     print(k, " -> ", v)
 end
 table.sort(v9)
 
 print("测试 table 排序, 排序后: ")
-for k,v in pairs(v9)
-do
+for k,v in pairs(v9) do
     print(k, " -> ", v)
 end
 
@@ -102,26 +98,22 @@ print("测试函数调用: ", test(1))
 
 print("测试循环和选择判断: ")
 
-for i = 1,10,2
-do
+for i = 1,10,2 do
     print(i)
 end
 
-for k, v in pairs(v9)
-do
+for k, v in pairs(v9) do
     print(k, " -> ", v)
 end
 
 i = 1
-while (i < 10)
-do
+while (i < 10) do
     print (i)
     i = i + 3
 end
 
 i = 10
-if (i == 0)
-then
+if (i == 0) then
     print("i == 0")
 else if (i == 1)
     then
@@ -131,8 +123,7 @@ else if (i == 1)
     end
 end
 
-if ("123" == 123)
-then
+if ("123" == 123) then
     print('"123" == 123');
 else
     print('"123" != 123');
