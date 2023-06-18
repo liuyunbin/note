632269bbd3f6dbab897f6fb42c5b6d7c2168f714
liuyunbin <yunbinliu@outlook.com>
2023-06-18 09:56:14 +0800

    rename

--- 006-06.cc
+++ 006-06.cc
@@ -1,38 +1,4 @@
 
-#include <setjmp.h>
-#include <signal.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
-
-#include <algorithm>
-#include <bitset>
-#include <cctype>
-#include <cfenv>
-#include <cfloat>
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <iomanip>
-#include <iostream>
-#include <limits>
-#include <map>
-#include <sstream>
-#include <string>
-
-std::string get_time() {
-    time_t now = time(NULL);
-    struct tm* info = localtime(&now);
-    char buf[1024];
-    strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S %z", info);
-    return buf;
-}
-
-void log(const std::string& msg = "") {
-    std::cout << get_time() << " " << msg << std::endl;
-}
 
 void test(pid_t pid, pid_t pgid) {
     log();
