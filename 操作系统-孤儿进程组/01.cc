1c66ade6681b20182cd988291ba5570fa34b8949
liuyunbin <yunbinliu@outlook.com>
2023-06-23 17:16:20 +0800

    添加 操作系统-孤儿进程组

---
+++ 操作系统-孤儿进程组/01.cc
@@ -0,0 +1,124 @@
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <map>
+#include <string>
+
+void log(const std::string& msg = "") {
+    time_t     now  = time(NULL);
+    struct tm* info = localtime(&now);
+    char       buf[1024];
+    strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S %z", info);
+    std::cout << buf << " " << msg << std::endl;
+}
+
+std::map<int, std::string> m{
+    {SIGHUP,    " 1-SIGHUP"   },
+    {SIGINT,    " 2-SIGINT"   },
+    {SIGQUIT,   " 3-SIGQUIT"  },
+    {SIGILL,    " 4-SIGILL"   },
+    {SIGTRAP,   " 5-SIGTRAP"  },
+    {SIGABRT,   " 6-SIGABRT"  },
+    {SIGBUS,    " 7-SIGBUS"   },
+    {SIGFPE,    " 8-SIGFPE"   },
+    {SIGKILL,   " 9-SIGKILL"  },
+    {SIGUSR1,   "10-SIGUSR1"  },
+    {SIGSEGV,   "11-SIGSEGV"  },
+    {SIGUSR2,   "12-SIGUSR2"  },
+    {SIGPIPE,   "13-SIGPIPE"  },
+    {SIGALRM,   "14-SIGALRM"  },
+    {SIGTERM,   "15-SIGTERM"  },
+    {SIGSTKFLT, "16-SIGSTKFLT"},
+    {SIGCHLD,   "17-SIGCHLD"  },
+    {SIGCONT,   "18-SIGCONT"  },
+    {SIGSTOP,   "19-SIGSTOP"  },
+    {SIGTSTP,   "20-SIGTSTP"  },
+    {SIGTTIN,   "21-SIGTTIN"  },
+    {SIGTTOU,   "22-SIGTTOU"  },
+    {SIGURG,    "23-SIGURG"   },
+    {SIGXCPU,   "24-SIGXCPU"  },
+    {SIGXFSZ,   "25-SIGXFSZ"  },
+    {SIGVTALRM, "26-SIGVTALRM"},
+    {SIGPROF,   "27-SIGPROF"  },
+    {SIGWINCH,  "28-SIGWINCH" },
+    {SIGIO,     "29-SIGIO"    },
+    {SIGPWR,    "30-SIGPWR"   },
+    {SIGSYS,    "31-SIGSYS"   }
+};
+
+void handle_signal(int sig, siginfo_t* sig_info, void*) {
+    log("捕获来自 " + std::to_string(sig_info->si_pid) + " 的信号 " + m[sig]);
+}
+
+int main() {
+    log();
+    log("操作系统-孤儿进程组");
+    log();
+
+    log("设置信号处理");
+    struct sigaction act;
+    act.sa_sigaction = handle_signal;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = SA_SIGINFO;
+    sigaction(SIGHUP, &act, NULL);
+    sigaction(SIGCONT, &act, NULL);
+
+    pid_t main_pid = getpid();
+    if (fork() == 0) {
+        // 测试的父进程
+        log("测试的父进程启动: " + std::to_string(getpid()));
+        log("设置新的进程组: " + std::to_string(getpid()));
+        setpgid(getpid(), getpid());
+        pid_t child_1 = fork();
+        if (child_1 == 0) {
+            // 测试的第一个子进程
+            log("测试的第一个子进程启动: " + std::to_string(getpid()));
+            log("测试的第一个子进程使自己暂停");
+            kill(getpid(), SIGSTOP);
+            for (;;)
+                ;
+        } else if (fork() == 0) {
+            // 测试的第二个子进程
+            sleep(1);
+            log("测试的第二个子进程启动: " + std::to_string(getpid()));
+            log("进程状态");
+            std::string cmd = "ps -o pid,ppid,pgid,sid,state,comm -p ";
+            cmd += std::to_string(main_pid) + ",";
+            cmd += std::to_string(child_1) + ",";
+            cmd += std::to_string(getpid()) + ",";
+            cmd += std::to_string(getppid());
+            log("进程状态");
+            system(cmd.data());
+            log("杀死测试的父进程: " + std::to_string(getppid()));
+            kill(getppid(), SIGKILL);
+            sleep(1);
+            sleep(1);
+            cmd = "ps -o pid,ppid,pgid,sid,state,comm -p ";
+            cmd += std::to_string(main_pid) + ",";
+            cmd += std::to_string(child_1) + ",";
+            cmd += std::to_string(getpid()) + ",";
+            cmd += std::to_string(getppid());
+            log("进程状态");
+            system(cmd.data());
+            log("杀死测试的第一个子进程: " + std::to_string(child_1));
+            kill(child_1, SIGKILL);
+            log("测试的第二个子进程退出");
+            return 0;
+        } else {
+            // 父进程
+            for (;;)
+                ;
+        }
+    }
+
+    sleep(3);
+
+    log();
+    log("主进程正常退出");
+    log();
+    return 0;
+}
