45903606bc134f0313ef75eb5ba5fad4ba36e830
liuyunbin <yunbinliu@outlook.com>
2023-06-29 14:24:04 +0800

    测试宏

--- test.cc
+++ test.cc
@@ -1,5 +1,10 @@
 
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
 #include <time.h>
+#include <unistd.h>
 
 #include <iostream>
 #include <sstream>
@@ -30,4 +35,118 @@ void log(const std::string& msg = "") {
 template <typename... Args>
 void log(Args... args) {
     log(to_string(args...));
+}
+
+void test_va();     // 测试可变参数
+void test_macro();  // 测试宏
+
+class A {
+  public:
+    A() {
+        std::cout << "调用构造函数" << std::endl;
+    }
+
+    ~A() {
+        std::cout << "调用析构函数" << std::endl;
+    }
+};
+
+void test_1() {
+    std::cout << "测试函数-1" << std::endl;
+}
+
+void test_2() {
+    std::cout << "测试函数-2" << std::endl;
+}
+
+void test_atexit() {
+    std::cout << "注册退出函数" << std::endl;
+    atexit(test_1);
+    atexit(test_1);
+    atexit(test_2);
+    atexit(test_2);
+}
+
+void test_exit() {
+    if (fork() == 0) {
+        std::cout << "测试 exit" << std::endl;
+        A a;
+        test_atexit();
+        std::cout << "退出" << std::endl;
+        exit(0);
+    }
+
+    sleep(1);
+    std::cout << std::endl;
+
+    if (fork() == 0) {
+        std::cout << "测试 _exit" << std::endl;
+        A a;
+        test_atexit();
+        std::cout << "退出" << std::endl;
+        _exit(0);
+    }
+    sleep(1);
+}
+
+int main() {
+#if 1
+    test_macro();  // 测试宏
+#endif
+
+#if 0
+    test_va();  // 测试可变参数
+#endif
+
+    //    std::cout << "环境变量 PATH: " << getenv("PATH") << std::endl;
+
+    //    std::cout << std::endl;
+
+    //    test_exit();
+
+    return 0;
+}
+
+// 测试宏
+int v123 = 123456;
+
+#define TEST_MACRO_STR(fmt, X) printf(fmt, #X, X)
+#define TEST_MACRO_CAT(fmt, X) printf(fmt, v##X)
+#define TEST_MACRO(fmt, ...) \
+    printf(fmt, ##__VA_ARGS__)  // 如果可变参数不存在, 去掉前面的逗号
+
+void test_macro() {
+    TEST_MACRO_STR("测试宏变字符串: %s -> %d\n", 123);
+    TEST_MACRO_CAT("测试    宏连接: %d\n", 123);
+    TEST_MACRO("测试宏有可变参数: %d\n", 123);
+    TEST_MACRO("测试宏无可变参数\n");
+}
+
+// 测试可变参数
+
+#define LOG(...)   printf(__VA_ARGS__)
+#define TEST(ARGS) printf("%s => %d\n", #ARGS, ARGS)
+#define LYB        111
+
+void test_va_c(const char* s, ...) {
+    va_list ap;
+    va_start(ap, s);
+    vprintf(s, ap);
+    va_end(ap);
+}
+
+void test_va_cpp() {
+}
+
+template <typename T, typename... Args>
+void test_va_cpp(T t, Args... args) {
+    std::cout << t;
+    test_va_cpp(args...);
+}
+
+void test_va() {
+    LOG("测试 C 可变参数: %s -> %s\n", "123", "456");
+    test_va_c("测试 C 可变参数: %s -> %s\n", "123", "456");
+    test_va_cpp("测试 c++ 可变参数: ", "123", "456", "\n");
+    TEST(LYB);
 }
