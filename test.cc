a95f4a87bbac8d3e493b2adf198dd59cc9134314
liuyunbin <yunbinliu@outlook.com>
2023-06-29 14:51:59 +0800

    use log

--- test.cc
+++ test.cc
@@ -10,33 +10,6 @@
 #include <sstream>
 #include <string>
 
-template <typename T>
-std::string to_string(T data) {
-    std::stringstream tmp;
-    tmp << data;
-    return tmp.str();
-}
-
-template <typename T, typename... Args>
-std::string to_string(T data, Args... args) {
-    std::stringstream tmp;
-    tmp << data;
-    return tmp.str() + to_string(args...);
-}
-
-void log(const std::string& msg = "") {
-    time_t     now  = time(NULL);
-    struct tm* info = localtime(&now);
-    char       buf[1024];
-    strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S %z", info);
-    std::cout << buf << " " << msg << std::endl;
-}
-
-template <typename... Args>
-void log(Args... args) {
-    log(to_string(args...));
-}
-
 void test_va();     // 测试可变参数
 void test_macro();  // 测试宏
 
@@ -105,6 +78,34 @@ int main() {
     //    test_exit();
 
     return 0;
+}
+
+// 日志输出
+template <typename T>
+std::string to_string(T data) {
+    std::stringstream tmp;
+    tmp << data;
+    return tmp.str();
+}
+
+template <typename T, typename... Args>
+std::string to_string(T data, Args... args) {
+    std::stringstream tmp;
+    tmp << data;
+    return tmp.str() + to_string(args...);
+}
+
+void log(const std::string& msg = "") {
+    time_t     now  = time(NULL);
+    struct tm* info = localtime(&now);
+    char       buf[1024];
+    strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S %z", info);
+    std::cout << buf << " " << msg << std::endl;
+}
+
+template <typename... Args>
+void log(Args... args) {
+    log(to_string(args...));
 }
 
 // 测试宏
