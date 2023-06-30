97d98bdcd59ef09239fb561534e065f0e1a8b1ea
liuyunbin <yunbinliu@outlook.com>
2023-06-30 10:47:32 +0800

    整理僵尸进程的测试

--- test-linux.cc
+++ test-linux.cc
@@ -1,11 +1,13 @@
 
 #include <setjmp.h>
+#include <signal.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/resource.h>
 #include <sys/time.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
 
@@ -13,35 +15,69 @@
 #include <sstream>
 #include <string>
 
-void test_va();     // 测试可变参数
-void test_macro();  // 测试宏
-void test_exit();   // 测试退出
-void test_jmp();    // 测试跨函数跳转
-void test_limit();  // 测试资源限制
+void test_va();        // 测试可变参数
+void test_macro();     // 测试宏
+void test_exit();      // 测试退出
+void test_jmp();       // 测试跨函数跳转
+void test_limit();     // 测试资源限制
+void test_zombie_1();  // 重现僵尸进程的产生:
+                       // 父进程未处理子进程退出的状态信息
+void test_zombie_2();  // 重新僵尸进程的产生:
+                       // 父进程未正确处理子进程退出的状态信息
+void test_zombie_3();  // 预防僵尸进程的产生: 忽略信号 SIGCHLD
+void test_zombie_4();  // 预防僵尸进程的产生:
+                       // 设置 SIGCHLD 处理为 循环调用 waitpid");
+void test_zombie_5();  // 预防僵尸进程的产生:
+                       // 设置 SIGCHLD 选项为 SA_NOCLDWAIT
+void test_zombie_6();  // 预防僵尸进程的产生: 杀死父进程
+void test_zombie_7();  // 销毁僵尸进程: 杀死僵尸进程的父进程
+void test_zombie_8();  // 测试: 产生僵尸进程不退出
 
 int main() {
-#if 0
-    test_macro();  // 测试宏
-#endif
+    // 测试宏
+    // test_macro();
 
-#if 0
-    test_va();  // 测试可变参数
-#endif
+    // 测试可变参数
+    // test_va();
 
-#if 0
-    test_exit();  // 测试退出
-#endif
+    // 测试退出
+    // test_exit();
 
-#if 0
-    test_jmp(); // 测试跨函数跳转
-#endif
+    // 测试跨函数跳转
+    // test_jmp();
 
+    // 测试资源限制
+    // test_limit();
+
+    // 重现僵尸进程的产生: 父进程未处理子进程退出的状态信息
+    // test_zombie_1();
+
+    // 重新僵尸进程的产生: 父进程未正确处理子进程退出的状态信息
+    // test_zombie_2();
+
+    // 预防僵尸进程的产生: 忽略信号 SIGCHLD
+    // test_zombie_3();
+
+    // 预防僵尸进程的产生: 设置 SIGCHLD 处理为 循环调用 waitpid");
+    // test_zombie_4();
+
+    // 预防僵尸进程的产生: 设置 SIGCHLD 选项为 SA_NOCLDWAIT
+    // test_zombie_5();
+
+    // 预防僵尸进程的产生: 杀死父进程
+    // test_zombie_6();
+
+    // 销毁僵尸进程: 杀死僵尸进程的父进程
+    // test_zombie_7();
+
+    // 测试: 产生僵尸进程不退出
+    test_zombie_8();
     //    std::cout << "环境变量 PATH: " << getenv("PATH") << std::endl;
-
-#if 1
-    test_limit();  // 测试资源限制
-#endif
-
+    //    printf("123");
+    //    if (fork() == 0) {
+    //        exit(0);
+    //    }
+    //    sleep(1);
     return 0;
 }
 
@@ -242,4 +278,354 @@ void test_limit() {
     log();
     TEST_LIMIT(RLIMIT_CPU);
     TEST_LIMIT(RLIMIT_CORE);
+}
+
+//  fork()
+// vfork()
+//
+// getpid()  -- 进程 ID
+// getppid() -- 父进程 ID
+// getuid()  -- 实际用户
+// geteuid() -- 有效用户
+// getgid()  -- 实际组
+// getegid() -- 有效组
+//
+
+// 测试僵尸进程
+void test_zombie_1() {
+    log();
+    log("重现僵尸进程的产生: 父进程未处理子进程退出的状态信息");
+    log();
+
+    pid_t fd = fork();
+
+    if (fd == 0) {
+        log("子进程已启动: ", getpid());
+        for (;;)
+            ;
+    }
+    sleep(1);  // 保证子进程已启动
+    std::string cmd = "ps -o pid,comm,state -p " + std::to_string(fd);
+    log("子进程状态");
+    system(cmd.data());
+    log("杀死子进程:", fd);
+    kill(fd, SIGKILL);
+    sleep(1);
+    log("子进程状态");
+    system(cmd.data());
+
+    log();
+    log("主进程正常退出");
+    log();
+}
+
+void handle_signal_1(int sig, siginfo_t* sig_info, void*) {
+    log("捕获信号 SIGCHLD, 来自: ", sig_info->si_pid);
+    int fd = waitpid(-1, NULL, WNOHANG);
+    if (fd > 0) {
+        log("已退出的子进程是: ", fd);
+    }
+}
+
+void test_zombie_2() {
+    log();
+    log("重新僵尸进程的产生: 父进程未正确处理子进程退出的状态信息");
+    log();
+
+    log("设置 SIGCHLD 处理为: 调用 waitpid() 一次");
+    struct sigaction act;
+    act.sa_sigaction = handle_signal_1;
+    act.sa_flags     = SA_SIGINFO;
+    sigemptyset(&act.sa_mask);
+    sigaction(SIGCHLD, &act, NULL);
+
+    log("阻塞信号 SIGCHLD");
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGCHLD);
+    sigprocmask(SIG_SETMASK, &mask, NULL);
+
+    std::string cmd = "ps -o pid,comm,state -p ";
+
+    for (int i = 1; i <= 5; ++i) {
+        pid_t fd = fork();
+        if (fd == 0) {
+            // 子进程
+            log("子进程启动后退出: ", getpid());
+            exit(-1);
+        } else {
+            // 父进程
+            cmd += std::to_string(fd) + ",";
+            sleep(1);
+        }
+    }
+
+    cmd.pop_back();  // 删除多余的逗号
+
+    log("解除信号 SIGCHLD 的阻塞");
+    sigprocmask(SIG_UNBLOCK, &mask, NULL);
+    sleep(1);
+    log("子进程的状态");
+    system(cmd.data());
+
+    log();
+    log("主进程正常退出");
+    log();
+}
+
+void test_zombie_3() {
+    log();
+    log("预防僵尸进程的产生: 忽略信号 SIGCHLD");
+    log();
+
+    log("设置 SIGCHLD 的信号处理");
+    signal(SIGCHLD, SIG_IGN);
+
+    log("阻塞信号 SIGCHLD");
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGCHLD);
+    sigprocmask(SIG_SETMASK, &mask, NULL);
+
+    std::string cmd = "ps -o pid,comm,state -p ";
+
+    for (int i = 1; i <= 5; ++i) {
+        pid_t fd = fork();
+        if (fd == 0) {
+            // 子进程
+            log("子进程启动后退出: ", getpid());
+            exit(-1);
+        } else {
+            // 父进程
+            cmd += std::to_string(fd) + ",";
+            sleep(1);
+        }
+    }
+
+    cmd.pop_back();  // 删除多余的逗号
+
+    log("解除信号 SIGCHLD 的阻塞");
+    sigprocmask(SIG_UNBLOCK, &mask, NULL);
+    sleep(1);
+    log("子进程的状态");
+    system(cmd.data());
+
+    log();
+    log("主进程正常退出");
+    log();
+}
+
+void handle_signal_2(int sig, siginfo_t* sig_info, void*) {
+    log("捕获信号 SIGCHLD 来自: ", sig_info->si_pid);
+    for (;;) {
+        int fd = waitpid(-1, NULL, WNOHANG);
+        if (fd <= 0)
+            break;
+        log("已退出的子进程是: ", fd);
+    }
+}
+
+void test_zombie_4() {
+    log();
+    log("预防僵尸进程的产生: 设置 SIGCHLD 处理为 循环调用 waitpid");
+    log();
+
+    log("设置 SIGCHLD 的信号处理: 循环调用 waitpid");
+    struct sigaction act;
+    act.sa_sigaction = handle_signal_2;
+    act.sa_flags     = SA_SIGINFO;
+    sigemptyset(&act.sa_mask);
+    sigaction(SIGCHLD, &act, NULL);
+
+    log("阻塞信号 SIGCHLD");
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGCHLD);
+    sigprocmask(SIG_SETMASK, &mask, NULL);
+
+    std::string cmd = "ps -o pid,comm,state -p ";
+
+    for (int i = 1; i <= 5; ++i) {
+        pid_t fd = fork();
+        if (fd == 0) {
+            // 子进程
+            log("子进程启动后退出: ", getpid());
+            exit(-1);
+        } else {
+            // 父进程
+            cmd += std::to_string(fd) + ",";
+            sleep(1);
+        }
+    }
+
+    cmd.pop_back();  // 删除多余的逗号
+
+    log("解除信号 SIGCHLD 的阻塞");
+    sigprocmask(SIG_UNBLOCK, &mask, NULL);
+    sleep(1);
+    log("子进程的状态");
+    system(cmd.data());
+
+    log();
+    log("主进程正常退出");
+    log();
+}
+
+void handle_signal_3(int sig, siginfo_t* sig_info, void*) {
+    log("捕获信号 SIGCHLD 来自: ", sig_info->si_pid);
+}
+
+void test_zombie_5() {
+    log();
+    log("预防僵尸进程的产生: 设置 SIGCHLD 选项为 SA_NOCLDWAIT");
+    log();
+
+    log("设置 SIGCHLD 的信号选项: SA_NOCLDWAIT");
+    struct sigaction act;
+    act.sa_sigaction = handle_signal_3;
+    act.sa_flags     = SA_SIGINFO | SA_NOCLDWAIT;
+    sigemptyset(&act.sa_mask);
+    sigaction(SIGCHLD, &act, NULL);
+
+    log("阻塞信号 SIGCHLD");
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGCHLD);
+    sigprocmask(SIG_SETMASK, &mask, NULL);
+
+    std::string cmd = "ps -o pid,comm,state -p ";
+
+    for (int i = 1; i <= 5; ++i) {
+        pid_t fd = fork();
+        if (fd == 0) {
+            // 子进程
+            log("子进程启动后退出: ", getpid());
+            exit(-1);
+        } else {
+            // 父进程
+            cmd += std::to_string(fd) + ",";
+            sleep(1);
+        }
+    }
+
+    cmd.pop_back();  // 删除多余的逗号
+
+    log("解除信号 SIGCHLD 的阻塞");
+    sigprocmask(SIG_UNBLOCK, &mask, NULL);
+    sleep(1);
+    log("子进程的状态");
+    system(cmd.data());
+
+    log();
+    log("主进程正常退出");
+    log();
+}
+
+void test_zombie_6() {
+    log();
+    log("预防僵尸进程的产生: 杀死父进程");
+    log();
+
+    if (fork() == 0) {
+        pid_t fd = fork();
+        if (fd == 0) {
+            log("测试的子进程启动: ", getpid());
+            for (;;)
+                ;
+        } else if (fork() == 0) {
+            log("测试的控制进程启动: ", getpid());
+            sleep(1);
+            std::string cmd = "ps -o pid,ppid,state -p " + std::to_string(fd);
+            log("测试的子进程的状态");
+            system(cmd.data());
+            log("杀死测试的父进程: ", getppid());
+            kill(getppid(), SIGKILL);
+            sleep(1);
+            log("测试的子进程的状态");
+            system(cmd.data());
+            log("杀死测试的子进程: ", fd);
+            kill(fd, SIGKILL);
+            sleep(1);
+            log("测试的子进程的状态");
+            system(cmd.data());
+            return;
+        } else {
+            log("测试的父进程启动: ", getpid());
+            for (;;)
+                ;
+        }
+    }
+
+    sleep(4);
+
+    log();
+    log("主进程正常退出");
+    log();
+}
+
+void test_zombie_7() {
+    log();
+    log("销毁僵尸进程: 杀死僵尸进程的父进程");
+    log();
+
+    if (fork() == 0) {
+        pid_t fd = fork();
+
+        if (fd == 0) {
+            log("测试的子进程启动: ", getpid());
+            for (;;)
+                ;
+        } else if (fork() == 0) {
+            log("测试的控制进程启动: ", getpid());
+            sleep(1);
+            std::string cmd = "ps -o pid,ppid,state -p " + std::to_string(fd);
+            log("测试的子进程的状态");
+            system(cmd.data());
+            log("杀死测试的子进程: ", fd);
+            kill(fd, SIGKILL);
+            sleep(1);
+            log("测试的子进程的状态");
+            system(cmd.data());
+            log("杀死测试的父进程: ", getppid());
+            kill(getppid(), SIGKILL);
+            sleep(1);
+            log("测试的子进程的状态");
+            system(cmd.data());
+            return;
+        } else {
+            log("测试的父进程启动: ", getpid());
+            for (;;)
+                ;
+        }
+    }
+
+    sleep(4);
+
+    log();
+    log("主进程正常退出");
+    log();
+}
+
+void test_zombie_8() {
+    log();
+    log("测试: 产生僵尸进程不退出");
+    log();
+
+    pid_t child = fork();
+
+    if (child == 0) {
+        exit(0);
+    }
+    sleep(1);  // 保证子进程已启动并退出
+    log("产生僵尸进程: ", child);
+    std::string cmd = "ps -o pid,comm,state -p " + std::to_string(child);
+    system(cmd.data());
+
+    log("死循环...");
+    for (;;)
+        ;
+
+    log();
+    log("主进程正常退出");
+    log();
 }
