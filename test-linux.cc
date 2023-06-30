d158a1473c53084e411ac9a3d8a78f81dc07ee9a
liuyunbin <yunbinliu@outlook.com>
2023-06-30 11:14:36 +0800

    整理孤儿进程组的测试

--- test-linux.cc
+++ test-linux.cc
@@ -12,6 +12,7 @@
 #include <unistd.h>
 
 #include <iostream>
+#include <map>
 #include <sstream>
 #include <string>
 
@@ -32,7 +33,8 @@ void test_zombie_5();  // 预防僵尸进程的产生:
 void test_zombie_6();  // 预防僵尸进程的产生: 杀死父进程
 void test_zombie_7();  // 销毁僵尸进程: 杀死僵尸进程的父进程
 void test_zombie_8();  // 测试: 产生僵尸进程不退出
-void test_orphan_process();  // 测试孤儿进程
+void test_orphan_process();        // 测试孤儿进程
+void test_orphan_process_group();  // 测试孤儿进程组
 
 int main() {
     // 测试宏
@@ -75,7 +77,10 @@ int main() {
     // test_zombie_8();
 
     // 测试孤儿进程
-    test_orphan_process();
+    // test_orphan_process();
+
+    // 测试孤儿进程组
+    test_orphan_process_group();
     //    std::cout << "环境变量 PATH: " << getenv("PATH") << std::endl;
     //    printf("123");
     //    if (fork() == 0) {
@@ -660,6 +665,113 @@ void test_orphan_process() {
         } else {
             // 测试的父进程
             log("测试的父进程启动: " + std::to_string(getpid()));
+            for (;;)
+                ;
+        }
+    }
+
+    sleep(3);
+
+    log();
+    log("主进程正常退出");
+    log();
+}
+
+std::map<int, std::string> m{
+    {SIGHUP,    " 1-SIGHUP"   },
+    {SIGINT,    " 2-SIGINT"   },
+    {SIGQUIT,   " 3-SIGQUIT"  },
+    {SIGILL,    " 4-SIGILL"   },
+    {SIGTRAP,   " 5-SIGTRAP"  },
+    {SIGABRT,   " 6-SIGABRT"  },
+    {SIGBUS,    " 7-SIGBUS"   },
+    {SIGFPE,    " 8-SIGFPE"   },
+    {SIGKILL,   " 9-SIGKILL"  },
+    {SIGUSR1,   "10-SIGUSR1"  },
+    {SIGSEGV,   "11-SIGSEGV"  },
+    {SIGUSR2,   "12-SIGUSR2"  },
+    {SIGPIPE,   "13-SIGPIPE"  },
+    {SIGALRM,   "14-SIGALRM"  },
+    {SIGTERM,   "15-SIGTERM"  },
+    {SIGSTKFLT, "16-SIGSTKFLT"},
+    {SIGCHLD,   "17-SIGCHLD"  },
+    {SIGCONT,   "18-SIGCONT"  },
+    {SIGSTOP,   "19-SIGSTOP"  },
+    {SIGTSTP,   "20-SIGTSTP"  },
+    {SIGTTIN,   "21-SIGTTIN"  },
+    {SIGTTOU,   "22-SIGTTOU"  },
+    {SIGURG,    "23-SIGURG"   },
+    {SIGXCPU,   "24-SIGXCPU"  },
+    {SIGXFSZ,   "25-SIGXFSZ"  },
+    {SIGVTALRM, "26-SIGVTALRM"},
+    {SIGPROF,   "27-SIGPROF"  },
+    {SIGWINCH,  "28-SIGWINCH" },
+    {SIGIO,     "29-SIGIO"    },
+    {SIGPWR,    "30-SIGPWR"   },
+    {SIGSYS,    "31-SIGSYS"   }
+};
+
+// 测试孤儿进程组
+void handle_signal(int sig, siginfo_t* sig_info, void*) {
+    log("捕获来自 ", sig_info->si_pid, " 的信号 ", m[sig]);
+}
+
+void test_orphan_process_group() {
+    log();
+    log("测试孤儿进程组");
+    log();
+
+    log("设置信号处理");
+    struct sigaction act;
+    act.sa_sigaction = handle_signal;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = SA_SIGINFO;
+    sigaction(SIGHUP, &act, NULL);
+    sigaction(SIGCONT, &act, NULL);
+
+    pid_t main_pid = getpid();
+    if (fork() == 0) {
+        // 测试的父进程
+        log("测试的父进程启动: " + std::to_string(getpid()));
+        log("设置新的进程组: " + std::to_string(getpid()));
+        setpgid(getpid(), getpid());
+        pid_t child_1 = fork();
+        if (child_1 == 0) {
+            // 测试的第一个子进程
+            log("测试的第一个子进程启动: " + std::to_string(getpid()));
+            log("测试的第一个子进程使自己暂停");
+            kill(getpid(), SIGSTOP);
+            for (;;)
+                ;
+        } else if (fork() == 0) {
+            // 测试的第二个子进程
+            sleep(1);
+            log("测试的第二个子进程启动: " + std::to_string(getpid()));
+            log("进程状态");
+            std::string cmd = "ps -o pid,ppid,pgid,sid,state,comm -p ";
+            cmd += std::to_string(main_pid) + ",";
+            cmd += std::to_string(child_1) + ",";
+            cmd += std::to_string(getpid()) + ",";
+            cmd += std::to_string(getppid());
+            log("进程状态");
+            system(cmd.data());
+            log("杀死测试的父进程: " + std::to_string(getppid()));
+            kill(getppid(), SIGKILL);
+            sleep(1);
+            sleep(1);
+            cmd = "ps -o pid,ppid,pgid,sid,state,comm -p ";
+            cmd += std::to_string(main_pid) + ",";
+            cmd += std::to_string(child_1) + ",";
+            cmd += std::to_string(getpid()) + ",";
+            cmd += std::to_string(getppid());
+            log("进程状态");
+            system(cmd.data());
+            log("杀死测试的第一个子进程: " + std::to_string(child_1));
+            kill(child_1, SIGKILL);
+            log("测试的第二个子进程退出");
+            return;
+        } else {
+            // 父进程
             for (;;)
                 ;
         }
