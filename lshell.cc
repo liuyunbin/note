0e68399296b9de1e9ffaaabf44fe0790cbd44384
Yunbin Liu <yunbinliu@outlook.com>
2023-07-25 13:04:49 +0800

    add ulimit

--- lshell.cc
+++ lshell.cc
@@ -11,7 +11,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/resource.h>
 #include <sys/stat.h>
+#include <sys/time.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <termios.h>
@@ -48,6 +50,7 @@ void do_about();
 void do_exit();
 void do_quit();
 void do_cd();
+void do_ulimit();
 
 int main() {
     init();
@@ -61,10 +64,11 @@ int main() {
 }
 
 void init() {
-    m["about"] = do_about;
-    m["exit"]  = do_exit;
-    m["quit"]  = do_quit;
-    m["cd"]    = do_cd;
+    m["about"]  = do_about;
+    m["exit"]   = do_exit;
+    m["quit"]   = do_quit;
+    m["cd"]     = do_cd;
+    m["ulimit"] = do_ulimit;
 
     user_id            = getuid();
     struct passwd *pwd = getpwuid(user_id);
@@ -245,6 +249,42 @@ void do_cd() {
     }
     if (chdir(p) == -1)
         printf("cd %s : %s\n", p, strerror(errno));
+}
+
+#define LIMIT(name, X)                                                       \
+    {                                                                        \
+        struct rlimit rlim;                                                  \
+        getrlimit(X, &rlim);                                                 \
+        std::string s_cur;                                                   \
+        if (rlim.rlim_cur == RLIM_INFINITY)                                  \
+            s_cur = "unlimited";                                             \
+        else                                                                 \
+            s_cur = std::to_string(rlim.rlim_cur);                           \
+        std::string s_max;                                                   \
+        if (rlim.rlim_max == RLIM_INFINITY)                                  \
+            s_max = "unlimited";                                             \
+        else                                                                 \
+            s_max = std::to_string(rlim.rlim_max);                           \
+        printf("%20s %10s %10s %s\n", #X, s_cur.data(), s_max.data(), name); \
+    }
+
+void do_ulimit() {
+    LIMIT("虚拟内存大小", RLIMIT_AS);
+    LIMIT("core 文件大小", RLIMIT_CORE);
+    LIMIT("CPU 总的时间大小", RLIMIT_CPU);
+    LIMIT("数据段(初始化数据, 未初始化数据, 堆)", RLIMIT_DATA);
+    LIMIT("文件大小", RLIMIT_FSIZE);
+    LIMIT("文件锁的个数", RLIMIT_LOCKS);
+    LIMIT("内存中可以锁定的大小", RLIMIT_MEMLOCK);
+    LIMIT("消息队列的大小", RLIMIT_MSGQUEUE);
+    LIMIT("进程优先级的上限", RLIMIT_NICE);
+    LIMIT("文件描述符的最大限制", RLIMIT_NOFILE);
+    LIMIT("用户的进程线程数", RLIMIT_NPROC);
+    LIMIT("物理内存大小", RLIMIT_RSS);
+    LIMIT("调度的优先级", RLIMIT_RTPRIO);
+    LIMIT("调度时 CPU 的最大耗时 毫秒", RLIMIT_RTTIME);
+    LIMIT("信号队列的长度", RLIMIT_SIGPENDING);
+    LIMIT("栈大小", RLIMIT_STACK);
 }
 
 void run_cmd(char *curr_cmd) {
