f531f8da5fa88730ac382d917f71ae78710f9bac
Yunbin Liu <yunbinliu@outlook.com>
2023-08-03 16:48:18 +0800

    add ls for lshell

--- lshell.cc
+++ lshell.cc
@@ -394,6 +394,122 @@ int do_ulimit(cmd_t &cmd) {
     return 0;
 }
 
+// do_ls
+std::string uid_to_name(uid_t uid) {
+    struct passwd *pw_ptr = getpwuid(uid);
+
+    if (pw_ptr == NULL) {  // 无该 uid 对应的用户
+        return std::to_string(uid);
+    } else {
+        return std::string(pw_ptr->pw_name);
+    }
+}
+
+std::string gid_to_name(gid_t gid) {
+    struct group *grp_ptr = getgrgid(gid);
+
+    if (grp_ptr == NULL) {  // 无该 gid 对应的组
+        return std::to_string(gid);
+    } else {
+        return std::string(grp_ptr->gr_name);
+    }
+}
+
+void mode_to_letters(int mode, char *str) {
+    strcpy(str, "----------");
+
+    // 文件类型
+    if (S_ISDIR(mode))
+        str[0] = 'd';  // 目录
+    if (S_ISCHR(mode))
+        str[0] = 'c';  // 设备文件
+    if (S_ISBLK(mode))
+        str[0] = 'b';  // 块文件
+
+    // 用户权限
+    if (mode & S_IRUSR)
+        str[1] = 'r';
+    if (mode & S_IWUSR)
+        str[2] = 'w';
+    if (mode & S_IXUSR)
+        str[3] = 'x';
+
+    // 组权限
+    if (mode & S_IRGRP)
+        str[4] = 'r';
+    if (mode & S_IWGRP)
+        str[5] = 'w';
+    if (mode & S_IXGRP)
+        str[6] = 'x';
+
+    // 其他人权限
+    if (mode & S_IROTH)
+        str[7] = 'r';
+    if (mode & S_IWOTH)
+        str[8] = 'w';
+    if (mode & S_IXOTH)
+        str[9] = 'x';
+}
+
+void show_file_info(const char *file_name, const struct stat *stat_ptr) {
+    char mode_str[11];
+    mode_to_letters(stat_ptr->st_mode, mode_str);
+
+    printf("%s", mode_str);
+    printf("%4d ", (int)stat_ptr->st_nlink);
+    printf("%-8s ", uid_to_name(stat_ptr->st_uid).data());
+    printf("%-8s ", gid_to_name(stat_ptr->st_gid).data());
+    printf("%8ld ", (long)stat_ptr->st_size);
+    printf("%.12s ", 4 + ctime(&stat_ptr->st_mtime));
+    printf("%s\n", file_name);
+}
+
+int do_stat(const char *file_name, int dir_fd) {
+    struct stat info;
+
+    if (fstatat(dir_fd, file_name, &info, 0) == -1) {
+        perror(file_name);
+        return -1;
+    }
+    show_file_info(file_name, &info);
+    return 0;
+}
+
+int is_directory(const char *name) {
+    struct stat info;
+    if (stat(name, &info) == -1) {
+        perror(name);
+        return -1;
+    }
+    return S_ISDIR(info.st_mode) ? 0 : 1;
+}
+
+int do_ls(cmd_t &cmd) {
+    const char *name;
+    if (cmd.cmd_vec.size() == 1)
+        name = ".";
+    else
+        name = cmd.cmd_vec[1].data();
+    int ret = is_directory(name);
+    if (ret == -1)
+        return -1;
+    if (ret == 0) {  // 目录
+        DIR *dir_ptr = opendir(name);
+        if (dir_ptr == NULL) {  // 无法打开目录
+            perror(name);
+            return -1;
+        }
+        struct dirent *dirent_ptr;
+        while ((dirent_ptr = readdir(dir_ptr)) != NULL)
+            if (do_stat(dirent_ptr->d_name, dirfd(dir_ptr)) == -1)
+                return -1;
+        closedir(dir_ptr);
+        return 0;
+    } else {  // 普通文件
+        return do_stat(name, AT_FDCWD);
+    }
+}
+
 int do_jobs(cmd_t &cmd) {
     sigprocmask(SIG_SETMASK, &mask_child, NULL);  // 阻塞信号
     list_job(-1);
@@ -453,6 +569,7 @@ void init_cmd_builtin() {
     m["jobs"]   = do_jobs;
     m["bg"]     = do_bg;
     m["fg"]     = do_fg;
+    m["ls"]     = do_ls;
 }
 
 void handle_signal(int sig) {
