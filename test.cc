c5701e6cc9edc481bafcbe93b2b26c9377bdc8df
Yunbin Liu <yunbinliu@outlook.com>
2023-08-07 15:15:22 +0800

    添加 计算机操作系统-孤儿进程

--- test.cc
+++ test.cc
@@ -657,44 +657,6 @@ void test_signal() {
  *
  ****************************************/
 
-// 测试孤儿进程
-void test_orphan_process() {
-    log();
-    log("测试孤儿进程");
-    log();
-
-    if (fork() == 0) {
-        if (fork() == 0) {
-            // 测试的子进程
-            sleep(1);
-            log("测试的子进程启动: " + std::to_string(getpid()));
-            std::string cmd = "ps -o pid,ppid,pgid,sid,state,comm -p ";
-            cmd += std::to_string(getpid()) + "," + std::to_string(getppid());
-            log("进程状态");
-            system(cmd.data());
-            log("杀死父进程 " + std::to_string(getppid()));
-            kill(getppid(), SIGKILL);
-            sleep(1);
-            cmd = "ps -o pid,ppid,pgid,sid,state,comm -p ";
-            cmd += std::to_string(getpid()) + "," + std::to_string(getppid());
-            log("进程状态");
-            system(cmd.data());
-            return;
-        } else {
-            // 测试的父进程
-            log("测试的父进程启动: " + std::to_string(getpid()));
-            for (;;)
-                ;
-        }
-    }
-
-    sleep(3);
-
-    log();
-    log("主进程正常退出");
-    log();
-}
-
 // 测试孤儿进程组
 void test_orphan_process_group() {
     log();
