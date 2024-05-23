2af494c308fbf465f855a009375f597f338f3900
Yunbin Liu <yunbinliu@outlook.com>
2024-05-23 09:44:12 +0000

    修改返回结果的处理

--- region-cn/main.py
+++ region-cn/main.py
@@ -39,8 +39,11 @@ def handle_url(url, encode):
 
 # https://www.stats.gov.cn/sj/tjbz/tjyqhdmhcxhfdm/2023/14/07/28/140728202.html -- 村数据
 # 结构: tr class="villagetr"(多) => td(3) (code=td[1].text(), code_villagetr=td[2].text(), name=td[3].text())
-def handle_villagetrs(url, encode):
-    results    = []
+def handle_villagetrs(url, encode, results):
+    if url == "":
+        return
+    results["children"] = []
+    results  = results["children"]
     tree       = handle_url(url, encode)
     villagetrs = tree.xpath("//tr[@class='villagetr']")
     url_base   = os.path.dirname(url) + "/"
@@ -50,14 +53,16 @@ def handle_villagetrs(url, encode):
         villagetr["code_villagetr"] = access(v.xpath("./td[2]//text()"))
         villagetr["name"]           = access(v.xpath("./td[3]//text()"))
         results.append(villagetr)
-    return results
 #print(handle_villagetrs("https://www.stats.gov.cn/sj/tjbz/tjyqhdmhcxhfdm/2023/14/07/28/140728202.html", "utf-8"))
 
 # https://www.stats.gov.cn/sj/tjbz/tjyqhdmhcxhfdm/2023/14/07/140728.html -------- 乡镇数据
 # 结构: tr class="towntr"(多) => td(2) => a (url=td[1].href, code=td[1].text(), name=td[2].text())
 # 返回: 28/140728202.html
-def handle_towns(url, encode):
-    results  = []
+def handle_towns(url, encode, results):
+    if url == "" or True:
+        return
+    results["children"] = []
+    results  = results["children"]
     tree     = handle_url(url, encode)
     towns    = tree.xpath("//tr[@class='towntr']")
     url_base = os.path.dirname(url) + "/"
@@ -66,17 +71,18 @@ def handle_towns(url, encode):
         town["code"] = access(v.xpath("./td[1]//text()"))
         town["name"] = access(v.xpath("./td[2]//text()"))
         url  = access_url(v.xpath("./td[1]/a/@href"), url_base)
-        if url != "":
-            town["children"] = handle_villagetrs(url, encode)
+        handle_villagetrs(url, encode, town)
         results.append(town)
-    return results
 #print(handle_towns("https://www.stats.gov.cn/sj/tjbz/tjyqhdmhcxhfdm/2023/14/07/140728.html", "utf-8"))
 
 # https://www.stats.gov.cn/sj/tjbz/tjyqhdmhcxhfdm/2023/14/1407.html ------------- 县区数据
 # 结构: tr class="countytr"(多) => td(2) => a (url=td[1].href, code=td[1].text(), name=td[2].text())
 # 返回: 07/140728.html
-def handle_countys(url, encode):
-    results  = []
+def handle_countys(url, encode, results):
+    if url == "":
+        return
+    results["children"] = []
+    results  = results["children"]
     tree     = handle_url(url, encode)
     countys  = tree.xpath("//tr[@class='countytr']")
     url_base = os.path.dirname(url) + "/"
@@ -85,17 +91,18 @@ def handle_countys(url, encode):
         county["code"] = access(v.xpath("./td[1]//text()"))
         county["name"] = access(v.xpath("./td[2]//text()"))
         url  = access_url(v.xpath("./td[1]/a/@href"), url_base)
-        if url != "" and False:
-            county["children"] = handle_towns(url, encode)
+        handle_towns(url, encode, county)
         results.append(county)
-    return results
 #print(handle_countys("https://www.stats.gov.cn/sj/tjbz/tjyqhdmhcxhfdm/2023/14/1407.html", "utf-8"))
 
 # https://www.stats.gov.cn/sj/tjbz/tjyqhdmhcxhfdm/2023/14.html ------------------ 市数据
 # 结构: tr class="citytr"(多) => td(2) => a (url=td[1].href, code=td[1].text(), name=td[2].text())
 # 返回: 14/1407.html
-def handle_citys(url, encode):
-    results  = []
+def handle_citys(url, encode, results):
+    if url == "":
+        return
+    results["children"] = []
+    results  = results["children"]
     tree     = handle_url(url, encode)
     citys    = tree.xpath("//tr[@class='citytr']")
     url_base = os.path.dirname(url) + "/"
@@ -104,17 +111,14 @@ def handle_citys(url, encode):
         city["code"] = access(v.xpath("./td[1]/a/text()"))
         city["name"] = access(v.xpath("./td[2]/a/text()"))
         url  = access_url(v.xpath("./td[1]/a/@href"), url_base)
-        if url != "" and False:
-            city["children"] = handle_countys(url, encode)
+        handle_countys(url, encode, city)
         results.append(city)
-    return results
 #print(handle_citys("https://www.stats.gov.cn/sj/tjbz/tjyqhdmhcxhfdm/2023/14.html", "utf-8"))
 
 # https://www.stats.gov.cn/sj/tjbz/tjyqhdmhcxhfdm/2023/index.html --------------- 省数据
 # 结构: tr class="provincetr" => td(多) => a (url=href, name=text())
 # 返回: 14.html
-def handle_provinces(url, encode):
-    results   = []
+def handle_provinces(url, encode, results):
     tree      = handle_url(url, encode)
     provinces = tree.xpath("//tr[@class='provincetr']//a")
     url_base  = os.path.dirname(url) + "/"
@@ -122,10 +126,8 @@ def handle_provinces(url, encode):
         province         = {}
         province["name"] = access(v.xpath("./text()"))
         url  = access_url(v.xpath("./@href"), url_base)
-        if url != "":
-            province["children"] = handle_citys(url, encode)
+        handle_citys(url, encode, province)
         results.append(province)
-    return results
 #print(handle_provinces("https://www.stats.gov.cn/sj/tjbz/tjyqhdmhcxhfdm/2023/index.html", "utf-8"))
 
 # https://www.stats.gov.cn/sj/tjbz/qhdm/ ---------------------------------------- 年份数据
@@ -141,7 +143,8 @@ def handle_years(url, encode):
         encode = "utf-8"
         if year <= "2020":
             encode = "gbk"
-        results = handle_provinces(url, encode)
+        results = []
+        handle_provinces(url, encode, results)
         save_file(year, results)
 
 start_time = time.time()
