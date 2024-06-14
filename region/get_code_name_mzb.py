d9ed383a9f2726c66b3f607f78095f4c3024b4c2
Yunbin Liu <yunbinliu@outlook.com>
2024-06-14 03:27:36 +0000

    mzb-code-name => code-name-mzb

--- region/get_code_name_mzb.py
+++ region/get_code_name_mzb.py
@@ -64,7 +64,7 @@ session = HTMLSession()
 text_maker = html2text.HTML2Text()
 text_maker.ignore_tables  = True
 
-path_name = "mzb-code-name"
+path_name = "code-name-mzb"
 if not os.path.exists(path_name):
     os.makedirs(path_name)
 os.chdir(path_name)
