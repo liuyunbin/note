6b575844712c9037e238a49acaae33e145d02a47
Yunbin Liu <yunbinliu@outlook.com>
2024-06-16 10:53:02 +0000

    添加对空目录的处理

--- region/get_code_name_gov_stats_json_all.sh
+++ region/get_code_name_gov_stats_json_all.sh
@@ -5,8 +5,13 @@ set -ueo pipefail
 mkdir -p code-name-gov-stats-json-all
 
 cd code-name-gov-stats-json-all
-for v in ./*.tgz:
-    tar xf $v
+
+if [[ -f *.tgz ]]; then
+    for v in *.tgz; do
+        tar xf $v
+    done
+fi
+
 cd ..
 
 ./get_code_name_gov_stats_json.py all_data
