e06c648ec85be9e1c6a1a0b23c93e5f2ec008a22
Yunbin Liu <yunbinliu@outlook.com>
2024-06-19 04:14:47 +0000

    修改sql的输出目录

--- region/csv_to_sql_gov_stats.sh
+++ region/csv_to_sql_gov_stats.sh
@@ -3,27 +3,16 @@
 set -ueo pipefail
 
 function log_info() { echo -e "$(date +'%Y-%m-%d %H:%M:%S %z') $@";          }
-function log_warn() { echo -e "$(date +'%Y-%m-%d %H:%M:%S %z') $@";          }
 function log_erro() { echo -e "$(date +'%Y-%m-%d %H:%M:%S %z') $@"; exit -1; }
 
-path_gcov_stats_sql=$(pwd)/code-name-gov-stats-sql-all/
-mkdir -p $path_gcov_stats_sql
-
-cd code-name-gov-stats-csv-all
-
-log_info "解压缩..."
-for v in *; do
-    [[ "$v" =~ ^.*\.tgz$ ]] && tar xf $v || true
-done
-
-for file_name_csv in *.csv; do
+for file_name_csv in ./gov-stats-*.csv; do
     log_info "handle $file_name_csv..."
-    year=${file_name_csv::-4}
-    file_name_sql=${path_gcov_stats_sql}${year}.sql
+    year=${file_name_csv:12:4}
+    file_name_sql=${file_name_csv/csv/sql}
 
     cat > $file_name_sql <<'EOF'
-drop table if exists `gov_stats_all`;
-create table `gov_stats_all` (
+drop table if exists `gov_stats`;
+create table `gov_stats` (
   `code`     bigint      not null comment '区划代码',
   `name`     varchar(40) not null comment '名称',
   `level`    tinyint     not null comment '级别1-5,省市县镇村',
@@ -36,18 +25,18 @@ create table `gov_stats_all` (
    index `category` (`category`)
 ) engine=innodb default charset=utf8mb4 collate=utf8mb4_unicode_ci;
 
-lock tables `gov_stats_all` write;
+lock tables `gov_stats` write;
 EOF
 
     awk -F, '
         BEGIN {
-            str = "insert into `gov_stats_all` values "
+            str = "insert into `gov_stats` values "
         }
 
         {
             if (NR % 1000 == 0) {
                 print(str)
-                str = "insert into `gov_stats_all` values "
+                str = "insert into `gov_stats` values "
             }
 
             str = str"("$1",'\''"$2"'\'',"$3","$4","$5"),"
@@ -56,21 +45,9 @@ EOF
             print(str)
         }
         ' $file_name_csv | sed 's/,$/;/' >> $file_name_sql
-    sed -i "s/gov_stats_all/gov_stats_all_$year/g" $file_name_sql
+    sed -i "s/gov_stats/gov_stats_$year/g" $file_name_sql
 
     mysql -D testdb < $file_name_sql
-    mysqldump testdb gov_stats_all_$year > $file_name_sql
-done
-
-log_info "压缩..."
-cd $path_gcov_stats_sql
-if [[ -f *.sql ]]; then
-    for v in *.sql; do
-        tar acf $v.tgz $v
-    done
-fi
-
-for v in *; do
-    [[ $v =~ ^.*\.sql$ ]] && tar acf $v.tgz $v || true
+    mysqldump testdb gov_stats_$year > $file_name_sql
 done
 
