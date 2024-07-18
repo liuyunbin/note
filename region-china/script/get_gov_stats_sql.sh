86353f17afb78533c62798f53ced81f4803a0496
Yunbin Liu <yunbinliu@outlook.com>
2024-07-18 03:24:11 +0000

    use gov-stats-sql

--- region-china/script/get_gov_stats_sql.sh
+++ region-china/script/get_gov_stats_sql.sh
@@ -5,12 +5,42 @@ set -ueo pipefail
 function log_info() { echo -e "$(date +'%Y-%m-%d %H:%M:%S %z') $@";          }
 function log_erro() { echo -e "$(date +'%Y-%m-%d %H:%M:%S %z') $@"; exit -1; }
 
-for file_name_csv in ./gov-stats-*.csv; do
-    log_info "handle $file_name_csv..."
-    year=${file_name_csv:12:4}
-    file_name_sql=${file_name_csv/csv/sql}
+path_sql=$(pwd)/gov-stats-sql
+path_csv=$(pwd)/gov-stats-csv
 
-    cat > $file_name_sql <<'EOF'
+database_name=testdb # 需要在数据库中添加库 testdb
+
+mkdir -p $path_sql &> /dev/null
+
+cd $path_csv
+for file_name_csv_tgz in *.csv.tgz; do
+    year=${file_name_csv_tgz%%.*}
+    file_name_csv=${file_name_csv_tgz%.*}
+    file_name_sql=${file_name_csv/csv/sql}
+    file_name_sql_tgz=${file_name_csv_tgz/csv/sql}
+    file_name_sql_abs=$path_sql/$file_name_sql
+    file_name_sql_tgz_abs=$path_sql/$file_name_sql_tgz
+
+    log_info "handle $file_name_csv_tgz..."
+
+    if [[ -f $file_name_sql_tgz_abs ]]; then
+        log_info "$file_name_sql_tgz 已存在, 解压后存入数据库..."
+        [[ -f $file_name_sql_abs ]] || tar xf $file_name_sql_tgz_abs -C $path_sql
+        mysql -D $database_name < $file_name_sql_abs
+        continue
+    fi
+
+    if [[ -f $file_name_sql_abs ]]; then
+        log_info "$file_name_sql 已存在, 生成压缩文件, 存入数据库..."
+        (cd $path_sql && tar acf $file_name_sql_tgz $file_name_sql)
+        mysql -D $database_name < $file_name_sql_abs
+        continue
+    fi
+
+    log_info "获取 $file_name_sql_tgz..."
+    [[ -f $file_name_csv ]] || tar xf $file_name_csv_tgz
+
+    cat > $file_name_sql_abs <<'EOF'
 drop table if exists `gov_stats`;
 create table `gov_stats` (
   `code`     bigint      not null comment '区划代码',
@@ -29,7 +59,7 @@ create table `gov_stats` (
    index `county`   (`county`),
    index `town`     (`town`),
    index `category` (`category`)
-) engine=innodb default charset=utf8mb4 collate=utf8mb4_unicode_ci;
+);
 
 lock tables `gov_stats` write;
 EOF
@@ -50,10 +80,13 @@ EOF
         END {
             print(str)
         }
-        ' $file_name_csv | sed 's/,$/;/' >> $file_name_sql
-    sed -i "s/gov_stats/gov_stats_$year/g" $file_name_sql
+        ' $file_name_csv | sed 's/,$/;/' >> $file_name_sql_abs
+    sed -i "s/gov_stats/gov_stats_$year/g" $file_name_sql_abs
 
-    mysql -D testdb < $file_name_sql
-    mysqldump testdb gov_stats_$year > $file_name_sql
+    mysql -D $database_name < $file_name_sql_abs
+    mysqldump $database_name gov_stats_$year > $file_name_sql_abs
+    (cd $path_sql && tar acf $file_name_sql_tgz $file_name_sql)
 done
+
+log_info "完成"
 
