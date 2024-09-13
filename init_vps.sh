273517b1850b48d9b7c171f2d8f3a02906dd717d
Yunbin Liu <yunbinliu@outlook.com>
2024-09-13 05:37:17 +0000

    update init_vps.sh

--- init_vps.sh
+++ init_vps.sh
@@ -12,87 +12,62 @@ PASS=lyb2636196546
 PORT=443
 SERVICE=https
 
-function handle_certbot() {
-    log_info "安装 certbot..."
-    if ! which certbot &> /dev/null; then
-        apt install snapd &> /dev/null
-        snap install --classic certbot &> /dev/null
-        ln -s /snap/bin/certbot /usr/bin/certbot
-    fi
-#    log_info "签证书: $DOMAIN"
-    if ! certbot certificates 2> /dev/null | grep "$DOMAIN" &> /dev/null;then
-        certbot certonly --standalone -d "$DOMAIN" &> /dev/null
-    fi
-}
+# 1. 安装常用软件
+log_info "1. 安装常用软件"
+apt -y update                       # 更新软件源
+apt -y upgrade                      # 更新软件
+apt install -y git vim lrzsz icdiff # 安装常用软件
+#python3-pip
+apt -y autoremove                   # 卸载多余的软件
 
-function handle_docker() {
-    log_info "安装 docker..."
-    if ! which docker &> /dev/null; then
-        curl -fsSL https://get.docker.com -o get-docker.sh &> /dev/null
-        sh get-docker.sh &> /dev/null
-        rm -rf get-docker.sh
-    fi
-    systemctl start  docker &> /dev/null || true
-    systemctl enable docker &> /dev/null
-}
+# 2. 安装 certbot, 签证书
+log_info "2. 安装 certbot, 签证书"
+apt install -y certbot
+certbot certificates | grep "$DOMAIN" || certbot certonly --standalone -d "$DOMAIN"
 
-function handle_gost() {
-    log_info "安装 gost..."
-    if ! docker ps -a --format "{{.Names}}" | grep gost &> /dev/null; then
-        BIND_IP=0.0.0.0
-        CERT_DIR=/etc/letsencrypt
-        CERT=${CERT_DIR}/live/${DOMAIN}/fullchain.pem
-        KEY=${CERT_DIR}/live/${DOMAIN}/privkey.pem
+# 3. 安装 docker
+log_info "3. 安装 docker"
+which docker || bash <(curl -fsSL https://get.docker.com)
+systemctl enable docker
 
-        docker run -d \
-            --name gost \
-            -v ${CERT_DIR}:${CERT_DIR}:ro \
-            --net=host \
-            ginuerzh/gost -L "http2://${USER}:${PASS}@${BIND_IP}:${PORT}?cert=${CERT}&key=${KEY}&probe_resist=code:400&knock=www.google.com"
-    fi
+# 4. 安装 gost
+log_info "4. 安装 gost"
+if ! docker ps -a --format "{{.Names}}" | grep gost; then
+    BIND_IP=0.0.0.0
+    CERT_DIR=/etc/letsencrypt
+    CERT=${CERT_DIR}/live/${DOMAIN}/fullchain.pem
+    KEY=${CERT_DIR}/live/${DOMAIN}/privkey.pem
 
-    log_info "启动 gost..."
-    if ! docker ps --format "{{.Names}}" | grep gost &> /dev/null; then
-        docker start gost &> /dev/null
-    fi
-}
+    docker run -d \
+        --name gost \
+        -v ${CERT_DIR}:${CERT_DIR}:ro \
+        --net=host \
+        ginuerzh/gost -L "http2://${USER}:${PASS}@${BIND_IP}:${PORT}?cert=${CERT}&key=${KEY}&probe_resist=code:400&knock=www.google.com"
+fi
 
-function handle_firewalld() {
-    log_info "处理防火墙..."
-    apt install -y   firewalld &> /dev/null
-    systemctl start  firewalld &> /dev/null || true
-    systemctl enable firewalld &> /dev/null
-    firewall-cmd --add-service=${SERVICE} --permanent --zone=public &> /dev/null
-    firewall-cmd --reload > /dev/null
-}
+docker ps --format "{{.Names}}" | grep gost || docker start gost
 
-function handle_crontab() {
-    log_info "添加定时任务: 更新证书..."
-    cmd="certbot renew --force-renewal"
-    if ! crontab -l 2> /dev/null | grep "$cmd" &>/dev/null; then
-        echo "0 0 1 * * $cmd" >> /var/spool/cron/crontabs/root
-    fi
+# 5. 处理防火墙
+log_info "5. 处理防火墙"
+apt install -y   firewalld
+systemctl enable firewalld
+firewall-cmd --add-service=${SERVICE} --permanent --zone=public
+firewall-cmd --reload
 
-    log_info "添加定时任务: 重启 gost..."
-    cmd="docker restart gost"
-    if ! crontab -l 2> /dev/null | grep "$cmd" &>/dev/null; then
-        echo "5 0 1 * * $cmd" >> /var/spool/cron/crontabs/root
-    fi
-}
+# 6. 添加定时任务
+log_info "6. 添加定时任务"
+apt install -y cron
 
-log_info "更新系统..."
-apt -y update  &> /dev/null
-apt -y upgrade &> /dev/null
-log_info "安装常用软件..."
-apt install -y git vim python3-pip lrzsz icdiff &> /dev/null
+cmd="certbot renew --force-renewal"
+crontab -l | grep "$cmd" || echo "0 0 1 * * $cmd" >> /var/spool/cron/crontabs/root
 
-handle_certbot
-handle_docker
-handle_gost
-handle_firewalld
-handle_crontab
+cmd="docker restart gost"
+crontab -l | grep "$cmd" || echo "5 0 1 * * $cmd" >> /var/spool/cron/crontabs/root
 
-#    apt install -y man-db
-#    unminimize -y
+# 7. 安装 man 文档
+log_info "7. 安装 man 文档"
+apt install -y man-db
+unminimize
+
 log_info "完成"
 
