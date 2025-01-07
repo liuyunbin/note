2028f30c0f24540034358ecd27051f18af1b57cf
Yunbin Liu <yunbinliu@outlook.com>
2025-01-07 10:14:03 +0000

    add yum

---
+++ proxy/yum.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ueo pipefail
+
+function log_info() { echo -e "$(date +'%Y-%m-%d %H:%M:%S %z') $@" > /dev/tty;          }
+function log_erro() { echo -e "$(date +'%Y-%m-%d %H:%M:%S %z') $@" > /dev/tty; exit -1; }
+
+function do_test() {
+    sudo yum clean all;            # 清空软件源缓存
+    sed -i "s/proxy=.*/proxy=$1/g" /etc/yum.conf
+    sudo yum makecache;            # 新建软件源缓存
+}
+
+do_test http://host-60:8001
+
+
+
