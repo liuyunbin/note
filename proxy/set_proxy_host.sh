a873a2ba4579be9d26669044afe2812cda9ee09b
Yunbin Liu <yunbinliu@outlook.com>
2025-01-08 05:07:51 +0000

    update set_proxy_host.sh

--- proxy/set_proxy_host.sh
+++ proxy/set_proxy_host.sh
@@ -5,28 +5,18 @@ set -ueo pipefail
 function log_info() { echo -e "$(date +'%Y-%m-%d %H:%M:%S %z') $@" > /dev/tty;          }
 function log_erro() { echo -e "$(date +'%Y-%m-%d %H:%M:%S %z') $@" > /dev/tty; exit -1; }
 
-log_info "1. 安装 gost..."
-if which gost &> /dev/null; then
-    log_info "gost 已安装, 跳过"
-else
-    sudo snap install core;
-    sudo snap install gost;
-    log_info "gost 安装完成"
-fi
-
-log_info "2. 处理防火墙端口"
-if sudo firewall-cmd --query-port=8001-8008/tcp &> /dev/null; then
-    log_info "防火墙端口已开启, 跳过"
-else
-    sudo firewall-cmd --add-port=8001-8008/tcp;
-    log_info "防火墙端口开启完成"
-fi
-
-log_info "3. 启动代理, 注意修改域名..."
 DOMAIN=yunbinliu.com
 USER=admin
 PASS=123456
 
+log_info "1. 安装 gost..."
+which gost &> /dev/null || sudo snap install core gost
+
+log_info "2. 处理防火墙..."
+which firewall-cmd &> /dev/null || sudo apt install -y firewalld
+sudo firewall-cmd --add-port=8001-8008/tcp &> /dev/null
+
+log_info "3. 启动代理..."
 gost -L http://:8001                \
      -L http://$USER:$PASS@:8002    \
      -L socks4://:8003              \
