62085dc6ce22cf1c44aa96b2aee1ed5af4d43f23
Yunbin Liu <yunbinliu@outlook.com>
2025-01-08 00:35:48 +0000

    update yum.sh

--- proxy/yum.sh
+++ proxy/yum.sh
@@ -7,7 +7,7 @@ function log_erro() { echo -e "$(date +'%Y-%m-%d %H:%M:%S %z') $@" > /dev/tty; e
 
 function do_test() {
     sudo yum clean all;            # 清空软件源缓存
-    sed -i "s/proxy=.*/proxy=$1/g" /etc/yum.conf
+    sudo sed -i "s|proxy=.*|proxy=$1|g" /etc/yum.conf
     sudo yum makecache;            # 新建软件源缓存
 }
 
