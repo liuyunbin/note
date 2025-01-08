67a6c5573fa5ccf5a83f00f6974ec3afba8f9b38
Yunbin Liu <yunbinliu@outlook.com>
2025-01-08 13:13:03 +0800

    update set_proxy_host.sh

--- proxy/set_proxy_host.sh
+++ proxy/set_proxy_host.sh
@@ -10,10 +10,10 @@ USER=admin
 PASS=123456
 
 log_info "1. 安装 gost..."
-which gost &> /dev/null || sudo snap install core gost
+which gost &> /dev/null || sudo snap install core gost &> /dev/null
 
 log_info "2. 处理防火墙..."
-which firewall-cmd &> /dev/null || sudo apt install -y firewalld
+which firewall-cmd &> /dev/null || sudo apt install -y firewalld &> /dev/null
 sudo firewall-cmd --add-port=8001-8008/tcp &> /dev/null
 
 log_info "3. 启动代理..."
