110c20a7c0402a9247c4f01a43ba60b7af0c955b
Yunbin Liu <yunbinliu@outlook.com>
2025-06-19 09:54:39 +0800

    update 1.sh

--- python/001-天眼查/1.sh
+++ python/001-天眼查/1.sh
@@ -1,6 +1,13 @@
 
-year=2024
 
-cat $year-*.csv | sort -k 1,3 > $year.csv
+for year in {2010..2030}; do
+    echo -n "handle $year..."
+    if [[ -f $year-*.csv ]]; then
+        cat $year-*.csv | sort -k 1,3 >> $year.csv
+        mv $year-*.csv old
+        echo "done"
+    else
+        echo "skip"
+    fi
+done
 
-mv $year-*.csv old
