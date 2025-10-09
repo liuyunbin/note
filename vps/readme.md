cddc5f68f3f829aea2a90255a973d5338b6ffbf9
Yunbin Liu <yunbinliu@outlook.com>
2025-10-09 05:59:22 +0000

    use vim

--- vps/readme.md
+++ vps/readme.md
@@ -3,7 +3,8 @@
 ```
 ./user.sh  # 1. 新建用户
 ./git.sh   # 2. 初始化 git
-./bash.sh  # 3. 初始化 bashrc
+./bash.sh  # 3. 配置 bashrc
+./vim.sh   # 4. 配置 vimrc
 
 
 
---
+++ vps/vim.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ueo pipefail
+
+function log_info() { echo -e "$(date +'%Y-%m-%d %H:%M:%S %z') $@" > /dev/tty;          }
+function log_erro() { echo -e "$(date +'%Y-%m-%d %H:%M:%S %z') $@" > /dev/tty; exit -1; }
+
+log_info "1. 配置 vimrc"
+cp ~/github/note/vim/vimrc ~/.vimrc
+
