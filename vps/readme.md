36f28550b5cbf6636c11305563e4d77b3b93126b
Yunbin Liu <yunbinliu@outlook.com>
2025-10-09 06:02:57 +0000

    update vim

--- vps/readme.md
+++ vps/readme.md
@@ -3,8 +3,8 @@
 ```
 ./user.sh  # 1. 新建用户
 ./git.sh   # 2. 初始化 git
-./bash.sh  # 3. 配置 bashrc
-./vim.sh   # 4. 配置 vimrc
+./bash.sh  # 3. 初始化 bashrc
+./vim.sh   # 4. 初始化 vimrc
 
 
 
--- vps/vim.sh
+++ vps/vim.sh
@@ -5,6 +5,9 @@ set -ueo pipefail
 function log_info() { echo -e "$(date +'%Y-%m-%d %H:%M:%S %z') $@" > /dev/tty;          }
 function log_erro() { echo -e "$(date +'%Y-%m-%d %H:%M:%S %z') $@" > /dev/tty; exit -1; }
 
+log_info "0. 安装 vim"
+sudo apt -y -qq install vim &> /dev/null
+
 log_info "1. 配置 vimrc"
 cp ~/github/note/vim/vimrc ~/.vimrc
 
