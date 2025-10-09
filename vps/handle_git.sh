0798d84805c6bac96de7cda7731342f94e9d1f1a
Yunbin Liu <yunbinliu@outlook.com>
2025-10-09 05:17:42 +0000

    handle git

---
+++ vps/handle_git.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -ueo pipefail
+
+function log_info() { echo -e "$(date +'%Y-%m-%d %H:%M:%S %z') $@" > /dev/tty;          }
+function log_erro() { echo -e "$(date +'%Y-%m-%d %H:%M:%S %z') $@" > /dev/tty; exit -1; }
+
+../git/init.sh
+
