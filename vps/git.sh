6fce9b31b67fdec2ba70d40278dc7e63878577ba
Yunbin Liu <yunbinliu@outlook.com>
2025-12-30 05:42:13 +0000

    rm -rf vps/git.sh

--- vps/git.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ueo pipefail
-
-function log_info() { echo -e "$(date +'%Y-%m-%d %H:%M:%S %z') $@" > /dev/tty;          }
-function log_erro() { echo -e "$(date +'%Y-%m-%d %H:%M:%S %z') $@" > /dev/tty; exit -1; }
-
-../git/init.sh
-
