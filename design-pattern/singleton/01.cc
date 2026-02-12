3ed9a610a7b090c1a7d2eb7bd33a28cd73780ce4
Yunbin Liu <yunbinliu@outlook.com>
2026-02-12 08:09:37 +0000

    实现第一版本

---
+++ design-pattern/singleton/01.cc
@@ -0,0 +1,30 @@
+
+#include <iostream>
+
+class singleton {
+public:
+    static singleton& instance() {
+        std::cout << "构造函数" << std::endl;
+        return ins;
+    }
+
+protected:
+    singleton() {
+        std::cout << "析构函数" << std::endl;
+    }
+
+    singleton(const singleton&) = delete;
+    singleton(singleton&&)      = delete;
+
+    singleton& operator=(const singleton&) = delete;
+    singleton& operator=(singleton&&)      = delete;
+
+    static singleton ins;
+};
+
+singleton singleton::ins;
+
+int main() {
+    auto& v = singleton::instance();
+    return 0;
+}
