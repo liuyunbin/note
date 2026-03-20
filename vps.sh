49a946829981982a9f27094ea8a70c9090bbf883
Yunbin Liu <yunbinliu@outlook.com>
2026-03-20 10:10:47 +0000

    重构

--- vps.sh
+++ vps.sh
@@ -6,8 +6,7 @@ set -ueo pipefail # 使用未初始化的变量, 命令报错, 管道报错, 直
 
 exec &>/dev/null  # 禁止命令输出
 
-function log_info() { echo -e "$(date +'%Y-%m-%d %H:%M:%S %z') $@" > /dev/tty;          }
-function log_erro() { echo -e "$(date +'%Y-%m-%d %H:%M:%S %z') $@" > /dev/tty; exit -1; }
+function log() { echo -e "$(date +'%Y-%m-%d %H:%M:%S %z') $@" > /dev/tty; }
 
 DOMAIN=yunbinliu.com       # 域名
 USER=yunbinliu             # 用户名
@@ -16,116 +15,161 @@ PORT=442                   # gost 端口号
 SERVICE=https              # 更新域名使用的服务
 MAIL=yunbinliu@outlook.com # 邮箱
 
-log_info "1. 更新系统"
-log_info "1.1 更新软件源"
-apt -y update
+function update_os() {
+    apt -y update     # 更新软件源
+    apt -y upgrade    # 更新软件
+    apt -y autoremove # 卸载没用的软件
+}
 
-log_info "1.2 更新软件"
-apt -y upgrade
+function handle_cerbot() {
+    apt -y install certbot
+    certbot certificates | grep $DOMAIN || certbot certonly --standalone --agree-tos -n -m $MAIL -d $DOMAIN
+}
 
-log_info "1.3 自动卸载没用的软件"
-apt -y autoremove
+function handle_docker() {
+    which docker || bash <(curl -fsSL https://get.docker.com)
+    systemctl enable docker
+}
 
-log_info "2. 安装软件"
-log_info "2.1 安装常用软件"
-apt -y install certbot firewalld cron man-db g++ vim lrzsz man-db git icdiff
+function handle_gost() {
+    if ! docker ps -a --format "{{.Names}}" | grep gost; then
+        BIND_IP=0.0.0.0                                # 绑定的IP
+        CERT_DIR=/etc/letsencrypt                      # 证书的目录
+        CERT=${CERT_DIR}/live/${DOMAIN}/fullchain.pem  # 证书的公钥
+        KEY=${CERT_DIR}/live/${DOMAIN}/privkey.pem     # 证书的私钥
 
-log_info "2.2 安装 docker"
-which docker || bash <(curl -fsSL https://get.docker.com)
+        docker run -d \
+            --name gost \
+            -v ${CERT_DIR}:${CERT_DIR}:ro \
+            --net=host \
+            ginuerzh/gost -L "http2://${USER}:${PASS}@${BIND_IP}:${PORT}?cert=${CERT}&key=${KEY}&probe_resist=code:400&knock=www.google.com"
+    fi
 
-log_info "2.3 解压 man-db"
-echo y | unminimize
+    docker ps --format "{{.Names}}" | grep gost || docker start gost # 启动代理
+}
 
-log_info "3. 设置 docker firewalld 开机自动启动"
-systemctl enable docker firewalld
+function handle_crontab() {
+    apt -y install cron
 
-log_info "4. 处理防火墙"
-log_info "4.1 开启端口号 ${PORT} 和 服务 ${SERVICE}"
-firewall-cmd --permanent --zone=public --add-port=${PORT}/tcp   # 代理
-firewall-cmd --permanent --zone=public --add-service=${SERVICE} # 更新证书
+    cmd="$(which certbot) renew --force-renewal"
+    crontab -l | grep "$cmd" || echo "0 0 1 * * $cmd" >> /var/spool/cron/crontabs/root
 
-log_info "4.2 重新加载防火墙"
-firewall-cmd --reload
+    cmd="$(which docker) restart gost"
+    crontab -l | grep "$cmd" || echo "5 0 1 * * $cmd" >> /var/spool/cron/crontabs/root
+}
 
-log_info "5. 初始化 Git"
-log_info "5.1 配置用户名"
-git config --global user.name "Yunbin Liu"
+function handle_firewalld() {
+    apt -y install firewalld
+    systemctl enable firewalld
 
-log_info "5.2 配置邮箱"
-git config --global user.email yunbinliu@outlook.com
+    firewall-cmd --permanent --zone=public --add-port=${PORT}/tcp   # 代理
+    firewall-cmd --permanent --zone=public --add-service=${SERVICE} # 更新证书
+    firewall-cmd --reload
+}
 
-log_info "5.3 配置编辑器"
-git config --global core.editor vim
+function handle_git() {
+    apt -y install git icdiff
 
-log_info "5.4 配置日期格式"
-git config --global log.date iso
+    git config --global user.name "Yunbin Liu"           # 配置用户名
+    git config --global user.email yunbinliu@outlook.com # 配置邮箱
+    git config --global core.editor vim                  # 配置编辑器
+    git config --global log.date iso                     # 配置日期格式
+    git config --global alias.co checkout                # 配置简要命令
+    git config --global alias.br branch
+    git config --global alias.ci commit
+    git config --global alias.st status
+    git config --global alias.lg "log --pretty=format:'%ad %h %s %d %C(bold)%an%Creset' --graph"
+                                                         # 配置日志显示格式
+    git config --global core.quotepath false             # 配置中文显示不乱码
+    git config --global --list                           # 查看配置
+    [[ -f ~/.ssh/id_ed25519.pub ]] || ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N "" # 生成秘钥
 
-log_info "5.5 配置简要命令"
-git config --global alias.co checkout
-git config --global alias.br branch
-git config --global alias.ci commit
-git config --global alias.st status
+    log "复制公钥到 GitHub, 按回车继续..."
+    cat ~/.ssh/id_ed25519.pub > /dev/tty
+    read < /dev/tty
 
-log_info "5.6 配置日志显示格式"
-git config --global alias.lg "log --pretty=format:'%ad %h %s %d %C(bold)%an%Creset' --graph"
+    log "测试连接"
+    ssh -T git@github.com -o BatchMode=yes -o StrictHostKeyChecking=no &> /dev/tty || true
+}
 
-log_info "5.7 中文显示不乱码"
-git config --global core.quotepath false
+function handle_man() {
+    apt -y install g++ vim lrzsz man-db
+    echo y | unminimize
+}
 
-log_info "5.8 查看配置"
-git config --global --list
+function handle_new_user() {
+    useradd -m -s /bin/bash lyb || true # 添加用户
+    echo lyb:654321 | chpasswd          # 设置密码
 
-log_info "5.9 生成秘钥"
-[[ -f ~/.ssh/id_ed25519.pub ]] || ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N ""
+    chmod +w /etc/sudoers
+    grep lyb /etc/sudoers || echo "lyb ALL=(ALL:ALL) ALL" >> /etc/sudoers # 赋予 sudo 权限
+    chmod -w /etc/sudoers
 
-log_info "5.10 复制公钥到 GitHub, 按回车继续..."
-read
+    grep lyb /etc/ssh/sshd_config || echo "DenyUsers lyb" >> /etc/ssh/sshd_config # 禁止使用 ssh 登录
+    systemctl restart ssh
+}
 
-log_info "5.11 测试连接"
-ssh -T git@github.com -o BatchMode=yes -o StrictHostKeyChecking=no &> /dev/tty || true
+function handle_note() {
+    mkdir -p ~/github
+    cd ~/github
+    [[ -d note ]] || git clone git@github.com:liuyunbin/note
+}
 
-log_info "6. 克隆仓库 note"
-mkdir -p ~/github
-cd ~/github
-[[ -d note ]] || git clone git@github.com:liuyunbin/note
+function handle_vim() {
+    cp ~/github/note/vimrc ~/.vimrc
+}
 
-log_info "7. 配置 vim"
-cp ~/github/note/vimrc ~/.vimrc
+function handle_bashrc() {
+    grep github/note/bashrc /etc/ssh/sshd_config || echo ". ~/github/note/bashrc" >> ~/.bashrc
+    log "请执行: . ~/.bashrc"
+}
 
-log_info "8. 配置 bashrc"
-log_info "8.1 配置"
-echo ". ~/github/note/bashrc" >> ~/.bashrc
-log_info "8.2 请执行: . ~/.bashrc"
+log "01. 更新系统"
+update_os
 
-log_info "9. 申请证书"
-certbot certificates | grep $DOMAIN || certbot certonly --standalone --agree-tos -n -m $MAIL -d $DOMAIN
+log "02. 申请证书"
+handle_cerbot
 
-log_info "10. 处理 gost"
-log_info "10.1 配置代理"
-if ! docker ps -a --format "{{.Names}}" | grep gost; then
-    BIND_IP=0.0.0.0                                # 绑定的IP
-    CERT_DIR=/etc/letsencrypt                      # 证书的目录
-    CERT=${CERT_DIR}/live/${DOMAIN}/fullchain.pem  # 证书的公钥
-    KEY=${CERT_DIR}/live/${DOMAIN}/privkey.pem     # 证书的私钥
+log "03. 处理 docker"
+handle_docker
 
-    docker run -d \
-        --name gost \
-        -v ${CERT_DIR}:${CERT_DIR}:ro \
-        --net=host \
-        ginuerzh/gost -L "http2://${USER}:${PASS}@${BIND_IP}:${PORT}?cert=${CERT}&key=${KEY}&probe_resist=code:400&knock=www.google.com"
-fi
+log "04. 处理 gost"
+handle_gost
 
-log_info "10.2 启动代理"
-docker ps --format "{{.Names}}" | grep gost || docker start gost
+log "05. 添加定时任务"
+handle_crontab
 
-log_info "11. 添加定时任务"
-log_info "11.1 添加任务: 更新证书, 注意, 更新证书时, 端口号不能被占用"
-cmd="$(which certbot) renew --force-renewal"
-crontab -l | grep "$cmd" || echo "0 0 1 * * $cmd" >> /var/spool/cron/crontabs/root
+log "06. 处理防火墙"
+handle_firewalld
 
-log_info "11.2 添加任务: 重启 gost"
-cmd="$(which docker) restart gost"
-crontab -l | grep "$cmd" || echo "5 0 1 * * $cmd" >> /var/spool/cron/crontabs/root
+log "07. 处理 git"
+handle_git
 
-log_info "12. 完成"
+log "08. 处理 man 等其他命令"
+handle_man
+
+log "09. 新建用户"
+handle_new_user
+
+log "10. 切换用户"
+
+sudo -u lyb bash <<EOF
+    $(declare -f log)
+    $(declare -f handle_note)
+    $(declare -f handle_vim)
+    $(declare -f handle_bash)
+
+    log "11. 克隆仓库 note"
+    handle_note
+
+    log "12. 配置 vim"
+    handle_vim
+
+    log "13. 配置 bashrc"
+    handle_bashrc
+
+    true
+EOF
+
+log "14. 完成"
 
