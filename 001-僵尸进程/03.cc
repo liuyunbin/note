24163c594bae9a7b70a1f434025ab9f0aaffe168
Yunbin Liu <yunbinliu@outlook.com>
2026-09-04 09:40:39 +0000

    Revert "update 03.cc"
    This reverts commit 5b7d9b0fe003d791a94d7f2c3f1029dbc8cd2ff2.

--- 001-僵尸进程/03.cc
+++ 001-僵尸进程/03.cc
@@ -7,7 +7,7 @@
 #include <iostream>
 #include <string>
 
-void log(const std::string& msg) {
+void log(const std::string& msg = "") {
     time_t     now  = time(NULL);
     struct tm* info = localtime(&now);
     char       buf[1024];
@@ -16,7 +16,9 @@ void log(const std::string& msg) {
 }
 
 int main() {
-    log("僵尸进程-预防: SIGCHLD 处理为 SIG_IGN");
+    log();
+    log("计算机操作系统-僵尸进程-预防: SIGCHLD 处理为 SIG_IGN");
+    log();
 
     log("设置 SIGCHLD 的信号处理");
     signal(SIGCHLD, SIG_IGN);
@@ -51,6 +53,8 @@ int main() {
     log("子进程的状态");
     system(cmd.data());
 
+    log();
     log("主进程正常退出");
+    log();
     return 0;
 }
