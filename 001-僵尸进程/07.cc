d5dc5083c06b66947949421455946db892a87df4
Yunbin Liu <yunbinliu@outlook.com>
2026-09-04 09:40:31 +0000

    Revert "update 07.cc"
    This reverts commit 493c5c7cd05134cad60172ab72c91f130e6b5f56.

--- 001-僵尸进程/07.cc
+++ 001-僵尸进程/07.cc
@@ -7,7 +7,7 @@
 #include <iostream>
 #include <string>
 
-void log(const std::string& msg) {
+void log(const std::string& msg = "") {
     time_t     now  = time(NULL);
     struct tm* info = localtime(&now);
     char       buf[1024];
@@ -16,7 +16,9 @@ void log(const std::string& msg) {
 }
 
 int main() {
-    log("僵尸进程-销毁: 杀死僵尸进程的父进程");
+    log();
+    log("计算机操作系统-僵尸进程-销毁: 杀死僵尸进程的父进程");
+    log();
 
     if (fork() == 0) {
         pid_t fd = fork();
@@ -52,6 +54,8 @@ int main() {
 
     sleep(4);
 
+    log();
     log("主进程正常退出");
+    log();
     return 0;
 }
