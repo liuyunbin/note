465ce4a234033e6df11e93256f92f62884610c4e
Yunbin Liu <yunbinliu@outlook.com>
2026-09-04 07:41:27 +0000

    update 01.cc

--- 001-僵尸进程/01.cc
+++ 001-僵尸进程/01.cc
@@ -7,7 +7,7 @@
 #include <iostream>
 #include <string>
 
-void log(const std::string& msg = "") {
+void log(const std::string& msg) {
     time_t     now  = time(NULL);
     struct tm* info = localtime(&now);
     char       buf[1024];
@@ -16,9 +16,7 @@ void log(const std::string& msg = "") {
 }
 
 int main() {
-    log();
-    log("计算机操作系统-僵尸进程-产生: 未处理 SIGCHLD");
-    log();
+    log("僵尸进程-产生: 未处理 SIGCHLD");
 
     pid_t fd = fork();
 
@@ -37,8 +35,6 @@ int main() {
     log("子进程状态");
     system(cmd.data());
 
-    log();
     log("主进程正常退出");
-    log();
     return 0;
 }
