2de248735962731a86c501ed0b7579891c8ea02a
Yunbin Liu <yunbinliu@outlook.com>
2026-09-04 09:40:33 +0000

    Revert "update 06.cc"
    This reverts commit b28191e7c3118736e5c7860e0635a5496374f42e.

--- 001-僵尸进程/06.cc
+++ 001-僵尸进程/06.cc
@@ -7,7 +7,7 @@
 #include <iostream>
 #include <string>
 
-void log(const std::string& msg) {
+void log(const std::string& msg = "") {
     time_t     now  = time(NULL);
     struct tm* info = localtime(&now);
     char       buf[1024];
@@ -16,7 +16,9 @@ void log(const std::string& msg) {
 }
 
 int main() {
-    log("僵尸进程-预防: 杀死父进程");
+    log();
+    log("计算机操作系统-僵尸进程-预防: 杀死父进程");
+    log();
 
     if (fork() == 0) {
         pid_t child = fork();
@@ -51,6 +53,8 @@ int main() {
 
     sleep(4);
 
+    log();
     log("主进程正常退出");
+    log();
     return 0;
 }
