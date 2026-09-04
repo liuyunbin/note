493c5c7cd05134cad60172ab72c91f130e6b5f56
Yunbin Liu <yunbinliu@outlook.com>
2026-09-04 08:05:12 +0000

    update 07.cc

--- 001-僵尸进程/07.cc
+++ 001-僵尸进程/07.cc
@@ -7,7 +7,7 @@
 #include <iostream>
 #include <string>
 
-void log(const std::string& msg = "") {
+void log(const std::string& msg) {
     time_t     now  = time(NULL);
     struct tm* info = localtime(&now);
     char       buf[1024];
@@ -16,9 +16,7 @@ void log(const std::string& msg = "") {
 }
 
 int main() {
-    log();
-    log("计算机操作系统-僵尸进程-销毁: 杀死僵尸进程的父进程");
-    log();
+    log("僵尸进程-销毁: 杀死僵尸进程的父进程");
 
     if (fork() == 0) {
         pid_t fd = fork();
@@ -54,8 +52,6 @@ int main() {
 
     sleep(4);
 
-    log();
     log("主进程正常退出");
-    log();
     return 0;
 }
